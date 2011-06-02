7d808f1ee6e33f0ca82215f150d8206179e7dcab
Parker Bossier <parker@planjar.com>
2011-06-01 23:13:22 -0500

    Changed base_url

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'planjar.com/dev';
+$config['base_url']	= 'http://testing.pagodabox.com/';
 
 /*
 |--------------------------------------------------------------------------
