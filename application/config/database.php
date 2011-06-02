6b9a4aa306acb73137ebb380081036936ec3529b
Parker Bossier <parker@planjar.com>
2011-06-01 23:04:34 -0500

    Updated CI initial prefs

--- application/config/database.php
+++ application/config/database.php
@@ -41,10 +41,10 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['hostname'] = 'outnotabout.com';
+$db['default']['username'] = 'billsona_dev';
+$db['default']['password'] = 'planDev11';
+$db['default']['database'] = 'billsona_planjar';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
