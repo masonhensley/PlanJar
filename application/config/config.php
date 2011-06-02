15f6c3ecc9c82fd6e3a6c7f972a20f0451472953
Wells Johnston <wellsjohnston@gmail.com>
2011-06-02 00:55:52 -0500

    Changed .htaccess file to ignore "index.php".  It also enforces a url standard by including "www." and adding the controller name to the end.  In addition, it strips the "/" at the end of the URL.

--- application/config/config.php
+++ application/config/config.php
@@ -26,7 +26,7 @@ $config['base_url']	= 'http://testing.pagodabox.com/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
