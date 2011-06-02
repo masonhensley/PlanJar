d22eb7a2f37dbfd0513bdea5232d78c03d8f6d40
Parker Bossier <parker@planjar.com>
2011-06-02 01:09:58 -0500

    Updated default view to welcome splash screen

--- application/assets/welcome_screen/coming_soon.php
+++ application/assets/welcome_screen/coming_soon.php
@@ -1,4 +1,4 @@
-<?php require_once(base_url() . "/application/assets/welcome_screen/assets/includes/functions.php"); ?>
+<?php $this->load->helper('url'); require_once(base_url . "/application/assets/welcome_screen/assets/includes/functions.php"); ?>
 <?php include("assets/includes/settings.php"); ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
