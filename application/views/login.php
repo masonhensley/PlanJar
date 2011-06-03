274d8338426ab384a6d446cd33b0b7a4f9279a4a
Parker Bossier <parker@planjar.com>
2011-06-03 15:00:29 -0500

    validate/signup

--- application/views/login.php
+++ application/views/login.php
@@ -120,8 +120,6 @@
                         <div class="div_texbox">
                             <input id="su_grad_year" name="su_grad_year" type="text" class="textbox" value="<?php echo date('Y') + 4; ?>" onfocus="if(this.value==<?php echo date('Y') + 4; ?>){this.value='';}">
                         </div>
-                        
-                        <div id="sign_up_errors"></div>
 
                         <div class="button_div">
                             <input name="sign_up" type="button" value="Sign up" class="buttons">
@@ -130,8 +128,8 @@
                 </fieldset>
             </div>
             <div id="rightSide">
-                <p><u>This is the right side div that can be used for showing info's in order to help the visitor.</u> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                </p></div>
+                <div id="sign_up_errors"></div>
+            </div>
             <div class="clear"></div>
         </div>
 
