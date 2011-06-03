758fc73da0cfd4ac87981d349913f3e0f70eb9ac
Parker Bossier <parker@planjar.com>
2011-06-03 16:33:33 -0500

    validate/signup

--- application/assets/js/sign_up_functions.js
+++ application/assets/js/sign_up_functions.js
@@ -69,56 +69,55 @@ $(document).ready(function() {
                 required: "Password field is required."
             }
         }
-        
     });
     
-    // Initialize the login validate instance.
-    //    $("#login").validate({
-    //        rules: {
-    //            email: {
-    //                required: true,
-    //                email: true
-    //            },
-    //            password: {
-    //                required: true,
-    //                password: true
-    //            }
-    //        },
-    //        submitHandler: function(form) {
-    //        },
-    //        invalidHandler: function(form, validator) {
-    //        }
-    //    });
-    
-    /**--------------------------
+// Initialize the login validate instance.
+//    $("#login").validate({
+//        rules: {
+//            email: {
+//                required: true,
+//                email: true
+//            },
+//            password: {
+//                required: true,
+//                password: true
+//            }
+//        },
+//        submitHandler: function(form) {
+//        },
+//        invalidHandler: function(form, validator) {
+//        }
+//    });
+});
+
+/**--------------------------
 //* Validate Date Field script- By JavaScriptKit.com
 //* For this script and 100s more, visit http://www.javascriptkit.com
 //* This notice must stay intact for usage
 ---------------------------**/
 
-    function checkdate(input){
-        var validformat=/^\d{2}\/\d{2}\/\d{4}$/ //Basic check for format validity
-        var returnval=false
-        if (!validformat.test(input.value))
-            alert("Invalid Date Format. Please correct and submit again.")
-        else{ //Detailed check for valid date ranges
-            var monthfield=input.value.split("/")[0]
-            var dayfield=input.value.split("/")[1]
-            var yearfield=input.value.split("/")[2]
-            var dayobj = new Date(yearfield, monthfield-1, dayfield)
-            if ((dayobj.getMonth()+1!=monthfield)||(dayobj.getDate()!=dayfield)||(dayobj.getFullYear()!=yearfield))
-                alert("Invalid Day, Month, or Year range detected. Please correct and submit again.")
-            else
-                returnval=true
-        }
-        if (returnval==false) input.select()
-        return returnval
+function checkdate(input){
+    var validformat=/^\d{2}\/\d{2}\/\d{4}$/ //Basic check for format validity
+    var returnval=false
+    if (!validformat.test(input.value))
+        alert("Invalid Date Format. Please correct and submit again.")
+    else{ //Detailed check for valid date ranges
+        var monthfield=input.value.split("/")[0]
+        var dayfield=input.value.split("/")[1]
+        var yearfield=input.value.split("/")[2]
+        var dayobj = new Date(yearfield, monthfield-1, dayfield)
+        if ((dayobj.getMonth()+1!=monthfield)||(dayobj.getDate()!=dayfield)||(dayobj.getFullYear()!=yearfield))
+            alert("Invalid Day, Month, or Year range detected. Please correct and submit again.")
+        else
+            returnval=true
     }
+    if (returnval==false) input.select()
+    return returnval
+}
     
-    //returns the current year. used for login.
-    function getYear()
-    {
-        var d = new Date();
-        return d.getFullYear();
-    }
-});
+//returns the current year. used for login.
+function getYear()
+{
+    var d = new Date();
+    return d.getFullYear();
+}
