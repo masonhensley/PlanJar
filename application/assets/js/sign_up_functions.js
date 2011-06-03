935b7a10e96d95cf88b7c6ea9c1de19960c00c0a
Parker Bossier <parker@planjar.com>
2011-06-03 01:31:20 -0500

    login form

--- application/assets/js/sign_up_functions.js
+++ application/assets/js/sign_up_functions.js
@@ -63,9 +63,28 @@ $(document).ready(function() {
         }
     });
     
-    // Initialize the birthday picker instance.
-    $('#birthday').birthdaypicker({
-        placeholder: false,
-        defaultDate:'03-30-1990'
-    });
+    /**--------------------------
+//* Validate Date Field script- By JavaScriptKit.com
+//* For this script and 100s more, visit http://www.javascriptkit.com
+//* This notice must stay intact for usage
+---------------------------**/
+
+    function checkdate(input){
+        var validformat=/^\d{2}\/\d{2}\/\d{4}$/ //Basic check for format validity
+        var returnval=false
+        if (!validformat.test(input.value))
+            alert("Invalid Date Format. Please correct and submit again.")
+        else{ //Detailed check for valid date ranges
+            var monthfield=input.value.split("/")[0]
+            var dayfield=input.value.split("/")[1]
+            var yearfield=input.value.split("/")[2]
+            var dayobj = new Date(yearfield, monthfield-1, dayfield)
+            if ((dayobj.getMonth()+1!=monthfield)||(dayobj.getDate()!=dayfield)||(dayobj.getFullYear()!=yearfield))
+                alert("Invalid Day, Month, or Year range detected. Please correct and submit again.")
+            else
+                returnval=true
+        }
+        if (returnval==false) input.select()
+        return returnval
+    }
 });
