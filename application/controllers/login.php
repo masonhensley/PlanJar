261b580c2ea0c605102194f7f99b16ec8f2237e0
Parker Bossier <parker@planjar.com>
2011-06-03 12:25:35 -0500

    login

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -3,7 +3,7 @@
 if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Login extends CI_Controller {
 
     /**
      * Index Page for this controller.
