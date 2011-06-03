d3f038376d65e3aa3a69ef640358c8ae54156883
Parker Bossier <parker@planjar.com>
2011-06-02 20:38:20 -0500

    Validate

--- application/views/login.php
+++ application/views/login.php
@@ -6,9 +6,8 @@
         <script type="text/javascript" src="/application/assets/js/jquery-1.6.1.min.js"></script>
         <script type="text/javascript" src="/application/assets/js/jquery-validate-1.5.5/jquery.validate.min.js"></script>
         <script type="text/javascript" src="/application/assets/js/formly.min.js"></script>
-        
-        <link rel="stylesheet" href="/application/assets/css/formly.css" type="text/css" />   
-        
+        <link rel="stylesheet" href="/application/assets/css/formly.min.css" type="text/css" />   
+
         <style type="text/css">
             * { font-family: Verdana; font-size: 96%; }
             label { width: 10em; float: left; }
