89d8fc4e636719ff7e6bfef88b198bd348cb9159
Parker Bossier <parker@planjar.com>
2011-06-05 15:21:41 -0500

    login stuff

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -38,15 +38,17 @@ class Login extends CI_Controller {
         
         $registered = $this->ion_auth->register($email, $password, $email, $additional_data);
         
-        if($registered)
-        {
-            echo "success";
-        }else{
-            if(!$this->ion_auth->username_check($email))
-            {
-                echo "username_error";
-            }
-        }
+        echo($additional_data['grad_year']);
+        
+//        if($registered)
+//        {
+//            echo "success";
+//        }else{
+//            if(!$this->ion_auth->username_check($email))
+//            {
+//                echo "username_error";
+//            }
+//        }
         
     }
 
