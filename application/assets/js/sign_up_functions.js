fdc9843e10c2892232941d9e20345c978b166235
Parker Bossier <parker@planjar.com>
2011-06-05 17:19:34 -0500

    login stuff

--- application/assets/js/sign_up_functions.js
+++ application/assets/js/sign_up_functions.js
@@ -14,7 +14,6 @@ $(document).ready(function() {
         },
         submitHandler: function(form) {
             $.get('/login/try_log_in', $('#log_in').serialize(), function(data) {
-                alert(data);
                 if (data != 'error')  {
                     window.location.href = data;
                 }
