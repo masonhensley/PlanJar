cddb4159e526b7743d685a1c7c8e159765f273c4
Wells Johnston <wellsjohnston@gmail.com>
2011-06-05 08:44:37 -0500

    css

---
+++ application/controllers/home.php
@@ -0,0 +1,9 @@
+<?php
+// prevent direct script access
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
--- application/controllers/login.php
+++ application/controllers/login.php
@@ -1,5 +1,6 @@
 <?php
 
+// prevent direct script access
 if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 
