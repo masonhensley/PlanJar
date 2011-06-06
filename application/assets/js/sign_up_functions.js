a92bdb46772f88d846d70029bf43c67ebc35540e
Parker Bossier <parker@planjar.com>
2011-06-05 23:46:00 -0500

    login stuff

--- application/assets/js/sign_up_functions.js
+++ application/assets/js/sign_up_functions.js
@@ -34,106 +34,113 @@ $(document).ready(function() {
         },
         errorLabelContainer: '#li_error_list',
         wrapper: 'li',
-        showErrors: function(error_map, error_list) {
-            if(error_list.length) {
-                $("#li_error_list").html(error_list[0]['message']);
-                $(errorList[0]['element']).addClass("error");
+        showErrors: function(errorMap, errorList) {
+            // Adapted from http://stackoverflow.com/questions/4342950/jquery-validate-plugin-display-one-error-at-a-time-with-css/4343177#4343177
+            $("#log_in").find("input").each(function() {
+                $(this).removeClass("error");
+            });
+            
+            $("#li_error_list").html("");
+            if(errorList.length) {
+                $("#li_error_list").html(errorList[0]['message']);
+                $(errorList[0]['element']).parent.addClass("error_message");
             }
-        });
-    
-        // Initialize the sign up Validator instance.
-        $('#sign_up').validate({
-            rules: {
-                su_email_1: {
-                    required: true,
-                    email: true
-                },
-                su_email_2: {
-                    required: true,
-                    equalTo: '#su_email_1'
-                },
-                su_password: {
-                    required: true,
-                    rangelength: [8, 20]
-                },
-                su_first_name: {
-                    required: true,
-                    rangelength: [2, 20]
-                },
-                su_last_name: {
-                    required: true,
-                    rangelength: [2, 20]
-                },
-                su_school: {
-                    required: true,
-                    maxlength: 60
-                },
-                // Note that validating #su_sex isn't needed.
-                su_birthday: {
-                    required: true,
-                    date:true
-                },
-                su_grad_year: {
-                    required: true,
-                    max: get_year() + 6
-                }
-            },
-            submitHandler: function(form) {
-                // Send the form information to the try_sign_up function.
-                alert($('#sign_up').serialize());
-                $.get('/login/try_sign_up', $('#sign_up').serialize(), function(data) {
-                    // Redirect or display the error.
-                    alert(data);
-                    //                if (data != 'error')  {
-                    //                    window.location.href = data;
-                    //                } else {
-                    //                    alert(data);
-                    //                }
-                });
-            },
-            messages: {
-                su_email_1: {
-                    required: 'Email address is required.',
-                    email: 'Email address must be valid.'
-                },
-                su_email_2: {
-                    required: 'Re-enter you remail address.',
-                    equalTo: 'Email addresses must match.'
-                },
-                su_password: {
-                    required: 'Password is required.',
-                    rangelength: 'Password must be between 8 and 20 characters.'
-                
-                },
-                su_first_name: {
-                    required: 'First name is required.',
-                    rangelength: 'First name must be between 2 and 20 characters.'
-                },
-                su_last_name: {
-                    required: 'Last name is required.',
-                    rangelength: 'Last name must be between 2 and 20 characters.'
-                },
-                su_school: {
-                    required: 'School is required.',
-                    maxlength: 60
-                },
-                // Note that validating #su_sex isn't needed.
-                su_month: {
-               
-                },
-                su_grad_year: {
-                    required: 'Graduation year is required.',
-                    max: "Sorry, you can't graduate more than 6 years from now."
-                }
-            },
-            errorLabelContainer: '#su_error_list',
-            wrapper: 'li'
-        });
+        }
     });
+    
+    // Initialize the sign up Validator instance.
+    $('#sign_up').validate({
+        rules: {
+            su_email_1: {
+                required: true,
+                email: true
+            },
+            su_email_2: {
+                required: true,
+                equalTo: '#su_email_1'
+            },
+            su_password: {
+                required: true,
+                rangelength: [8, 20]
+            },
+            su_first_name: {
+                required: true,
+                rangelength: [2, 20]
+            },
+            su_last_name: {
+                required: true,
+                rangelength: [2, 20]
+            },
+            su_school: {
+                required: true,
+                maxlength: 60
+            },
+            // Note that validating #su_sex isn't needed.
+            su_birthday: {
+                required: true,
+                date:true
+            },
+            su_grad_year: {
+                required: true,
+                max: get_year() + 6
+            }
+        },
+        submitHandler: function(form) {
+            // Send the form information to the try_sign_up function.
+            alert($('#sign_up').serialize());
+            $.get('/login/try_sign_up', $('#sign_up').serialize(), function(data) {
+                // Redirect or display the error.
+                alert(data);
+            //                if (data != 'error')  {
+            //                    window.location.href = data;
+            //                } else {
+            //                    alert(data);
+            //                }
+            });
+        },
+        messages: {
+            su_email_1: {
+                required: 'Email address is required.',
+                email: 'Email address must be valid.'
+            },
+            su_email_2: {
+                required: 'Re-enter you remail address.',
+                equalTo: 'Email addresses must match.'
+            },
+            su_password: {
+                required: 'Password is required.',
+                rangelength: 'Password must be between 8 and 20 characters.'
+                
+            },
+            su_first_name: {
+                required: 'First name is required.',
+                rangelength: 'First name must be between 2 and 20 characters.'
+            },
+            su_last_name: {
+                required: 'Last name is required.',
+                rangelength: 'Last name must be between 2 and 20 characters.'
+            },
+            su_school: {
+                required: 'School is required.',
+                maxlength: 60
+            },
+            // Note that validating #su_sex isn't needed.
+            su_month: {
+               
+            },
+            su_grad_year: {
+                required: 'Graduation year is required.',
+                max: "Sorry, you can't graduate more than 6 years from now."
+            }
+        },
+        errorLabelContainer: '#su_error_list',
+        wrapper: 'li'
+    });
+});
 
-    // Returns the current year.
-    function get_year()
-    {
-        var d = new Date();
-        return d.getFullYear();
-    }
+// Returns the current year.
+function get_year()
+{
+    var d = new Date();
+    return d.getFullYear();
+}
