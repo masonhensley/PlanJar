97e9a6292ff8ffaf39624e2e6b986f7eafeb13f5
Mason Hensley <masonhensley@gmail.com>
2011-06-06 00:37:34 -0700

    Changed formatting of gender selection to be consistent with the date fields on the sign in page

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -84,6 +84,7 @@
                         <label for="su_sex">Sex</label>
                         <div class="div_texbox" style="">
                             <select class="buttons_black" name="su_sex">
+ <option value="gender"selected="selected">Select Gender:</option>
                                 <option value="male">Male</option>
                                 <option value="female">Female</option>
                             </select>
