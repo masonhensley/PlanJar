6d9ee5e7e1b2e96c8c42fce3f24032a0945fc10b
Parker Bossier <parker@planjar.com>
2011-06-06 18:20:48 -0500

    login stuff

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -87,7 +87,7 @@
 
                         <label for="su_school">School</label>
                         <div class="div_texbox">
-                            <input id="su_school" name="su_school" type="text" class="textbox" onchange="this.value = ''">
+                            <input id="su_school" name="su_school" type="text" class="textbox" onblur="force_school()">
                         </div>
                         
                         <!-- Hidden input used to pass the school id to the server instead of the school name -->
