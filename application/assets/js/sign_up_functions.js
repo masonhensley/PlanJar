5f6b98f68732403022e8e84dc34b5957856d71e0
Parker Bossier <parker@planjar.com>
2011-06-06 15:23:20 -0500

    login stuff

--- application/assets/js/sign_up_functions.js
+++ application/assets/js/sign_up_functions.js
@@ -47,12 +47,13 @@ $(document).ready(function() {
     //    });
     //    
 
-alert('here');
+    alert('here');
     // Initialize the sign up Validator instance.
     $('#sign_up').validate({
         rules: {
             su_email_1: {
-//                required: true,
+                required: true
+            }
 //                email: true,
 //                remote: {
 //                    url: '/login/check_email',
@@ -61,51 +62,51 @@ alert('here');
 //                        email: '#su_email_1'.val()
 //                    }
 //                }
-            },
-            su_email_2: {
-                required: true
-            },
-            su_password: {
-                required: true,
-                rangelength: [8, 20]
-            },
-            su_first_name: {
-                required: true,
-                rangelength: [2, 20]
-            },
-            su_last_name: {
-                required: true,
-                rangelength: [2, 20]
-            },
-            su_school: {
-                required: true,
-                maxlength: 60
-            },
-            su_sex: {
-                //nna: true
-            },
-            su_month: {
-                //nna: true
-            },
-            su_year: {
-                //nna: true
-            },
-            su_grad_year: {
-                required: true,
-                max: get_year() + 6
-            }
+//            },
+//            su_email_2: {
+//                required: true
+//            },
+//            su_password: {
+//                required: true,
+//                rangelength: [8, 20]
+//            },
+//            su_first_name: {
+//                required: true,
+//                rangelength: [2, 20]
+//            },
+//            su_last_name: {
+//                required: true,
+//                rangelength: [2, 20]
+//            },
+//            su_school: {
+//                required: true,
+//                maxlength: 60
+//            },
+//            su_sex: {
+//                //nna: true
+//            },
+//            su_month: {
+//                //nna: true
+//            },
+//            su_year: {
+//                //nna: true
+//            },
+//            su_grad_year: {
+//                required: true,
+//                max: get_year() + 6
+//            }
         },
         submitHandler: function(form) {
             // Send the form information to the try_sign_up function.
             alert($('#sign_up').serialize());
             $.get('/login/try_sign_up', $('#sign_up').serialize(), function(data) {
                 alert(data);
-            //                // Redirect or display the error.
-            //                if (data != 'error')  {
-            //                    window.location.href = data;
-            //                } else {
-            //                    alert(data);
-            //                }
+                //                // Redirect or display the error.
+                //                if (data != 'error')  {
+                //                    window.location.href = data;
+                //                } else {
+                //                    alert(data);
+                //                }
             });
         },
         messages: {
@@ -140,46 +141,46 @@ alert('here');
         },
         showErrors: function(errorMap, errorList) {
             // Adapted from http://stackoverflow.com/questions/4342950/jquery-validate-plugin-display-one-error-at-a-time-with-css/4343177#4343177
-//            $("#sign_up").find("input").each(function() {
-//                $(this).removeClass("highlight_error");
-//            });
+            //            $("#sign_up").find("input").each(function() {
+            //                $(this).removeClass("highlight_error");
+            //            });
             
-//            $("#su_error").html("");
-//            if(errorList.length) {
-//                $("#su_error").html(errorList[0]['message']);
-//                $(errorList[0]['element']).addClass("highlight_error");
-//            }
+            //            $("#su_error").html("");
+            //            if(errorList.length) {
+            //                $("#su_error").html(errorList[0]['message']);
+            //                $(errorList[0]['element']).addClass("highlight_error");
+            //            }
         }
     });
 
 
-//    
-//    // Initialize the autocomplete instance.
-//    $('#su_school').autocomplete({
-//        minLength: 2,
-//        source: function (request, response) {
-//            $.get('/login/search_schools', {
-//                needle: request.term
-//            }, function (data) {
-//                
-//                // Convert each item in the input JSON to the required JSON form for the autocomplete
-//                // and pass the result through the response handler.
-//                data = $.parseJSON(data);
-//                response($.map(data, function (item) {
-//                    return {
-//                        label: item.school + ' (' + item.city + ')', 
-//                        value: item.school
-//                    };
-//                }));
-//                
-//            });
-//        },
-//        select: function (event, ui) {
-//            $('#su_school').value = ui.item.value;
-//        }
-//    })
-//    
-//// End of ready function.
+    //    
+    //    // Initialize the autocomplete instance.
+    //    $('#su_school').autocomplete({
+    //        minLength: 2,
+    //        source: function (request, response) {
+    //            $.get('/login/search_schools', {
+    //                needle: request.term
+    //            }, function (data) {
+    //                
+    //                // Convert each item in the input JSON to the required JSON form for the autocomplete
+    //                // and pass the result through the response handler.
+    //                data = $.parseJSON(data);
+    //                response($.map(data, function (item) {
+    //                    return {
+    //                        label: item.school + ' (' + item.city + ')', 
+    //                        value: item.school
+    //                    };
+    //                }));
+    //                
+    //            });
+    //        },
+    //        select: function (event, ui) {
+    //            $('#su_school').value = ui.item.value;
+    //        }
+    //    })
+    //    
+    //// End of ready function.
 });
 //
 //// Returns the current year.
