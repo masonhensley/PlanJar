e660f435299c98fc6d175fb7b5a2a87d8ab88d55
Parker Bossier <parker@planjar.com>
2011-06-06 16:47:36 -0500

    login stuff

--- application/config/ion_auth.php
+++ application/config/ion_auth.php
@@ -89,7 +89,7 @@ $config['max_password_length'] = 20;
 /**
  * Email Activation for registration
  * */
-$config['email_activation'] = false;
+$config['email_activation'] = true;
 
 /**
  * Allow users to be remembered and enable auto-login
