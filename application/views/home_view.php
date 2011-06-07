810b7bcee8d58c4810a4d6d9d8392751cd6be425
Parker Bossier <parker@planjar.com>
2011-06-07 17:04:10 -0500

    groups tab on the left

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -43,9 +43,9 @@
 
                 <div id="one_mult">
                     <label for="sel_one">Select one</label>
-                    <input type="radio" id="sel_one" onchange="reset_my_groups()"/>
-                    <label for="sel_all">Select one</label>
-                    <input type="radio" id="sel_one"/>
+                    <input type="radio" id="sel_one" name="one_mult_group" onchange="reset_my_groups()"/>
+                    <label for="sel_all">Select multiple</label>
+                    <input type="radio" id="sel_mult" name="one_mult_group"/>
                 </div> 
                 <ol id="my_groups">
 
