9b2b7cdf2ec3a2e192d467e466ece835c727a3e5
Parker Bossier <parker@planjar.com>
2011-06-07 14:06:31 -0500

    login stuff

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -3,6 +3,7 @@
         <script type="text/javascript" src="/application/assets/js/jquery-1.6.1.min.js"></script>
         <script type="text/javascript" src="/application/assets/js/jquery-ui-1.8.13.min.js"></script>
         <script type="text/javascript" src="/application/assets/js/home_functions.js"></script>
+        <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
 
         <link rel=stylesheet href="/application/assets/css/home.css" type="text/css" />
     </head>
@@ -12,14 +13,14 @@
         <div class="container">
 
             <div class="left_panel">
-                <ol id="test">
+<!--                <ol id="test">
                     <?php
-                    for ($i = 1; $i < 10; ++$i)
-                    {
-                        echo("<li class=\"ui-widget-content\">Item " . $i . "</li>");
-                    }
+//                    for ($i = 1; $i < 10; ++$i)
+//                    {
+//                        echo("<li class=\"ui-widget-content\">Item " . $i . "</li>");
+//                    }
                     ?>
-                </ul>
+                    </ul>-->
             </div>
 
             <div class="centerpanel">
