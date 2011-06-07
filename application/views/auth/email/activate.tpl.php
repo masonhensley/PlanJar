4ab2e559dc81230582958d91ae010012d617f796
Parker Bossier <parker@planjar.com>
2011-06-06 21:35:43 -0500

    login stuff

--- application/views/auth/email/activate.tpl.php
+++ application/views/auth/email/activate.tpl.php
@@ -1,6 +1,6 @@
 <html>
     <body>
-        <div class="almost_there">
+        <div style="width:75%">
             <fieldset style="border:1px solid #000000; padding:10px; background-color: white;">
                 <legend style="font-family:Arial, Helvetica, sans-serif; font-size: 120%;
                         letter-spacing: -1px; font-weight: bold; line-height: 1.1; color:#fff;
@@ -8,7 +8,9 @@
                     Activate your account (<?php echo $identity; ?>)
                 </legend>
                 <p>Please click this link to
+                    <font color="white">
                     <?php echo anchor('auth/activate/' . $id . '/' . $activation, 'activate your account'); ?>.</p>
+                </font>
             </fieldset>
         </div>
     </body>
