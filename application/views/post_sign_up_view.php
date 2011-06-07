02ad87802510dbe94191340516d79190f8c1bfea
Parker Bossier <parker@planjar.com>
2011-06-06 20:03:42 -0500

    login stuff

--- application/views/post_sign_up_view.php
+++ application/views/post_sign_up_view.php
@@ -1,9 +1,19 @@
 <html>
     <head>
-        
+        <title>Almost there</title>
+        <?php
+        // Add the includes from js-css-includes.
+        echo(add_includes());
+        ?>
     </head>
-    
+
     <body>
-        Thanks for registering for PlanJar.com. Please check your email to confirm your account.
+        <div class="width:250px">
+            <fieldset>
+                <legend>Almost there...</legend>
+                Thanks for registering for PlanJar.com.
+                Please check your email to confirm your account.
+            </fieldset>
+        </div>
     </body>
 </html>
