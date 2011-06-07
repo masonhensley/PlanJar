fc38b9b08f2d009a4159edf9b279e3dea2ca8ea0
Parker Bossier <parker@planjar.com>
2011-06-07 01:14:47 -0500

    login stuff

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -31,17 +31,17 @@
                         <form id="log_in" class="form">
 
                             <div class="div_texbox">
-<!--                                <p>
+                                <p>
                                     <label for="li_email">Email</label><br>-->
-                                <input id="li_email" name="li_email" type="text" class="textbox" id="li_email" />
-                                <!--                                </p>-->
+                                    <input id="li_email" name="li_email" type="text" class="textbox" id="li_email" />
+                                </p>
                             </div>
 
                             <div class="div_texbox">
-<!--                                <p>
+                                <p>
                                     <label for="li_password">Password</label>-->
-                                <input name="li_password" type="password" class="textbox" id="password" />
-                                <!--                            </p>-->
+                                    <input name="li_password" type="password" class="textbox" id="password" />
+                                </p>
                             </div>
 
                             <div class="button_div">
