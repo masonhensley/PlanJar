07cb72e9492388c7b649e4a26d875992ab8af0e2
Parker Bossier <parker@planjar.com>
2011-06-07 12:56:58 -0500

    login stuff

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -69,7 +69,6 @@
                         </font>
                         <center>
                             <form id="sign_up" class="form">
-                                <div id="top_padding" style="position:relative; width:260px; height:10px;"></div>
 
                                 <div id="padding" style="position:relative; width:260px; height:10px;"></div>
                                 <div class="div_texbox">
