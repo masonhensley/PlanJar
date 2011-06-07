dd7ab8b9f940a78691792d477685efc024fd7fac
Parker Bossier <parker@planjar.com>
2011-06-07 17:19:28 -0500

    groups tab on the left

--- application/views/foo_view.php
+++ application/views/foo_view.php
@@ -10,7 +10,25 @@
 
     <body>
     <center>
-        <div id="map_canvas" style="width:400px; height:400px"></div>
+        <div id="one_mult">
+            <label for="sel_one">Select one</label>
+            <input type="radio" id="sel_one" name="one_mult_group" onchange="reset_my_groups()"/>
+            <label for="sel_all">Select multiple</label>
+            <input type="radio" id="sel_mult" name="one_mult_group"/>
+        </div>
+
+        <div style="width:400px; height:600px">
+            <ol id="my_groups">
+                <?php
+                for ($i = 1; $i < 10; ++$i)
+                {
+                    ?>
+                    <li class="ui-widget-content">Item  <?php echo($i); ?></li>
+                    <?php
+                }
+                ?>
+            </ol>
+        </div>
     </center>
 </body>
 </html>
