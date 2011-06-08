271feb642b6ed29b55c9df2a6eafc3051e0f7e9f
Parker Bossier <parker@planjar.com>
2011-06-08 15:08:04 -0500

    make a plan, bray

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -7,7 +7,9 @@
         <script type="text/javascript" src="/application/assets/js/home_functions.js"></script>
         <script src="http://maps.google.com/maps/api/js?sensor=false" type="text/javascript"></script>
         <script type="text/javascript" src="/application/assets/js/jquery.infieldlabel.min.js"></script>
+        <script type="text/javascript" src="/application/assets/js/jquery.simplemodal.1.4.1.min.js"></script>
 
+        
         <link rel=stylesheet href="/application/assets/css/home.css" type="text/css" />
         <link type="text/css" rel=stylesheet href="/application/assets/css/eggplant/theme.css"/>
 
@@ -35,8 +37,9 @@
                     </div>
 
                     <div class="center_top_right">
-                        <!-- Status panel -->
-                        <?php include(APPPATH . 'assets/php/status_panel.php'); ?>
+                        <!-- New plan panel -->
+                        <input type="button" id="make_a_plan" value="Make a plan."/>
+                        <?php include(APPPATH . 'assets/php/plan_panel.php'); ?>
                     </div>
                 </div>
                 <div class="center_day">
