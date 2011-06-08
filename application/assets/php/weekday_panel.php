a8bbdae8e2a5ca46e9d6de9137535993a8599cd0
Parker Bossier <parker@planjar.com>
2011-06-08 17:44:29 -0500

    make a plan, bray

--- application/assets/php/weekday_panel.php
+++ application/assets/php/weekday_panel.php
@@ -14,7 +14,7 @@
             <a href="#<?php echo($i); ?>"><?php
     // Format the displayed day name (e.g. Tue - 9).
     $day_name = $days[(date('w') + $i) % 7];
-    $day_name .= ' - ' . date('j') + $i;
+    $day_name .= ' - ' . (date('j') + $i);
     echo($day_name);
         ?>
             </a>
