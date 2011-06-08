85530afe3110d99641a0e0ab422d5cb433aff1aa
Parker Bossier <parker@planjar.com>
2011-06-08 12:13:11 -0500

    groups tab on the left

--- application/assets/js/home_functions.js
+++ application/assets/js/home_functions.js
@@ -5,7 +5,7 @@ $(function() {
     // Set up the day of the week tabs.
     $(".tab_content").hide(); //Hide all content
     $("ul.tabs li:first").addClass("active").show(); //Activate first tab
-    $(".tab_content:first").show(); //Show first tab content
+    //$(".tab_content:first").show(); //Show first tab content
 
     //On Click Event
     $("ul.tabs li").click(function() {
@@ -14,8 +14,8 @@ $(function() {
         $(this).addClass("active"); //Add "active" class to selected tab
         $(".tab_content").hide(); //Hide all tab content
 
-        var activeTab = $(this).find("a").attr("href"); //Find the href attribute value to identify the active tab + content
-        $(activeTab).fadeIn(); //Fade in the active ID content
+        //var activeTab = $(this).find("a").attr("href"); //Find the href attribute value to identify the active tab + content
+        //$(activeTab).fadeIn(); //Fade in the active ID content
         return false;
     });
     
--- application/assets/php/weekday_panel.php
+++ application/assets/php/weekday_panel.php
@@ -1,17 +1,17 @@
 <?php $days = array('Sun', 'Mon', 'Tues', 'Weds', 'Thurs', 'Fri', 'Sat'); ?>
 <ul class="tabs">
     <li>
-        <a href="#<?php echo($days[date('w')]); ?>">Today</a>
+        <a href="<?php echo(date('w')); ?>">Today</a>
     </li>
     <li>
-        <a href="#<?php echo($days[date('w') + 1]); ?>">Tom</a>
+        <a href="<?php echo(date('w') + 1); ?>">Tom</a>
     </li>
     <?php
     for ($i = 2; $i < 7; ++$i)
     {
         ?>
         <li>
-            <a href="#<?php echo($days[(date('w') + $i) % 7]); ?>"><?php echo($days[(date('w') + $i) % 7]); ?></a>
+            <a href="#<?php echo((date('w') + $i) % 7); ?>"><?php echo($days[(date('w') + $i) % 7]); ?></a>
         </li>
         <?php
     }
