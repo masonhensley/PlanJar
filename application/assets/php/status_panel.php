d2c5eaa3d6c829908ffad59c9fd5e4e7b221e071
Parker Bossier <parker@planjar.com>
2011-06-08 14:08:26 -0500

    make a plan, bray

--- application/assets/php/status_panel.php
+++ application/assets/php/status_panel.php
@@ -2,6 +2,6 @@ Make a plan:
 <form id="make_plan">
     <div class="in-field_block">
         <label for="plan_location">Where are you going?</label>
-        <input type="text" id="plan_location" name="plan_location"/>
+        <input type="text" id="plan_location" name="plan_location" class="textbox"/>
     </div>
 </form>
