5a364f6c4026200542a2e925003a21b3cfe28610
Parker Bossier <parker@planjar.com>
2011-06-08 18:03:55 -0500

    make a plan, bray

--- application/assets/php/plan_panel.php
+++ application/assets/php/plan_panel.php
@@ -4,14 +4,19 @@
     <div class="in-field_block">
         <label for="plan_location">Where are you going?</label>
         <input type="text" id="plan_location" name="plan_location" class="textbox"/>
+        <br/>
+        <p>Start typing, and we'll try to guess what you're looking for.</p>
+        <p>Can't find it? Just type in the name and keep going.</p>
     </div>
+    
+    <div style="width:20px; float:left"></div>
 
     <div class="in-field_block">
         <label for="plan_description">What are you doing?</label>
         <input type="text" id="plan_description" class="textbox"/>
+        <br/>
+        <p>Start typing what you plan to do.</p>
     </div>
-    
-    <div style="width:20px; float:left"></div>
 
     <select name="day" style="float:right">
         <option value="" selected="selected">What day?</option>
