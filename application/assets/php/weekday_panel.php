926e0718a6e4e49e8dbc888bbe800ebc54346e4c
Parker Bossier <parker@planjar.com>
2011-06-08 12:07:37 -0500

    groups tab on the left

---
+++ application/assets/php/weekday_panel.php
@@ -0,0 +1,19 @@
+<?php $days = array('Sun', 'Mon', 'Tues', 'Weds', 'Thurs', 'Fri', 'Sat'); ?>
+<ul class="tabs">
+    <li>
+        <a href="#<?php echo($days[date('w')]); ?>">Today</a>
+    </li>
+    <li>
+        <a href="#<?php echo($days[date('w') + 1]); ?>">Tom</a>
+    </li>
+    <?php
+    for ($i = 2; $i < 7; ++$i)
+    {
+        ?>
+        <li>
+            <a href="#<?php echo($days[(date('w') + $i) % 7]); ?>"><?php echo($days[(date('w') + $i) % 7]); ?></a>
+        </li>
+        <?php
+    }
+    ?>
+</ul>
--- application/views/home_view.php
+++ application/views/home_view.php
@@ -31,25 +31,7 @@
                 <div class="center_day">
                     
                     <!-- Add the markup for the tabs, starting with today. -->
-                    <?php $days = array('Sun', 'Mon', 'Tues', 'Weds', 'Thurs', 'Fri', 'Sat'); ?>
-                    <ul class="tabs">
-                        <li>
-                            <a href="#<?php echo($days[date('w')]); ?>">Today</a>
-                        </li>
-                        <li>
-                            <a href="#<?php echo($days[date('w') + 1]); ?>">Tom</a>
-                        </li>
-                        <?php
-                        for ($i = 2; $i < 7; ++$i)
-                        {
-                            ?>
-                            <li>
-                                <a href="#<?php echo($days[(date('w') + $i) % 7]); ?>"><?php echo($days[(date('w') + $i) % 7]); ?></a>
-                            </li>
-                            <?php
-                        }
-                        ?>
-                    </ul>
+                    <?php include(APPPATH . 'assets/php/weekday_panel.php'); ?>
 
                 </div>
                 <div class="center_graph">
