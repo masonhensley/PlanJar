ad4176ca4fc5e78373042916e1af0309c0c26f28
Parker Bossier <parker@planjar.com>
2011-06-08 18:50:11 -0500

    make a plan, bray

--- application/assets/php/events_panel.php
+++ application/assets/php/events_panel.php
@@ -1,4 +1,4 @@
-<div style="overflow:scroll;">
+<div style="overflow:scroll; height:100%">
     <?php for ($i = 0; $i < 30; ++$i) { ?>
     <p><?php echo($i); ?> - This is an event</p>
     <?php } ?>
