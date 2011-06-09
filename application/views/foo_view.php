9090dbd006c7154d1ccc572d11f25f33828ff076
Parker Bossier <parker@planjar.com>
2011-06-09 16:33:20 -0500

    make a plan, bray

--- application/views/foo_view.php
+++ application/views/foo_view.php
@@ -2,13 +2,14 @@
 <html>
     <head>
         <title>MaseBook</title>
-
-        <link type="text/css" rel="stylesheet" href="/application/assets/peity/style.css"/>
         
         <script type="text/javascript" src="/application/assets/js/jquery-1.6.1.min.js"></script>
         <script type="text/javascript" src="/application/assets/peity/jquery.peity.min.js"></script>
         <script type="text/javascript" src="/application/assets/peity/code_highlighter.js"></script>
         <script type="text/javascript" src="/application/assets/peity/syntax.js"></script>
+
+        <link type="text/css" rel="stylesheet" href="/application/assets/peity/style.css"/>
+
         <script> 
             $(function() {
                 // Just the defaults.
