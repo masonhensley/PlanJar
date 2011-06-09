fc1f74b2ebbbb7b6acbd768a504711557262a5e9
Parker Bossier <parker@planjar.com>
2011-06-09 13:45:15 -0500

    ya

--- application/assets/php/plan_panel.php
+++ application/assets/php/plan_panel.php
@@ -14,7 +14,7 @@
         <script type="text/javascript">
             $(function() {
                 // Initialize the in-field labels.
-                $('#plan_content label').inFieldLabels();
+                $('label').inFieldLabels();
                 
                 // Set up the Google autocomplete.
                 var defaultBounds = new google.maps.LatLngBounds(
@@ -34,51 +34,15 @@
     </head>
 
     <body>
-        <div id="plan_content">
-            <form id="make_plan">
-                Make a plan:
+        <form id="make_plan">
+            <div class="in-field_block">
+                <label for="plan_location">Where are you going?</label>
+                <input type="text" id="plan_location" name="plan_location" class="textbox"/>
                 <br/>
-                <div class="in-field_block">
-                    <label for="plan_location">Where are you going?</label>
-                    <input type="text" id="plan_location" name="plan_location" class="textbox"/>
-                    <br/>
-                    <!--        <p>Start typing, and we'll try to guess what you're looking for.</p>
-            <p>Can't find it? Just type in the name and keep going.</p>-->
-                </div>
-
-                <div style="width:50px; height:10px; float:left"></div>
-
-                <div class="in-field_block">
-                    <label for="plan_description">What are you doing?</label>
-                    <input type="text" id="plan_description" class="textbox"/>
-                    <br/>
-                    <p>Start typing what you plan to do.</p>
-                </div>
-
-                <select name="day" style="float:right">
-                    <option value="" selected="selected">What day?</option>
-                    <option value="0">Today - <?php echo(date('j')); ?></option>
-                    <option value="1">Tom - <?php echo(date('j') + 1); ?></option>
-                    <?php
-                    $days = array('Sun', 'Mon', 'Tues', 'Weds', 'Thurs', 'Fri', 'Sat');
-                    for ($i = 2; $i < 7; ++$i)
-                    {
-                        ?>
-                        <option value="<?php echo($i); ?>">
-                            <?php
-                            // Format the displayed day name (e.g. Tue - 9).
-                            $day_name = $days[(date('w') + $i) % 7];
-                            $day_name .= ' - ' . (date('j') + $i);
-                            echo($day_name);
-                            ?>
-                        </option>
-                        <?php
-                    }
-                    ?>
-                </select>
-
-                <input type="submit" value="Make a plan"/>
-            </form>
-        </div>
-    </body>
+                <!--        <p>Start typing, and we'll try to guess what you're looking for.</p>
+        <p>Can't find it? Just type in the name and keep going.</p>-->
+            </div>
+        </form>
+    </div>
+</body>
 </html>
