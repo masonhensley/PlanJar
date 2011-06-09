64f0ec815f76a044a29df8ba7d5e4c056b59bb19
Parker Bossier <parker@planjar.com>
2011-06-09 14:02:18 -0500

    ya

--- application/assets/php/plan_panel.php
+++ application/assets/php/plan_panel.php
@@ -21,13 +21,9 @@
                 new google.maps.LatLng(-33.8902, 151.1759),
                 new google.maps.LatLng(-33.8474, 151.2631));
 
-                var input = document.getElementById('plan_location');
-                var options = {
-                    bounds: defaultBounds,
-                    types: ['establishment']
-                };
-
-                autocomplete = new google.maps.places.Autocomplete(input, options);
+                $('#foo').change(function() {
+                    
+                });
             });
         </script>
     </head>
