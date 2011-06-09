2fb8cf3c03b3fd2f4eedbf32820ac3ff791fa539
Parker Bossier <parker@planjar.com>
2011-06-09 14:42:58 -0500

    make a plan, bray

--- application/views/foo_view.php
+++ application/views/foo_view.php
@@ -1,10 +1,9 @@
 <html>
     <head>
-        <script type="text/javascript" src="/application/assets/js/jquery.infieldlabel.min.js"></script>
 
     </head>
 
     <body>
-        <div id="map_canvas"></div>
+        
     </body>
 </html>
