06adbafe90d7cdd39f1a1709dfc12cfbcd5f1e1b
Parker Bossier <parker@planjar.com>
2011-06-09 11:48:01 -0500

    make a plan, bray

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -5,7 +5,7 @@
         <script type="text/javascript" src="/application/assets/js/jquery-1.6.1.min.js"></script>
         <script type="text/javascript" src="/application/assets/js/jquery-ui-1.8.13.min.js"></script>
         <script type="text/javascript" src="/application/assets/js/home_functions.js"></script>
-        <script src="http://maps.google.com/maps/api/js?sensor=false" type="text/javascript"></script>
+        <script src="http://maps.google.com/maps/api/js?libraries=places&sensor=false" type="text/javascript"></script>
         <script type="text/javascript" src="/application/assets/js/jquery.infieldlabel.min.js"></script>
         <script type="text/javascript" src="/application/assets/js/jquery.simplemodal.1.4.1.min.js"></script>
 
