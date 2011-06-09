cac043b475e09935817e79373120ddd8f86d5c54
Parker Bossier <parker@planjar.com>
2011-06-09 14:51:13 -0500

    make a plan, bray

--- application/views/foo_view.php
+++ application/views/foo_view.php
@@ -1,6 +1,6 @@
 <html>
     <head>
-        <title>MaceBook</title>
+        <title>MaseBook</title>
         <script type="text/javascript" src="/application/assets/js/jquery.peity.min.js"></script>
     </head>
 
