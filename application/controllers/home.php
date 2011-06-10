f2edc3ca9ad0287950f39243cf921637fb19d7bc
Parker Bossier <parker@planjar.com>
2011-06-09 23:29:31 -0500

    make a plan, bray

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -66,11 +66,11 @@ class Home extends CI_Controller
   + COS(? * PI() / 180) * COS(`latitude` * PI() / 180) * COS((? - `longitude`) 
   * PI() / 180)) * 180 / PI()) * 60 * 1.1515) AS distance, name, category 
   FROM `pois` WHERE ($like_clauses) ORDER BY distance ASC LIMIT ?";
-        $query = $this->db->query($query_string, array_merge($search_terms, array($latitude, $latitude, $longitude, 10)));
+        $query = $this->db->query($query_string, array_merge(array($latitude, $latitude, $longitude, 10), $search_terms));
 
         echo('foo');
         return;
-        
+
         // Return a JSON array.
         foreach ($query->result_array() as $row)
         {
