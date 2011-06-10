1095ba295f4236d2d416d796a75e8a1bdd6a79d6
Parker Bossier <parker@planjar.com>
2011-06-09 20:50:38 -0500

    ya

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -75,7 +75,7 @@ class Home extends CI_Controller
             $query_string = "SELECT `category` FROM `poi_categories` WHERE `id` = ?";
             $sub_query = $this->db->query($query_string, $row['category']);
             $sub_row = $sub_query->result_array();
-            $row['category'] = $sub_row['category'];
+            //$row['category'] = $sub_row['category'];
             
             // Append to the return array.
             $return_array[] = $row;
