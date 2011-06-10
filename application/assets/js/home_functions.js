52197b973ccc846efe2c52f79795a7c93a2dbf57
Parker Bossier <parker@planjar.com>
2011-06-09 21:42:07 -0500

    ya

--- application/assets/js/home_functions.js
+++ application/assets/js/home_functions.js
@@ -85,6 +85,7 @@ $(function() {
                             // Convert each item in the JSON from the server to the required JSON
                             // form for the autocomplete and pass the result through the response
                             // handler.
+                            alert(myLatitude + ' ' + myLongitude);
                             data = $.parseJSON(data);
                             response($.map(data, function (item) {
                                 return {
