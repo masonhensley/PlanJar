e7bc55a2da1e71080ba4c0c2de44afa868c6577c
Parker Bossier <parker@planjar.com>
2011-06-09 23:46:57 -0500

    make a plan, bray

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -67,9 +67,8 @@ class Home extends CI_Controller
   * PI() / 180)) * 180 / PI()) * 60 * 1.1515) AS distance, name, category 
   FROM `pois` WHERE ($like_clauses) ORDER BY distance ASC LIMIT 10";
         $temp = array_merge(array($latitude, $latitude, $longitude), $search_terms);
-        echo($query_string . '|' . var_export($temp));
-        return;
         $query = $this->db->query($query_string, $temp);
+        
         echo($this->db->last_query());
         return;
 
