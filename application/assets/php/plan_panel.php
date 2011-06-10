853eb2fed03aaccd4097b6211b6b37bb00cba2eb
Parker Bossier <parker@planjar.com>
2011-06-10 00:53:07 -0500

    make a plan, bray

--- application/assets/php/plan_panel.php
+++ application/assets/php/plan_panel.php
@@ -1,23 +1,22 @@
 <div id="plan_content">
-    
+
     <form id="make_plan">
-        
+
+        <p>Start typing the name of where you want to go.
+            If we don't find your location, don't worry about it.</p>
         <div class="in-field_block">
-            <p>Start typing the name of where you want to go.
-                If we don't find your location, don't worry about it.</p>
             <label for="plan_location">Where are you going?</label>
             <input type="text" id="plan_location" name="plan_location" class="textbox"/>
         </div>
-        
+
         <div style="width:30px; height:10px; float:left"></div>
-        
+
+        <p>Start typing what you plan on doing. Choose an option from the menu.</p>
         <div class="in-field_block">
-            <p>Start typing what you plan on doing. Choose an option from the menu.</p>
             <label for="plan_category">What are you doing?</label>
             <input type="text" id="plan_category" name="plan_category" class="textbox"/>
         </div>
-        
-        
+
         <input id="plan_location_id" name="plan_location_id" type="hidden"/>
         <input id="plan_category_id" name="plan_category_id" type="hidden"/>
     </form>
