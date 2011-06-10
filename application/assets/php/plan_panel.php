992f9b671dc4d597d3a4dddc31641b1a2e10bd1f
Parker Bossier <parker@planjar.com>
2011-06-10 01:11:02 -0500

    ya

--- application/assets/php/plan_panel.php
+++ application/assets/php/plan_panel.php
@@ -31,7 +31,7 @@
 
             <tr>
                 <td>
-                    <div id="plan_time" class="radio" style="height:100%">
+                    <div id="plan_time" class="radio" style="height:39px; width:200px">
                         <label for="plan_morning">Morning</label>
                         <input type="radio" id="plan_morning" name="plan_time_group" />
 
