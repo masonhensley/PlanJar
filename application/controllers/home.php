1e7853a6acd0a7e829e07a972b5a6b0302a3ed01
Parker Bossier <parker@planjar.com>
2011-06-10 14:21:55 -0500

    ya

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -69,6 +69,9 @@ class Home extends CI_Controller
   FROM places LEFT JOIN place_categories ON places.category_id=place_categories.id
         WHERE ($like_clauses) ORDER BY distance ASC LIMIT ?";
         $query = $this->db->query($query_string, array($latitude, $latitude, $longitude, 10));
+        
+        echo($this->db->last_query());
+        return;
 
         // Return a JSON array.
         foreach ($query->result_array() as $row)
