6d80ad1a11e7ba9774909650f03390e77b43e441
Parker Bossier <parker@planjar.com>
2011-06-09 23:14:27 -0500

    ya

--- application/assets/php/plan_panel.php
+++ application/assets/php/plan_panel.php
@@ -7,12 +7,14 @@
             <input type="text" id="plan_location" name="plan_location" class="textbox"/>
         </div>
         
+        <div style="width:30px; height:10px; float:left"></div>
+        
         <div class="in-field_block">
             <label for="plan_category">What are you doing?</label>
             <input type="text" id="plan_category" name="plan_category" class="textbox"/>
         </div>
         
-        <input id="plan_location_id" type="hidden"/>
-        <input id="plan_category_id" type="hidden"/>
+        <input id="plan_location_id" name="plan_location_id" type="hidden"/>
+        <input id="plan_category_id" name="plan_category_id" type="hidden"/>
     </form>
 </div>
