a7017c3a1e51246594542127c187a06893844399
Parker Bossier <parker@planjar.com>
2011-06-10 00:56:36 -0500

    ya

--- application/assets/php/plan_panel.php
+++ application/assets/php/plan_panel.php
@@ -2,20 +2,33 @@
 
     <form id="make_plan">
 
-        <p>Start typing the name of where you want to go.
-            If we don't find your location, don't worry about it.</p>
-        <div class="in-field_block">
-            <label for="plan_location">Where are you going?</label>
-            <input type="text" id="plan_location" name="plan_location" class="textbox"/>
-        </div>
+        <table>
+            <tr>
+                <td>
+                    Start typing the name of where you want to go.
+                    If we don't find your location, don't worry about it.
+                </td>
+                <td>
+                    Start typing what you plan on doing.
+                    Choose an option from the menu.
+                </td>
+            </tr>
 
-        <div style="width:30px; height:10px; float:left"></div>
-
-        <p>Start typing what you plan on doing. Choose an option from the menu.</p>
-        <div class="in-field_block">
-            <label for="plan_category">What are you doing?</label>
-            <input type="text" id="plan_category" name="plan_category" class="textbox"/>
-        </div>
+            <tr>
+                <td>
+                    <div class="in-field_block">
+                        <label for="plan_location">Where are you going?</label>
+                        <input type="text" id="plan_location" name="plan_location" class="textbox"/>
+                    </div>
+                </td>
+                <td>
+                    <div class="in-field_block">
+                        <label for="plan_category">What are you doing?</label>
+                        <input type="text" id="plan_category" name="plan_category" class="textbox"/>
+                    </div>
+                </td>
+            </tr>
+        </table>
 
         <input id="plan_location_id" name="plan_location_id" type="hidden"/>
         <input id="plan_category_id" name="plan_category_id" type="hidden"/>
