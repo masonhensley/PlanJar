7c0f0deb0039a024e3d81d8b49522051bbcba0a9
Parker Bossier <parker@planjar.com>
2011-06-12 13:42:29 -0500

    ya

--- application/assets/js/home_functions.js
+++ application/assets/js/home_functions.js
@@ -138,7 +138,7 @@ $(function() {
                         var options = {
                             api_key: 'JG0aox7ooCrWUcQHHWsYNd4vq0nYTxvALaUk0ziSgFwwjl6DKvMqghXj3pnYaPGD',
                             limit: place_limit,
-                            filters: my_filters
+                            filters: my_filters.serialize
                         }
                         
                         console.log($.param(options));
