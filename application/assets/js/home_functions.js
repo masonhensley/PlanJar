e528586bfdef64d0805f89e47cf6449cb8eb80ac
Parker Bossier <parker@planjar.com>
2011-06-11 21:57:01 -0500

    ya

--- application/assets/js/home_functions.js
+++ application/assets/js/home_functions.js
@@ -144,7 +144,7 @@ $(function() {
                     $.ajax({
                         url: 'http://api.factual.com/v2/tables/s4OOB4/read',
                         data: options,
-                        dataType: 'json',
+                        dataType: 'jsonp',
                         success : function(data) {
                             console.log(data);
                         }
