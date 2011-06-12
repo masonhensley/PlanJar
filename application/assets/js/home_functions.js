696a5806d0a845a988e5d3d822d89e0046c01ae3
Parker Bossier <parker@planjar.com>
2011-06-11 21:00:41 -0500

    make a plan, bray

--- application/assets/js/home_functions.js
+++ application/assets/js/home_functions.js
@@ -119,16 +119,8 @@ $(function() {
                     if (place_limit > 0) {
                         // If additional places are required, fetch places from Factual. Pick fields needed
                         // by the autocomplete from the resulting JSON and add them to response_json array.
-                        var my_filters = {
-                                "$loc":{
-                                    "$within":{
-                                        "$center":[[myLatitude, myLongitude],5000]
-                                    }
-                                },
-                               "name": {
-                                   '$search': request.term
-                               }
-                        };
+                        var my_filters = '{"$loc":{"$within":{"$center":[[' + myLatitude + ', ' + myLongitude + 
+                        '],5000]}},"name": {"$search":"' + request.term + '"}}';
                                                 
                         var options = {
                             api_key: 'JG0aox7ooCrWUcQHHWsYNd4vq0nYTxvALaUk0ziSgFwwjl6DKvMqghXj3pnYaPGD',
