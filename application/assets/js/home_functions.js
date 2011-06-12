3ca38f2844789fd97a0ad1bac3955edb18bfef17
Parker Bossier <parker@planjar.com>
2011-06-12 14:23:14 -0500

    ya

--- application/assets/js/home_functions.js
+++ application/assets/js/home_functions.js
@@ -139,7 +139,7 @@ $(function() {
                         }
                         
                         $.ajax({
-                            url: 'http://api.factual.com/v2/tables/s4OOB4/read',
+                            url: 'http://api.factual.com/v2/tables/s4OOB4/read?{[}]',
                             data: options,
                             dataType: 'jsonp',
                             success : function(data) {
