5838731571fcb4c60041332237c045230e0aa254
Parker Bossier <parker@planjar.com>
2011-06-18 12:53:01 -0500

    groups pane

--- application/assets/js/groups_panel_functions.js
+++ application/assets/js/groups_panel_functions.js
@@ -1,12 +1,11 @@
 $(function() {
-    // Set up the Selectable instance with default options.
-    create_selectables();
+    // Make the groups selectable.
+    make_selectable();
 });
 
-function create_selectables() {
-    // Make the list tiems selectable.
+function make_selectable() {
     $('div.group_selectable_wrapper li').click(function() {
-        // Toggle the selection.
+        // Make the list tiems togglable.
         if ($(this).hasClass('group_selected')) {
             $(this).removeClass('group_selected');
         } else {
@@ -20,7 +19,7 @@ function create_selectables() {
         });
         
         // Call the callback function.
-        on_groups_change();
+        on_groups_change(selected_groups);
     });
     
     // Initialize the clear all and select all button actions.
@@ -35,10 +34,5 @@ function create_selectables() {
 }
 
 // Callback function
-function on_groups_change() {
-    // Initialize the group list.
-    var selected_groups = ([]);
-    $('div.group_selectable_wrapper li.group_selected').each(function (index, element) {
-        selected_groups.push($(element).attr('group_id'));
-    });
+function on_groups_change(selected_groups) {
 }
