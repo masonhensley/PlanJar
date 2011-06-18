c25d3c5bb083d361e961f02cb88030d35c15f0ac
Parker Bossier <parker@planjar.com>
2011-06-17 21:44:42 -0500

    groups pane

--- application/assets/js/groups_panel_functions.js
+++ application/assets/js/groups_panel_functions.js
@@ -12,85 +12,64 @@ function destroy_selectables() {
     $('#followed_groups').selectable('destroy');
 }
 
-function create_selectable(ul_element, mode) {
-    if (mode == 'standard') {
-        // Set up the Selectable instance with default options (the shown
-        // options are to keep the last selected item from disappearing.
-        $(ul_element).selectable({
-            selected: function(event, ui) {
-                if ($(ui.selected).hasClass('group_label')) {
-                    // Dissalow group label divs from being selected.
-                    $(ui.selected).removeClass('ui-selected');
-                } else {
-                    $(ui.selected).addClass('my-selected');
-                }
-            },
-            unselected: function(event, ui) {
-                $(ui.unselected).removeClass('my-selected');
-            },
-            // Disallow group label divs from being selected.
-            selecting: function(event, ui) {
-                if ($(ui.selecting).hasClass('group_label')) {
-                    $(ui.selecting).removeClass('ui-selecting');
-                }
-            }
-        });
-    } else {
-        // The following instantiation was pulled from
-        // http://forum.jquery.com/topic/ui-selectable-allow-select-multiple-without-lasso
-        $(ul_element).selectable({
-            selected: function (event, ui) {
-                var e= $(ui.selected);
-                if (e.hasClass('group_label')) {
-                    // Dissalow group label divs from being selected.
-                    e.removeClass('ui-selected');
-                } else {
-                    if (e.hasClass('my-selected')) {
-                        e.removeClass('my-selected');
-                        e.removeClass('ui-selected');
-                    } else {
-                        e.addClass('my-selected');
-                        e.addClass('ui-selected');
-                    }
-                }
-            },
-            unselected: function (event, ui) {
-                var e= $(ui.unselected);
-                if (e.hasClass('my-selected')) {
-                    e.addClass('my-selected');
-                    e.addClass('ui-selected');
-                } else {
-                    e.removeClass('ui-selected');
-                    e.removeClass('my-selected');
-                }
-            },
-            // Disallow group label divs from being selected.
-            selecting: function(event, ui) {
-                if ($(ui.selecting).hasClass('group_label')) {
-                    $(ui.selecting).removeClass('ui-selecting');
-                }
-            }
-        });
-    }
+function create_selectable(ul_element) {
+    $(ul_element + ' ul').click(function() {
+        alert(this);
+    });
+//        // Set up the Selectable instance with default options (the shown
+//        // options are to keep the last selected item from disappearing.
+//        $(ul_element).selectable({
+//            selected: function(event, ui) {
+//                if ($(ui.selected).hasClass('group_label')) {
+//                    // Dissalow group label divs from being selected.
+//                    $(ui.selected).removeClass('ui-selected');
+//                } else {
+//                    $(ui.selected).addClass('my-selected');
+//                }
+//            },
+//            unselected: function(event, ui) {
+//                $(ui.unselected).removeClass('my-selected');
+//            },
+//            // Disallow group label divs from being selected.
+//            selecting: function(event, ui) {
+//                if ($(ui.selecting).hasClass('group_label')) {
+//                    $(ui.selecting).removeClass('ui-selecting');
+//                }
+//            }
+//        });
+//        // The following instantiation was pulled from
+//        // http://forum.jquery.com/topic/ui-selectable-allow-select-multiple-without-lasso
+//        $(ul_element).selectable({
+//            selected: function (event, ui) {
+//                var e= $(ui.selected);
+//                if (e.hasClass('group_label')) {
+//                    // Dissalow group label divs from being selected.
+//                    e.removeClass('ui-selected');
+//                } else {
+//                    if (e.hasClass('my-selected')) {
+//                        e.removeClass('my-selected');
+//                        e.removeClass('ui-selected');
+//                    } else {
+//                        e.addClass('my-selected');
+//                        e.addClass('ui-selected');
+//                    }
+//                }
+//            },
+//            unselected: function (event, ui) {
+//                var e= $(ui.unselected);
+//                if (e.hasClass('my-selected')) {
+//                    e.addClass('my-selected');
+//                    e.addClass('ui-selected');
+//                } else {
+//                    e.removeClass('ui-selected');
+//                    e.removeClass('my-selected');
+//                }
+//            },
+//        });
 }
 
 function create_selectables(mode) {
-    create_selectable('#friends_group', mode);
-    create_selectable('#joined_groups', mode);
-    create_selectable('#followed_groups', mode);
-}
-
-
-// Should be called when #sel_one or #sel_mult
-// Set up the Selectable instance with "standard" options or toggle options.
-function toggle_group_select() {
-    if ($('#sel_one').attr('checked') == 'checked') {
-        // Set up the Selectable instances with standard options.
-        destroy_selectables();
-        create_selectables('standard');
-    } else {
-        // Set up the Selectable instances with custom options.
-        destroy_selectables();
-        create_selectables('custom');
-    }
+    create_selectable('#friends_group');
+    create_selectable('#joined_groups');
+    create_selectable('#followed_groups');
 }
