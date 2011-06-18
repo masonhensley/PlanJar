43f66e6a0d479dc675e19791c06d837ebf2e55c0
Parker Bossier <parker@planjar.com>
2011-06-18 00:18:37 -0500

    groups pane

--- application/assets/js/groups_panel_functions.js
+++ application/assets/js/groups_panel_functions.js
@@ -10,65 +10,17 @@ function destroy_selectables() {
 }
 
 function create_selectable(ul_element) {
+    // Make the list tiems selectable.
     $(ul_element + ' li').click(function() {
         console.log(this);
         if ($(this).hasClass('group_selected')) {
-            $(this).removeClass('group_selected');
+            if (!$(ul_element + ' li').hasClass('group_selected')) {
+                $(this).removeClass('group_selected');
+            }
         } else {
             $(this).addClass('group_selected');
         }
     });
-    
-//        // Set up the Selectable instance with default options (the shown
-//        // options are to keep the last selected item from disappearing.
-//        $(ul_element).selectable({
-//            selected: function(event, ui) {
-//                if ($(ui.selected).hasClass('group_label')) {
-//                    // Dissalow group label divs from being selected.
-//                    $(ui.selected).removeClass('ui-selected');
-//                } else {
-//                    $(ui.selected).addClass('my-selected');
-//                }
-//            },
-//            unselected: function(event, ui) {
-//                $(ui.unselected).removeClass('my-selected');
-//            },
-//            // Disallow group label divs from being selected.
-//            selecting: function(event, ui) {
-//                if ($(ui.selecting).hasClass('group_label')) {
-//                    $(ui.selecting).removeClass('ui-selecting');
-//                }
-//            }
-//        });
-//        // The following instantiation was pulled from
-//        // http://forum.jquery.com/topic/ui-selectable-allow-select-multiple-without-lasso
-//        $(ul_element).selectable({
-//            selected: function (event, ui) {
-//                var e= $(ui.selected);
-//                if (e.hasClass('group_label')) {
-//                    // Dissalow group label divs from being selected.
-//                    e.removeClass('ui-selected');
-//                } else {
-//                    if (e.hasClass('my-selected')) {
-//                        e.removeClass('my-selected');
-//                        e.removeClass('ui-selected');
-//                    } else {
-//                        e.addClass('my-selected');
-//                        e.addClass('ui-selected');
-//                    }
-//                }
-//            },
-//            unselected: function (event, ui) {
-//                var e= $(ui.unselected);
-//                if (e.hasClass('my-selected')) {
-//                    e.addClass('my-selected');
-//                    e.addClass('ui-selected');
-//                } else {
-//                    e.removeClass('ui-selected');
-//                    e.removeClass('my-selected');
-//                }
-//            },
-//        });
 }
 
 function create_selectables(mode) {
