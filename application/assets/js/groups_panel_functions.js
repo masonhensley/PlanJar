dbab6f2a88b1efea84bcccd17c7f5aa0944f6c99
Parker Bossier <parker@planjar.com>
2011-06-20 16:08:58 -0500

    make a plan, bray

--- application/assets/js/groups_panel_functions.js
+++ application/assets/js/groups_panel_functions.js
@@ -12,14 +12,8 @@ function make_groups_selectable() {
             $(this).addClass('group_selected');
         }
         
-        // Initialize the group list.
-        var selected_groups = ([]);
-        $('div.group_selectable_wrapper li.group_selected').each(function (index, element) {
-            selected_groups.push($(element).attr('group_id'));
-        });
-        
         // Call the callback function.
-        on_groups_change(selected_groups);
+        on_groups_change();
     });
     
     // Initialize the clear all and select all button actions.
@@ -40,4 +34,12 @@ function on_groups_change(selected_groups) {
         $("#map_data_tabs").tabs('select', '#data_tab');
     }
     get_group_day_data();
+}
+
+// Returns a list of selected groups.
+function selected_groups() {
+    var return_list = ([]);
+    $('div.group_selectable_wrapper li.group_selected').each(function (index, element) {
+        return_list.push($(element).attr('group_id'));
+    });
 }
