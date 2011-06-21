fe071291bcee43e062e51055c53e943c3ac9aa14
Parker Bossier <parker@planjar.com>
2011-06-21 14:53:09 -0500

    ye

--- application/assets/js/all_plans_functions.js
+++ application/assets/js/all_plans_functions.js
@@ -8,6 +8,7 @@ function populate_visible_plans_panel() {
         'selected_groups': get_selected_groups(),
         'selected_day': get_selected_day()
     }, function (data) {
+        alert(get_selected_groups());
        $('#visible_plans_panel').html(data); 
     });
 }
