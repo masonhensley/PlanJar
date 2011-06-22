742003b0b636facf1a9b0be091baf830a3f8380e
Parker Bossier <parker@planjar.com>
2011-06-22 18:32:09 -0500

    ye

--- application/assets/js/visible_plans_functions.js
+++ application/assets/js/visible_plans_functions.js
@@ -4,7 +4,7 @@ $(function() {
 
 // Populates the list of plans every time a weekday or group is selected
 function populate_visible_plans_panel() {
-    alert(get_selected_groups());
+    alert('me:' + get_selected_groups());
     $.get('/home/get_visible_plans', {
         'selected_groups': get_selected_groups(),
         'selected_day': get_selected_day()
