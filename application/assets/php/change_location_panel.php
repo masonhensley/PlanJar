ac971490c5f54b27053a0ca42b6ba2a57e61a679
Parker Bossier <parker@planjar.com>
2011-06-23 16:03:01 -0500

    ye

--- application/assets/php/change_location_panel.php
+++ application/assets/php/change_location_panel.php
@@ -3,5 +3,5 @@
 <div id="change_location_content" title="Change your location.">
     <p>Your approximate current location is below.</p>
     
-    <div id="change_location_map" style="width:100%; height:300px"></div>
+    <div id="change_location_map" style="width:80%; height:300px"></div>
 </div>
