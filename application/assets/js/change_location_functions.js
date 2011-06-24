1be14da5ffa1055863996708ef64b476afb290d5
Parker Bossier <parker@planjar.com>
2011-06-23 19:48:20 -0500

    boje

--- application/assets/js/change_location_functions.js
+++ application/assets/js/change_location_functions.js
@@ -8,6 +8,7 @@ function initialize_change_location_modal() {
         height: 500,
         resizable: false,
         show: function (event, ui) {
+            alert('show');
             $(this).show('clip', function() {
                 alert('complete');
                 change_location_map.checkResize();
