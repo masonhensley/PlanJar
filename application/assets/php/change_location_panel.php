8ae216ff092e2163985cb112be0c527ebb5a0ad1
Parker Bossier <parker@planjar.com>
2011-06-24 18:27:30 -0500

    boje

--- application/assets/php/change_location_panel.php
+++ application/assets/php/change_location_panel.php
@@ -12,6 +12,7 @@
         <input type="text" id="change_location_search" class="textbox"/>
     </div>
 
+    <br/>
     <div id="change_location_map" style="width:50%; height:50%"></div>
     <br/>
     <input type="button" id="close_change_location" value="Close"/>
