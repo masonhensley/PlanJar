c25d8dd29b14a75f0fe95c0b5c00792ed2bda9c8
Parker Bossier <parker@planjar.com>
2011-06-25 17:24:25 -0500

    boje

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -177,7 +177,7 @@ class Home extends CI_Controller
             'user_id' => $user->id,
             'date' => $date->format('Y-m-d'),
             'time_of_day' => $this->input->get('plan_time_group'),
-            'category_id' => $this->input->get('plan_category')
+            'category' => $this->input->get('plan_category')
         );
 
         // Add the place to the database if a Factual place was selected.
