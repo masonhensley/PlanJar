537abfaf7047e5822ec21e1bcc6a8b8a57e8cd74
Parker Bossier <parker@planjar.com>
2011-06-27 18:50:37 -0500

    boje

--- application/assets/js/day_tabs.js
+++ application/assets/js/day_tabs.js
@@ -29,7 +29,7 @@ function on_day_change() {
     if ($("#map_data_tabs .ui-state-active a").attr('href') != '#data_tab') {
         $("#map_data_tabs").tabs('select', '#data_tab');
     }
-    //get_group_day_data();
+    get_group_day_data();
     load_visible_plans()
 }
 
