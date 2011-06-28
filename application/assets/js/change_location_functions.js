d2101a6d660777c802ad369cf45559d7e9700776
Parker Bossier <parker@planjar.com>
2011-06-27 19:04:52 -0500

    boje

--- application/assets/js/change_location_functions.js
+++ application/assets/js/change_location_functions.js
@@ -41,23 +41,31 @@ function initialize_change_location_panel() {
             var places_service = new google.maps.places.PlacesService(map);
             places_service.search(places_request, function (results, status) {
                 if (status == google.maps.places.PlacesServiceStatus.OK) {
+                    // Clear all markers and add the new ones.
                     clear_change_location_markers();
                     $.map(results, function (entry) {
+                        // Create the marker.
                         var temp_marker = new google.maps.Marker({
                             map: map,
                             position: new google.maps.LatLng(entry.geometry.location.Ha, entry.geometry.location.Ia),
                             title: entry.name
                         });
+                        
+                        // Assign the click event.
                         google.maps.event.addListener(temp_marker, 'click', function() {
                             alert('here');
                         });
+                        
+                        // Add the marker to the marker list.
                         change_location_marker_array.push(temp_marker);
                     });
+                    console.log('after map');
                     console.log(change_location_marker_array);
                 }
             });
         },
         success: function (data) {
+            console.log('success');
             console.log(data);
         }
     });
@@ -90,21 +98,4 @@ function clear_change_location_markers () {
     $.map(change_location_marker_array, function (entry) {
         entry.setMap(null);
     });
-    console.log('before delete');
-    console.log(change_location_marker_array);
-    change_location_marker_array = ([]);
-    console.log('after delete');
-    console.log(change_location_marker_array);
-}
-
-function add_marker(data, marker_array, map) {
-    var new_marker = new google.maps.Marker({
-        map: map,
-        position: new google.maps.LatLng({
-            lat: data.geometry.location.Ha,
-            lng: data.geometry.location.Ia
-        }),
-        title: data.name
-    })
-    marker_array.push(new_marker);
 }
