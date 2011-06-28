00b1447479cf6577765cada320a3f9afd709f4ae
Parker Bossier <parker@planjar.com>
2011-06-28 13:58:19 -0500

    boje

--- application/assets/js/change_location_functions.js
+++ application/assets/js/change_location_functions.js
@@ -35,8 +35,8 @@ function initialize_change_location_panel() {
             var places_request = {
                 location: new google.maps.LatLng(myLatitude, myLongitude),
                 radius: 10000,
-                name: request.term
-//                type: ['locality']
+                name: request.term,
+                type: ['locality']
             };
             
             var places_service = new google.maps.places.PlacesService(map);
