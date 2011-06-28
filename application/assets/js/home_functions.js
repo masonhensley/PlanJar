5d42bdb517fe5a8c9e21958f14f3779c076777d2
Parker Bossier <parker@planjar.com>
2011-06-28 18:09:06 -0500

    boje

--- application/assets/js/home_functions.js
+++ application/assets/js/home_functions.js
@@ -126,7 +126,7 @@ function update_current_city_name() {
     geocoder.geocode(request, function (result, status) {
         console.log(result);
         if (status == google.maps.GeocoderStatus.OK) {
-            result = result.address_components;
+            result = result[0].address_components;
             console.log(result[0][2].long_name + ', ' + result[0][4].short_name);
         }
     });
