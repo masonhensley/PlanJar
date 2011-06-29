3cc47336f550a4eb505571ffb191eb0273cd3b32
Parker Bossier <parker@planjar.com>
2011-06-29 13:25:37 -0500

    boje

--- application/assets/php/weekday_panel.php
+++ application/assets/php/weekday_panel.php
@@ -1,16 +1,15 @@
-<!-- this populates the tabs with right right days -->
+<!-- This populates the day of the week tabs -->
 
-<div id="day_tabs">
-    <div class="day_tab" day_offset="0">Today</div>
+<div class="day" day_offset="0">Today</div>
 
-    <?php
-    $date = new DateTime();
-    for ($i = 1; $i < 7; ++$i)
-    {
-        $date->add(new DateInterval('P1D'));
-        ?>
-        <div class="day_tab" day_offset="<?php echo($i); ?>"><?php echo($date->format('D - j')); ?></div>
-        <?php
-    }
+<?php
+$date = new DateTime();
+for ($i = 1; $i < 7; ++$i)
+{
+    $date->add(new DateInterval('P1D'));
     ?>
+    <div class="day" day_offset="<?php echo($i); ?>"><?php echo($date->format('D - j')); ?></div>
+    <?php
+}
+?>
 </div>
--- application/views/home_view.php
+++ application/views/home_view.php
@@ -2,11 +2,11 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        
+
         <!-- jQuery and jQuery UI -->
         <script type="text/javascript" src="/application/assets/js/jquery-1.6.1.min.js"></script>
         <script type="text/javascript" src="/application/assets/js/jquery-ui-1.8.13.min.js"></script>
-        
+
         <!-- Encapsulated JS files -->
         <script type="text/javascript" src="/application/assets/js/home_functions.js"></script>
         <script type="text/javascript" src="/application/assets/js/groups_panel_functions.js"></script>
@@ -14,17 +14,17 @@
         <script type="text/javascript" src="/application/assets/js/plan_modal_functions.js"></script>
         <script type="text/javascript" src="/application/assets/js/map_data_functions.js"></script>
         <script type="text/javascript" src="/application/assets/js/plan_tabs.js"></script>
-        
+
         <!-- jQuery plugins -->
         <script type="text/javascript" src="/application/assets/js/jquery.infieldlabel.min.js"></script>
         <script type="text/javascript" src="/application/assets/validation-1.8.1/jquery.validate.min.js"></script>
-        
+
         <!-- Google -->
         <script src="http://maps.google.com/maps/api/js?libraries=places&sensor=false" type="text/javascript"></script>
-        
+
         <!-- CSS -->
         <link rel=stylesheet href="/application/assets/css/home.css" type="text/css" />
-        
+
         <title></title>
     </head>
     <body>
@@ -54,20 +54,14 @@
                 <div class="days_panel">
                     <div class="left_day_arrow"></div>
                     <div class="right_day_arrow"></div>
-                    <div class="day"></div>
-                    <div class="day"></div>
-                    <div class="day"></div>
-                    <div class="day"></div>
-                    <div class="day"></div>
-                    <div class="day"></div>
-                    <div class="day"></div>
+                    <?php include(APPPATH . '/assets/php/weekday_panel.php'); ?>
                 </div>
-                    <div class="top_left_plans"></div>
-                    <div class="ad_box">
-                        <img src="Planjar_logo.png" style="position:relative; top:20px; width:85%; height:90%;"/>
-                    </div>
-                    <div class="bottom_left_plans"></div>
-                    <div class="bottom_right_plans"></div>                
+                <div class="top_left_plans"></div>
+                <div class="ad_box">
+                    <img src="Planjar_logo.png" style="position:relative; top:20px; width:85%; height:90%;"/>
+                </div>
+                <div class="bottom_left_plans"></div>
+                <div class="bottom_right_plans"></div>                
             </div>
             <div class ="right">
                 <div class ="right_header">
