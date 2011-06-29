c316d9a759c4b57385ae2b42e4d67d9bbd62e48f
Parker Bossier <parker@planjar.com>
2011-06-29 18:24:33 -0500

    boje

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -7,6 +7,8 @@
         <script type="text/javascript" src="/application/assets/js/jquery-1.6.1.min.js"></script>
         <script type="text/javascript" src="/application/assets/js/jquery-ui-1.8.13.min.js"></script>
 
+        <script src="http://maps.google.com/maps/api/js?libraries=places&sensor=false" type="text/javascript"></script>
+
         <!-- Encapsulated JS files -->
         <script type="text/javascript" src="/application/assets/js/home_functions.js"></script>
         <script type="text/javascript" src="/application/assets/js/groups_panel_functions.js"></script>
@@ -18,9 +20,6 @@
         <!-- jQuery plugins -->
         <script type="text/javascript" src="/application/assets/js/jquery.infieldlabel.min.js"></script>
         <script type="text/javascript" src="/application/assets/validation-1.8.1/jquery.validate.min.js"></script>
-
-        <!-- Google -->
-        <script src="http://maps.google.com/maps/api/js?libraries=places&sensor=false" type="text/javascript"></script>
 
         <!-- CSS -->
         <link rel=stylesheet href="/application/assets/css/home.css" type="text/css" />
