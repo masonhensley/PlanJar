95fa77a1ba3926c86454455641d866b2ef75c511
Parker Bossier <parker@planjar.com>
2011-06-29 17:09:58 -0500

    boje

--- application/assets/js/map_data_functions.js
+++ application/assets/js/map_data_functions.js
@@ -24,6 +24,9 @@ function initialize_map_data_tabs() {
 
 // Get the data based on groups and the day from the server.
 function get_group_day_data () {
+    console.log(get_selected_groups());
+    console.log(get_selected_day);
+    
     $.get('/home/get_group_day_data', {
         'selected_groups': get_selected_groups(),
         'selected_day': get_selected_day()
