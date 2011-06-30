c4b0a20ae588f9677e23708afc2285ef08d56cd1
Parker Bossier <parker@planjar.com>
2011-06-30 15:38:21 -0500

    boje

--- application/assets/php/change_location_panel.php
+++ application/assets/php/change_location_panel.php
@@ -1,12 +1,16 @@
 <div class="change_location_panel">
-    <center>
+    <div style="float: left; width: 50%">
         <p>Your approximate location is shown. Change your location by searching for your city below and clicking on a marker.</p>
-    </center>
-
-    <div class="in-field_block">
-        <label for="change_location_search">Enter a new location.</label>
-        <input type="text" id="change_location_search" class="textbox"/>
     </div>
 
-    <input type="button" id="close_change_location" value="Close"/>
+    <div style="float: left;">
+        <div class="in-field_block">
+            <label for="change_location_search">Enter a new location.</label>
+            <input type="text" id="change_location_search" class="textbox"/>
+        </div>
+    </div>
+
+    <div style="float: right">
+        <input type="button" id="close_change_location" value="Close"/>
+    </div>
 </div>
