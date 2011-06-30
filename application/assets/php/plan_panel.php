3308fd0bb300b0bc6ee51b18afadc3dc021e5a4f
Parker Bossier <parker@planjar.com>
2011-06-30 14:51:24 -0500

    boje

--- application/assets/php/plan_panel.php
+++ application/assets/php/plan_panel.php
@@ -1,55 +1,55 @@
 <div id="create_plan_content">
     <div class="draggable_title_bar">Make a plan</div>
 
-    <form id="make_plan">
-        <table>
-            <tr>
-                <td>
-                    <div class="in-field_block">
-                        <label for="plan_location">Where are you going?</label>
-                        <input type="text" id="plan_location" name="plan_location" class="textbox"/>
-                    </div>
+    <center>
+        <form id="make_plan">
+            <table>
+                <tr>
+                    <td>
+                        <div class="in-field_block">
+                            <label for="plan_location">Where are you going?</label>
+                            <input type="text" id="plan_location" name="plan_location" class="textbox"/>
+                        </div>
+                    </td>
+                    <td>
+                        <div class="in-field_block">
+                            <label for="plan_category">What are you doing?</label>
+                            <input type="text" id="plan_category" name="plan_category" class="textbox"/>
+                        </div>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td colspan="2">
+                <center>
+                    When are you going?
+                </center>
                 </td>
-                <td>
-                    <div class="in-field_block">
-                        <label for="plan_category">What are you doing?</label>
-                        <input type="text" id="plan_category" name="plan_category" class="textbox"/>
+                </tr>
+
+                <tr>
+                    <td colspan="2">
+                <center>
+                    <div id="plan_time" class="radio">
+                        <label for="plan_morning">morning</label>
+                        <input type="radio" id="plan_morning" value="morning" name="plan_time_group" />
+
+                        <label for="plan_afternoon">afternoon</label>
+                        <input type="radio" id="plan_afternoon" value="afternoon" name="plan_time_group" />
+
+                        <label for="plan_night">night</label>
+                        <input type="radio" id="plan_night" value="night" name="plan_time_group" />
+
+                        <label for="plan_late_night">late night</label>
+                        <input type="radio" id="plan_late_night" value="late_night" name="plan_time_group" />
                     </div>
+                </center>
                 </td>
-            </tr>
+                </tr>
 
-            <tr>
-                <td colspan="2">
-            <center>
-                When are you going?
-            </center>
-            </td>
-            </tr>
-
-            <tr>
-                <td colspan="2">
-            <center>
-                <div id="plan_time" class="radio">
-                    <label for="plan_morning">morning</label>
-                    <input type="radio" id="plan_morning" value="morning" name="plan_time_group" />
-
-                    <label for="plan_afternoon">afternoon</label>
-                    <input type="radio" id="plan_afternoon" value="afternoon" name="plan_time_group" />
-
-                    <label for="plan_night">night</label>
-                    <input type="radio" id="plan_night" value="night" name="plan_time_group" />
-
-                    <label for="plan_late_night">late night</label>
-                    <input type="radio" id="plan_late_night" value="late_night" name="plan_time_group" />
-                </div>
-            </center>
-            </td>
-            </tr>
-
-            <tr>
-                <td colspan="2">
-                    <div id="plan_day" class="radio">
-                        <center>
+                <tr>
+                    <td colspan="2">
+                        <div id="plan_day" class="radio">
                             <label for="plan_day_0">Today - <?php echo(date('j')); ?></label>
                             <input type="radio"  id="plan_day_0" value="0" name="plan_day_group" />
 
@@ -64,22 +64,22 @@
                                 <?php
                             }
                             ?>
-                        </center>
-                    </div>
-                </td>
-            </tr>
-        </table>
+                        </div>
+                    </td>
+                </tr>
+            </table>
 
-        <input type="submit" value="Go"/>
-        <input type="button" id="cancel_plan" value="Cancel"/>
+            <input type="submit" value="Go"/>
+            <input type="button" id="cancel_plan" value="Cancel"/>
 
-        <input id="plan_location_id" name="plan_location_id" type="hidden"/>
-        <input id="plan_location_name" type="hidden"/>
-        <input id="plan_category_id" name="plan_category_id" type="hidden"/>
-        <input id="plan_category_name" type="hidden"/>
-        <input id="new_place_name" name="new_place_name" type="hidden"/>
-        <input id="new_place_category" name="new_place_category" type="hidden"/>
-        <input id="new_place_latitude" name="new_place_latitude" type="hidden"/>
-        <input id="new_place_longitude" name="new_place_longitude" type="hidden"/>
-    </form>
+            <input id="plan_location_id" name="plan_location_id" type="hidden"/>
+            <input id="plan_location_name" type="hidden"/>
+            <input id="plan_category_id" name="plan_category_id" type="hidden"/>
+            <input id="plan_category_name" type="hidden"/>
+            <input id="new_place_name" name="new_place_name" type="hidden"/>
+            <input id="new_place_category" name="new_place_category" type="hidden"/>
+            <input id="new_place_latitude" name="new_place_latitude" type="hidden"/>
+            <input id="new_place_longitude" name="new_place_longitude" type="hidden"/>
+        </form>
+    </center>
 </div>
