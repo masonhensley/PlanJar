b9ec17690f899c351ef20e1b5a7ea97a6e471e7a
Parker Bossier <parker@planjar.com>
2011-07-04 22:44:00 -0500

    boje

--- application/models/load_groups.php
+++ application/models/load_groups.php
@@ -12,7 +12,7 @@ class Load_groups extends CI_Model
     function joined_groups()
     {
         $user = $this->ion_auth->get_user();
-        $query_string = "SELECT group_relationships.group_id, groups.name FROM group_relationships LEFT JOIN groups ON group_relationships.group_id = group.id " .
+        $query_string = "SELECT group_relationships.group_id, groups.name FROM group_relationships LEFT JOIN groups ON group_relationships.group_id = groups.id " .
                 "WHERE group_relationships.user_joined_id = ?";
         $query = $this->db->query($query_string, array($user->id));
 
@@ -29,7 +29,7 @@ class Load_groups extends CI_Model
     function followed_groups()
     {
         $user = $this->ion_auth->get_user();
-        $query_string = "SELECT group_relationships.group_id, groups.name FROM group_relationships LEFT JOIN groups ON group_relationships.group_id = group.id " .
+        $query_string = "SELECT group_relationships.group_id, groups.name FROM group_relationships LEFT JOIN groups ON group_relationships.group_id = groups.id " .
                 "WHERE group_relationships.user_following_id = ?";
         $query = $this->db->query($query_string, array($user->id));
 
