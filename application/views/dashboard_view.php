4e1d1816e4aa121505b1c511b357ff88047f867d
Parker Bossier <parker@planjar.com>
2011-07-06 19:15:40 -0500

    boje

--- application/views/dashboard_view.php
+++ application/views/dashboard_view.php
@@ -27,14 +27,15 @@
         </div>
         <div class="container">
             <div class="tab_container">
-                <div class="tab" assoc_div="#friends_content">Friends</div>
+                <div class="tab" assoc_div="#following_content">Following</div>
+                <div class="tab" assoc_div="#followers_content">Followers</div>
                 <div class="tab" assoc_div="#groups_content">Groups</div>
                 <div class="tab" assoc_div="#plans_content">Plans</div>
                 <div class="tab" assoc_div="#profile_content">Profile</div>
                 <div class="tab" assoc_div="#settings_content">Settings</div>
             </div>
 
-            <div id="friends_content" class="page_content">
+            <div id="following_content" class="page_content">
                 <div class="left">
                     <div class="radio">
                         <center>
@@ -60,6 +61,34 @@
                 <div class="right">
                 </div>
             </div>
+            
+            <div id="followers_content" class="page_content">
+                <div class="left">
+                    <div class="radio">
+                        <center>
+                            <label for="friends_following">Following</label>
+                            <input type="radio" id="friends_following" value="following" name="following_follower_group"/>
+                            <label for="friends_followers">Followers</label>
+                            <input type="radio" id="friends_followers" value="followers" name="following_follower_group"/>
+                        </center>
+                    </div>
+
+                    <div id="find_friends">
+                        <div class="in-field_block">
+                            <label for="friend_search">Search for people to follow.</label>
+                            <input type="text" class="textbox" id="friend_search" />
+                        </div>
+                    </div>
+                    <br/>
+                    <hr/>
+
+                    <div class="friends_list">
+                    </div>
+                </div>
+                <div class="right">
+                </div>
+            </div>
+            
             <div id="groups_content" class="page_content"></div>
             <div id="plans_content" class="page_content"></div>
             <div id="profile_content" class="page_content"></div>
