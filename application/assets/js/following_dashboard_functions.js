15bb35744f7847641f9a4975a77707e3586c2d9e
Wells Johnston <wellsjohnston@gmail.com>
2011-07-12 14:24:13 -0500

    Suggested friends

--- application/assets/js/following_dashboard_functions.js
+++ application/assets/js/following_dashboard_functions.js
@@ -50,7 +50,7 @@ function initialize_follow_search() {
 }
 
 // This sets up the suggested friends list
-initialize_suggested_friends()
+function initialize_suggested_friends()
 {
     $('.suggested_friends').click(function(){
         if($(this).hasClass('suggested_active'))
@@ -66,46 +66,3 @@ initialize_suggested_friends()
         }
     });
 }
-
-
-//function initialize_friends_list() {
-//    // Button click events
-//    $('#friends_following').click(function() {
-//        $('#friends_content .right').html('');
-//        $('#find_friends').css('display', 'block');
-//        populate_following();
-//    });
-//    $('#friends_followers').click(function() {
-//        $('#find_friends').css('display', 'none');
-//        populate_followers();
-//    });
-//    
-//    $('.in-field_block label').inFieldLabels();
-//}
-//
-//function populate_following() {
-//    $.get('/dashboard/get_following', function (data) {
-//        $('.friends_list').html(data);
-//    });
-//}
-//
-//function populate_followers() {
-//    $.get('/dashboard/get_followers', function (data) {
-//        $('.friends_list').html(data);
-//        make_followers_selectable();
-//    });
-//}
-//
-//function make_followers_selectable() {
-//    $('.follower_entry').click(function() {
-//        $('.follower_entry.selected_follower').removeClass('selected_follower');
-//        $(this).addClass('selected_follower');
-//        $.get('/dashboard/get_follower_details', {
-//            follower_id: $(this).attr('follower_id')
-//        }, function (data) {
-//            $('#friends_content .right').html(data);
-//        });
-//    });
-//    
-//    $('.follower_entry:first').click();
-//}
