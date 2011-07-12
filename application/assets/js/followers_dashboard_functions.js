c32187c299a5a64dd1e9997cf3381d66cb691b4d
Parker Bossier <parker@planjar.com>
2011-07-12 13:51:25 -0500

    boje

--- application/assets/js/followers_dashboard_functions.js
+++ application/assets/js/followers_dashboard_functions.js
@@ -1,22 +1,40 @@
 $(function() {
+    populate_followers_list();
     initialize_followers_list();
 });
     
 function initialize_followers_list() {
     $.get('/dashboard/get_followers', function (data) {
         $('#followers_list').html(data);
-        make_followers_selectable();
     });
 }
 
-function make_followers_selectable() {
-    $('#followers_list .user_follow_entry').click(function() {
-        $('.user_follow_entry.selected_follower').removeClass('selected_follower');
-        $(this).addClass('selected_follower');
-        $.get('/dashboard/get_follower_details', {
-            follower_id: $(this).attr('user_id')
-        }, function (data) {
-            $('#followers_content .right').html(data);
+function populate_followers_list() {
+    $.get('/dashboard/get_followers', function (data) {
+        $('#followers_list').html(data);
+        
+        // Click handler.
+        $('#followers_list .add_following').click(function () {
+            if ($(this).text() == '+ Follow') {
+                $(this).text('+ You sure?');
+            } else {
+                $.get('/dashboard/add_following', {
+                    following_id: $(this).parent().attr('user_id')
+                }, function (data) {
+                    populate_followers_list();
+                });
+            }
+        });
+        
+        // Make followers selectable
+        $('#followers_list .user_follow_entry').click(function() {
+            $('.user_follow_entry.selected_follower').removeClass('selected_follower');
+            $(this).addClass('selected_follower');
+            $.get('/dashboard/get_follower_details', {
+                follower_id: $(this).attr('user_id')
+            }, function (data) {
+                $('#followers_content .right').html(data);
+            });
         });
     });
 }
