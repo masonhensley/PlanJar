c66ddd96c6b9a6323226732f51cab6c63b99c90c
Parker Bossier <parker@planjar.com>
2011-07-13 16:40:15 -0500

    boje

--- application/models/load_plans.php
+++ application/models/load_plans.php
@@ -33,12 +33,10 @@ class Load_plans extends CI_Model
     function loadPlanData($plan)
     {
         // pull all user's current events
-        $query = "SELECT plans.id, plans.time_of_day, plans.date, places.name, plan_categories.category
-        FROM plans 
-        LEFT JOIN places 
-        ON plans.place_id=places.id 
-        LEFT JOIN plan_categories
-        ON plan_categories.id=plans.category_id
+        $query = "SELECT plans.id, plans.date, plans.time_of_day, plans.title, plans.event_id, places.name
+        FROM plans
+        LEFT JOIN places
+        ON plans.place_id=places.id
         WHERE plans.id=$plan";
 
         // pull data
@@ -59,13 +57,13 @@ class Load_plans extends CI_Model
             $date = $row->date;
             $date = date('m/d', strtotime($date));
             $name = $row->name;
-            $category = $row->category;
+            $title = $row->title;
         }
 
         // html to replace the data div
         $htmlString = "
         <div style=\"font-size:20px; width:100%; height:230px; color:darkblue; text-align: center;\">
-        $category at $name | $time_of_day | $date
+        $title at $name | $time_of_day | $date
         </div><br/><br/>
         <div class=\"delete_plan_container\"style=\"font-size: 20px; text-align:left;\">
         <div class=\"delete_plan\">Delete Plan</div></div>";
