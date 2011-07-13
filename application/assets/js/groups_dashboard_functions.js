34f5ddaf4fbd3cb675aeff3b2baaa94caa58c0c5
Parker Bossier <parker@planjar.com>
2011-07-12 19:52:40 -0500

    boje

--- application/assets/js/groups_dashboard_functions.js
+++ application/assets/js/groups_dashboard_functions.js
@@ -6,4 +6,29 @@ function populate_edit_groups_list() {
     $.get('/dashboard/get_following_groups', function (data) {
         $('#edit_groups_list').html(data);
     });
+    
+    // Click handlers
+    $('#edit_groups_list .abc').click(function () {
+            if ($(this).text() == '+ Follow') {
+                $(this).text('+ You sure?');
+            } else {
+                $.get('/dashboard/add_following', {
+                    following_id: $(this).parent().attr('user_id')
+                }, function (data) {
+                    populate_followers_list();
+                    populate_following_list();
+                });
+            }
+        });
+    
+    // Make followers selectable
+    $('#edit_groups_list .group_entry').click(function() {
+        $('.group_entry.selected_group').removeClass('selected_group');
+        $(this).addClass('selected_group');
+        $.get('/dashboard/get_group_details', {
+            group_id: $(this).attr('group_id')
+        }, function (data) {
+            $('#groups_content .middle').html(data);
+        });
+    });
 }
