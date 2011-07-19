93e1ca6671d62c138358a0433b88cf008f435660
Parker Bossier <parker@planjar.com>
2011-07-19 01:30:35 -0500

    boje

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -271,13 +271,11 @@ class Dashboard extends CI_Controller
         // This just looks nicer than a long function call.
         $name = $this->input->get('group_name');
         $description = $this->input->get('group_description');
-        $latitude = $user->latitude;
-        $longitude = $user->longitude;
         $school_id = $user->school_id;
         $description = $this->input->get('privacy');
 
         // Create the group.
-        $this->add_group($name, $description, $latitude, $longitude, $school_id, $privacy);
+        $this->add_group($name, $description, $privacy, $location_source);
 
         // Join the user to the group
         $this->follow_group($this->db->last_query());
@@ -300,13 +298,13 @@ class Dashboard extends CI_Controller
         if (count($invited_users) > 0)
         {
             $this->load->model('notification_ops');
-            $this->notification_ops->notify_users($invited_users, 'plan_invite', $this->db->insert_id());
+            $this->notification_ops->notify_users($invited_users, 'group_invite', $this->db->insert_id());
         }
 
         if (count($invited_groups) > 0)
         {
             $this->load->model('notification_ops');
-            $this->notification_ops->notify_joined_groups($invited_groups, 'plan_invite', $this->db->insert_id());
+            $this->notification_ops->notify_joined_groups($invited_groups, 'group_invite', $this->db->insert_id());
         }
 
         // Success
--- application/models/group_ops.php
+++ application/models/group_ops.php
@@ -165,8 +165,22 @@ class Group_ops extends CI_Model
 
     // Adds a gruop to the database.
     // If school_id isn't blank, use the latitude and longitude of the school.
-    public function add_group($name, $description, $latitude, $longitude, $school_id, $privacy)
+    public function add_group($name, $description, $privacy, $location_source)
     {
+        if ($location_source == 'school')
+        {
+            // Get the latitude and longitude from the school table.
+            $query_string = "SELECT latitude, longitude FROM school_data WHERE id = ?";
+            $query = $this->db->query($query_string, array($school_id));
+            $row = $query->row();
+            $latitude = $row->latitude;
+            $longitude = $row->longitude;
+        } else
+        {
+            $user = $this->ion_auth->get_user();
+            $latitude = $user->latitude;
+            $Longitude = $user->longitude;
+        }
         $query_string = "INSERT INTO groups VALUES (DEFAULT, ?, ?, ?, ?, ?, ?)";
         $query = $this->db->query($query_string, array($name, $latitude, $longitude, $description, $school_id, $privacy));
     }
