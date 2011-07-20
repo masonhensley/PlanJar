142c219730834549c66c11471d72d5238a1451a0
Parker Bossier <parker@planjar.com>
2011-07-19 21:27:47 -0500

    boje

--- application/assets/js/login.js
+++ application/assets/js/login.js
@@ -180,7 +180,7 @@ $(document).ready(function() {
             }
         },
         onfocusout: function(element) {
-            this.element(element);
+            this.form();
         }
     });
     
