cfb1804b4abb087c85c8fe2f34cca78ea68bf443
Parker Bossier <parker@planjar.com>
2011-07-19 22:22:38 -0500

    boje

--- application/models/sign_up_ops.php
+++ application/models/sign_up_ops.php
@@ -12,7 +12,7 @@ class Sign_up_ops extends CI_Model
     // Returns the school id corresponding to the given email's domain
     public function get_school_from_email($email)
     {
-        $domain = substr($email, strrpos($email, '@'));
+        $domain = substr($email, strrpos($email, '@') + 1);
 
         $query_string = "SELECT id FROM school_data WHERE email_domain = ?";
         $query = $this->db->query($query_string, array($domain));
