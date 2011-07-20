12c74259bdda94cfddc427a87b8835840df80be7
Parker Bossier <parker@planjar.com>
2011-07-19 21:32:56 -0500

    boje

--- application/assets/js/login.js
+++ application/assets/js/login.js
@@ -210,9 +210,10 @@ $.validator.addMethod('unique_email', function (value, element) {
 
 // Custom validator method to only allow school email addresses.
 $.validator.addMethod('allowed_email_domain', function (value, element) {
-    $.get('/login/check_email_domain', {
-        email: value
-    }, function (data) {
-        return data == 'true';
+    return $.validator.methods.remote.call(this, value, element, {
+        url: '/login/check_email_domain',
+        data: {
+            email: value
+        }
     });
 }, "That domain currently can't be used to create an account. Make sure to use your school email.");
