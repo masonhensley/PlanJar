6c2ebf26f781bd1847b5017658639bceebbf5fd3
Parker Bossier <parker@planjar.com>
2011-07-20 19:30:50 -0500

    boje

--- application/assets/js/start_plan_modal.js
+++ application/assets/js/start_plan_modal.js
@@ -29,9 +29,9 @@ function initialize_plan_modal() {
         $('#plan_location').focus();
     });
     
-    $('#cancel_plan').click(function () {
-        $('#create_plan_content').hide();
-    });
+//    $('#cancel_plan').click(function () {
+//        $('#create_plan_content').hide();
+//    });
     
     // Add the city name to the handle text.
     $('#create_plan_content .draggable_title_bar').html('Start a plan');
