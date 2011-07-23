7d700b3bc30f700e906f8888b4372e2f2624b476
Parker Bossier <parker@planjar.com>
2011-07-22 23:51:14 -0500

    boje

--- application/assets/js/start_plan_modal.js
+++ application/assets/js/start_plan_modal.js
@@ -147,9 +147,7 @@ function initialize_event_select_page() {
         // No title click handler
         $('#no_event_title').click(function () {
             // Clear the select
-            $('#plan_event_select').attr('selectedIndex', '-1');
-            console.log('clear');
-            console.log($('#plan_event_select').val());
+            $('#plan_event_select option[selected="selected"]').removeAttr('selected');
             
             // Reset and hide the title and privacy settings
             $('#plan_event_id').val('');
