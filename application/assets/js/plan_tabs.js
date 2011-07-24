e880fdc6199e49c9aca92e5e8ff5c288f0dc5538
Parker Bossier <parker@planjar.com>
2011-07-24 13:46:12 -0500

    boje

--- application/assets/js/plan_tabs.js
+++ application/assets/js/plan_tabs.js
@@ -67,6 +67,6 @@ function delete_plan_outer_click() {
         // Replace the original text and re-assign the one-time click event
         console.log('reassigned');
         $('.delete_plan').html(orig_text);
-        $('.delete_plan').one('click', delete_plan_outer_click);
+        //$('.delete_plan').one('click', delete_plan_outer_click);
     });
 }
