9f3f12f36421cb99329f24c1200baa89d2569cd8
Parker Bossier <parker@planjar.com>
2011-07-26 13:01:49 -0500

    boje

---
+++ application/views/foo_view.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <script src="http://maps.googleapis.com/maps/api/js?libraries=places&sensor=false" type="text/javascript"></script>
+        <script type="text/javascript">
+            var myLatlng = new google.maps.LatLng(30, -90);
+
+            var myOptions = {
+                zoom: 14,
+                center: myLatlng,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+
+            map = new google.maps.Map(document.getElementById("map"), myOptions);
+        </script>
+    </head>
+
+    <body>
+        <div id="map" style="width: 200px; height: 200px;"></div>
+    </body>
+</html>
