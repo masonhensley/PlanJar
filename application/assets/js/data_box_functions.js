b7fe3d1338b27a9cdd00ed130e98e3efe6c6da32
Parker Bossier <parker@planjar.com>
2011-07-26 10:24:59 -0500

    boje

--- application/assets/js/data_box_functions.js
+++ application/assets/js/data_box_functions.js
@@ -41,3 +41,21 @@ function load_upcoming_events(selected_groups){
         $('.upcoming_events').html(data); 
     });
 }
+
+function show_selected_location() {
+    $('div.location_tab').click(function() {
+        
+        if(!$(this).hasClass('selected_location_tab'))
+        {
+            $('.selected_location_tab').removeClass('selected_location_tab');
+            $(this).addClass('selected_location_tab');
+            $.get('/home/show_location_data', {
+                'place_id': $('.selected_location_tab').attr('place_id'),
+                'date': $('.selected_location_tab').attr('date'),
+                'selected_groups':get_selected_groups()
+            }, function (data) {
+                $('#location_data').html(data);
+            });
+        }
+        show_data_container('#location_data');
+    });
--- application/assets/js/location_tabs.js
+++ application/assets/js/location_tabs.js
@@ -2,21 +2,5 @@ $(function() {
     show_selected_location();
 });
 
-function show_selected_location() {
-    $('div.location_tab').click(function() {
-        
-        if(!$(this).hasClass('selected_location_tab'))
-        {
-            $('.selected_location_tab').removeClass('selected_location_tab');
-            $(this).addClass('selected_location_tab');
-            $.get('/home/show_location_data', {
-                'place_id': $('.selected_location_tab').attr('place_id'),
-                'date': $('.selected_location_tab').attr('date'),
-                'selected_groups':get_selected_groups()
-            }, function (data) {
-                $('#location_data').html(data);
-            });
-        }
-        show_data_container('#location_data');
-    });
+
 }
