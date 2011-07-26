e33695a676c7ab02209199bbc72c173b4d9dc4d6
Parker Bossier <parker@planjar.com>
2011-07-26 17:06:08 -0500

    boje

--- application/assets/js/home_functions.js
+++ application/assets/js/home_functions.js
@@ -74,8 +74,7 @@ function mapThisGoogle(latitude,longitude)
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
         
-    //map = 
-    new google.maps.Map(document.getElementById("map"), myOptions);
+    map = new google.maps.Map(document.getElementById("map"), myOptions);
 }
 
 // Returns a list of selected groups.
