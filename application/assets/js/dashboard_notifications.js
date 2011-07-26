e2be11f8aa620b8108224630bf567fd7e0fc7b3c
Parker Bossier <parker@planjar.com>
2011-07-25 20:15:27 -0500

    boje

--- application/assets/js/dashboard_notifications.js
+++ application/assets/js/dashboard_notifications.js
@@ -6,6 +6,19 @@ function get_notifications() {
     $.get('/dashboard/get_notifications', function (data) {
         $('#notifications_list').html(data);
         
+        // Read/unread toggle
+        $('.notification_entry .mark_read').click(function () {
+            if ($(this).parent().hasClass('unviewed')) {
+                $(this).parent().removeClass('unviewed');
+                $(this).html('Mark as unread');
+                update_notification_viewed($(this).parent().attr('notif_id'), 1);
+            } else {
+                $(this).parent().addClass('unviewed');
+                $(this).html('Mark as read');
+                update_notification_viewed($(this).parent().attr('notif_id'), 0);
+            }
+        });
+        
         // User profile link click handler
         $('.user_notif_link').click(function () {
             // Get the user profile and show it
