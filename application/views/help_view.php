e39cf3d90bda41424c32ea2c0673d827ab4e00a4
Mason Hensley <masonhensley@gmail.com>
2011-07-26 13:16:07 -0500

    asd

--- application/views/help_view.php
+++ application/views/help_view.php
@@ -3,17 +3,7 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
-        <!-- CSS -->
-        <link rel=stylesheet href="/application/assets/css/help.css" type="text/css" />
-        <link rel=stylesheet href="/application/assets/css/eggplant/theme.css" type="text/css" />
-
-        <!-- jQuery and jQuery UI -->
-        <script type="text/javascript" src="/application/assets/js/jquery-1.6.1.min.js"></script>
-        <script type="text/javascript" src="/application/assets/js/jquery-ui-1.8.13.min.js"></script>
-
-<!-- Help Script -->
-<script src='http://planjar.helpjuice.com/orange-juice/iFrameHeightJuicer.js'></script>
-
+       
         <title>PlanJar | Helpo</title>
 
     </head>
