f27fbfafba6f38589d1f9cc16d7a200606c3a7db
Parker Bossier <parker@planjar.com>
2011-07-26 20:54:48 -0500

    boje

--- application/assets/js/home_functions.js
+++ application/assets/js/home_functions.js
@@ -200,8 +200,6 @@ function show_data_panel(data_div, callback) {
 function map_user_position() {
     clear_map_markers();
     
-    console.log(myLatitude + ' ' + myLongitude);
-    
     map_marker_array.push(new google.maps.Marker({
         position: new google.maps.LatLng(myLatitude, myLongitude),
         map: map,
@@ -210,6 +208,7 @@ function map_user_position() {
     
     map.setCenter(new google.maps.LatLng(myLatitude, myLongitude));
     map.setZoom(14);
+    console.log('zoom 14');
 }
 
 // Calculates and sets the bounds for the map based on map_marker_array (global var)
