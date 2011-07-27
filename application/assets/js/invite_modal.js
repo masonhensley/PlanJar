966569c9e627832e71770df996ceaa6a4242c049
Parker Bossier <parker@planjar.com>
2011-07-27 10:48:35 -0500

    boje

--- application/assets/js/invite_modal.js
+++ application/assets/js/invite_modal.js
@@ -1,3 +1,15 @@
+function initialize_invite_modal() {
+    $('#close_invite_modal').click(function () {
+        reset_invite_modal();
+        
+        $('#invite_modal').hide('fast');
+    });
+}
+
 function open_invite_modal(priv_type, invite_type) {
-    console.log('opening with privacy ' + priv_type + ' and type ' + invite_type);
+    $('#invite_modal').show('fast');
+}
+
+function reset_invite_modal() {
+    
 }
