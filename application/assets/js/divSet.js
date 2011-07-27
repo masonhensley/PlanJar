3b4f0b38490e735d3b5182e6ceeea47790ad0677
Parker Bossier <parker@planjar.com>
2011-07-27 13:58:53 -0500

    boje

---
+++ application/assets/js/divSet.js
@@ -0,0 +1,27 @@
+// Custom jQuery divSet function
+// Call this function on a div containing the divs you want to make into buttons. Everything else is done for you.
+// Pass true to enable multiple selection
+$.fn.divSet = function(multiple) {
+    var parent_wrapper = $(this);
+    parent_wrapper.find('div').addClass('divset');
+    
+    // Click event
+    parent_wrapper.find('div').click(function() {
+        if (multiple == true) {
+            // Multiple select
+            if ($(this).hasClass('divset_selected')) {
+                $(this).removeClass('divset_selected');
+            } else {
+                $(this).addClass('divset_selected');
+            }
+        } else {
+            // Single select
+            if (!$(this).hasClass('divset_selected')) {
+                
+                // Select only this
+                parent_wrapper.find('div').removeClass('divset_selected');
+                $(this).addClass('divset_selected');
+            }
+        }
+    });
+}
