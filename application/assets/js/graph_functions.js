9ed285ce6819542e88bbf03fede4f758460e8429
Parker Bossier <parker@planjar.com>
2011-07-28 13:54:34 -0500

    boje

--- application/assets/js/graph_functions.js
+++ application/assets/js/graph_functions.js
@@ -2,21 +2,20 @@
 function populate_sex_info(container, data) {
     var j_first_container = $(container + ' div:first');
     var d_first_container = d3.select(container + ' div').node();
+    var first_container_width = j_first_container.width();
     
     // Add the squares
     for (var i = 0; i < 10; ++i) {
         j_first_container.append('<div class="sex_box"></div>');
     }
     
-//    var graph_data = [.75];
-//        var chart = d3.select(".graph_data");
-//        
-//        chart.enter()
-//        .append("div")
-//        .style("width", function(graph_data) {
-//            return graph_data * 10 + "px";
-//        }).style("height", "17px")
-//        .text(function(d) {
-//            return d;
-//        });
+    // Add the overlay
+    j_first_container.append('<div class="sex_overlay"></div>');
+    
+    var graph_data = [.75];
+    
+    var d_overlay = d3.select(container + ' div.sex_overlay');
+    d_overlay.style("width", function(graph_data) {
+        return first_container_width * (1-graph_data) + "px";
+    });
 }
