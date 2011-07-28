a8056786cdb250bf4fded00a7ea5a2a43bfc50ee
Parker Bossier <parker@planjar.com>
2011-07-28 18:57:14 -0500

    boje

--- application/assets/js/graph_functions.js
+++ application/assets/js/graph_functions.js
@@ -2,36 +2,47 @@
 
 // Outputs ten boxes with the supplied percentage of them filled in
 // Accepts 0 <= n <= 1
-function populate_percentage_box(container, data) {
-    // Vars needed to construct a set of marker boxes
-    var marker_box;
-    var half_marker_box;
-    var number_of_active_halves = Math.round(data * 20);
+function populate_percentage_box(container, percentage) {
+    // Create the marker boxes if they aren't there
+    if ($(container).children().length < 10) {
+        // Clear the container and add the boxes
+        $(container).html('');
+        
+        var marker_box;
+        var half_marker_box = $('<div class="half_marker_box"></div>');
     
-    // Loop through each marker box
-    for (var i = 0; i < 10; ++i) {
-        // Create a marker box
-        marker_box = $('<div class="marker_box"></div>');
+        // Loop through each marker box
+        for (i = 0; i < 10; ++i) {
+            // Create a marker box
+            marker_box = $('<div class="marker_box"></div>');
         
-        // Append the first half marker box and change color if necessary
-        half_marker_box = $('<div class="half_marker_box"></div>');
-        if (number_of_active_halves > 0) {
-            half_marker_box.css('background-color', 'purple');
-            --number_of_active_halves;
+            // Append the half marker boxes
+            half_marker_box.appendTo(marker_box);
+            half_marker_box.appendTo(marker_box);
+        
+            // Append the marker box to its wrapper
+            marker_box.appendTo(container);
         }
-        half_marker_box.appendTo(marker_box);
-        
-        // Append the second half marker box and change color if necessary
-        half_marker_box = $('<div class="half_marker_box"></div>');
-        if (number_of_active_halves > 0) {
-            half_marker_box.css('background-color', 'purple');
-            --number_of_active_halves;
+    
+        // Create an array with an entry for each half box
+        var d3_data = [];
+        for (var i = 0; i < 20; ++i) {
+            d3_data.push(i < percentage * 20);
         }
-        half_marker_box.appendTo(marker_box);
-        
-        // Append the marker box to its wrapper
-        marker_box.appendTo(container);
     }
+     
+    // Select all half boxes
+    d3.select(container).selectAll('.half_marker_box')
+    // Add the data to the selection
+    .data(d3_data)
+    // Set the background to purple if necessary
+    .style('background-color', function (d) {
+        if (d) {
+            return 'purple'
+        } else {
+            return 'white'
+        }
+    });
 }
 
 // Populates the container with a vertical bar graph
