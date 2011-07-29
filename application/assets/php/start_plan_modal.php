d59235ac66bf74bc769a081dffd8c30de08bd83c
Parker Bossier <parker@planjar.com>
2011-07-29 13:30:01 -0500

    boje

--- application/assets/php/start_plan_modal.php
+++ application/assets/php/start_plan_modal.php
@@ -45,7 +45,11 @@
                     <div id="plan_event_select_wrapper"></div>
                 </div>
 
-                <div id="new_event_side">
+                <div class="right">
+                    <div id="create_event_title">
+                        Don't find anything you want to go to? Click <b>Just going</b> to the right or
+                        <b>create an event</b> below.
+                    </div>
                     <input type="button" id="create_event" value="Start an event"/>
 
                     <div id="start_event_content" style="display: none;">
