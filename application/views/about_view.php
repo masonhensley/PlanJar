f15d8a7e6b7bd45f73f2c9a325ef4b1833172947
Mason Hensley <masonhensley@gmail.com>
2011-07-28 20:23:04 -0500

    Edited application/views/about_view.php via GitHub

--- application/views/about_view.php
+++ application/views/about_view.php
@@ -26,7 +26,13 @@
         <h1> Our Team <span class="arrow"></span> </h1> 
 
         <h3> Mason Hensley <span class="arrow_right"></span> </h3>
-
+                <div class ="person">
+        <div class ="pic">
+        </div>
+        <div class ="info">
+        </div>
+        
+        </div>
         <h3> Parker Bossier <span class="arrow_right"></span> </h3>
 
         <h3> Wells Johnston <span class="arrow_right"></span> </h3>
