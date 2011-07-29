bfc543ea4c8316c277c27aecadea128f91eb29c5
Parker Bossier <parker@planjar.com>
2011-07-29 11:43:51 -0500

    boje

--- application/assets/js/graph_functions.js
+++ application/assets/js/graph_functions.js
@@ -102,8 +102,8 @@ function populate_day_graph(container, data) {
         var cur_date = new Date();
         var given_date = new Date(d.date);
         var day_array = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-        console.log(given_date.toString());
-        console.log(cur_date.toString());
+        console.log(given_date.toString().substr(0, -18));
+        console.log(cur_date.toString().substr(0, -18));
         if (cur_date.toString().substr(0, -18) == given_date.toString().substr(0, -18)) {
             return 'Today';
         } else {
