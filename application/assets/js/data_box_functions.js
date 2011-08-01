43214f468246359dce5e0b41fafce1388c8a46cd
Parker Bossier <parker@planjar.com>
2011-08-01 15:32:55 -0500

    boje

--- application/assets/js/data_box_functions.js
+++ application/assets/js/data_box_functions.js
@@ -37,8 +37,10 @@ function load_data_box(selected_day, selected_groups, filter)
 
         // Capture the data
         data = data.data;
-        
         console.log(data);
+        
+        // Select the correct value for the select box
+        $('#filter option[value="' + data['filter'] + '"]').click();
         
         // Populate the graphs
         populate_percentage_box('.total_percent_container', data.percent_total_going_out, 'marker_total');
