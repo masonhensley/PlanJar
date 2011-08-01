858ed0ce2446fe21a3107cea6a9b52f69dd5703e
Parker Bossier <parker@planjar.com>
2011-08-01 15:55:38 -0500

    boje

--- application/assets/js/data_box_functions.js
+++ application/assets/js/data_box_functions.js
@@ -40,9 +40,6 @@ function load_data_box(selected_day, selected_groups, filter)
         console.log(data);
         
         // Select the correct value for the select box
-        //        console.log($('#filter option[value="' + data['filter'] + '"]'));
-        //        console.log('#filter option[value="' + data['filter'] + '"]');
-        //        $('#filter option[value="' + data['filter'] + '"]').select();
         $('#filter').val(data['filter']);
         
         // Populate the graphs
