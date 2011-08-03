9f9a5db36fb6c8b6af8159474b759bff21bb3008
Mason Hensley <masonhensley@gmail.com>
2011-08-03 18:31:31 -0500

    Edited application/views/privacy_view.php via GitHub

--- application/views/privacy_view.php
+++ application/views/privacy_view.php
@@ -13,7 +13,7 @@
                     dataType: "jsonp",
                     cache: false,
                     url: "https://api.instagram.com/v1/media/search?lat=36.14934&lng=-86.80554&distance=2500
-?client_id=[ClientID]&access_token=[CODE]",
+?client_id=93ccf3a9f7924a6b8e33cc5234cebc50",
                     success: function(data) {
                         for (var i = 0; i < 6; i++) {
                             $(".instagram").append("<div class='instagram-placeholder'><a target='_blank' href='" +
