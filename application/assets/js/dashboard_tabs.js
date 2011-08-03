e307cd2096c12e96ed166b9fc370e57bf2d4bfdc
Parker Bossier <parker@planjar.com>
2011-08-03 11:20:32 -0500

    boje

--- application/assets/js/dashboard_tabs.js
+++ application/assets/js/dashboard_tabs.js
@@ -11,7 +11,7 @@ function initialize_dashboard_tabs() {
     $('.tab_container .tab').click(function () {
         if (!$(this).hasClass('tab_selected')) {
             $('#create_group').hide(); // hide the create group icon when the group tab isn't selected'
-            $('#followers_content .right').hide(); // hide the profile for the followers content
+            $('#friends_content .right').hide(); // hide the profile for the followers content
             show_data_container($(this).attr('assoc_div'));
         } else {
             // Call the js setup function again (reload)
