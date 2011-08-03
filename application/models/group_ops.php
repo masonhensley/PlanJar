b92cf19cc746f515d7753dee0b88e647d69786ea
Parker Bossier <parker@planjar.com>
2011-08-02 23:10:04 -0500

    boje

--- application/models/group_ops.php
+++ application/models/group_ops.php
@@ -325,13 +325,17 @@ class Group_ops extends CI_Model
         $query_string = "DELETE FROM groups WHERE id = ?";
         $query = $this->db->query($query_string, array($group_id));
 
+        // Delete the followers
+        $query_string = "DELETE FROM group_relationships WHERE group_id = ?";
+        $query = $this->db->query($query_string, array($group_id));
+
         // Delete corresponding invites
         $query_string = "DELETE FROM group_invitees WHERE group_id = ?";
         $query = $this->db->query($query_string, array($group_id));
 
-        // Delete corresponding notification
+        // Delete corresponding notifications
         $query_string = "DELETE FROM notifications WHERE type = ? AND subject_id = ?";
-        $query = $this->db->query($query_string, array('event_invite', $group_id));
+        $query = $this->db->query($query_string, array('group_invite', $group_id));
     }
 
 }
