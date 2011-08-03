8c2cf3435ef075daca2ec2bf6cd348e1a6dd11e2
Parker Bossier <parker@planjar.com>
2011-08-03 17:14:29 -0500

    boje

--- application/views/privacy_view.php
+++ application/views/privacy_view.php
@@ -6,19 +6,12 @@
 
         <script type="text/javascript">
             $(function() {
-
-                var access_token = location.hash.split('=')[1];
-
-                if (location.hash) {
-
-
                     $.ajax({
                         type: "GET",
                         dataType: "jsonp",
                         cache: false,
-                        url: "https://api.instagram.com/v1/users/[userid]/media/recent/?access_token=[access_token]",
+                        url: "https://api.instagram.com/v1/media/popular?client_id=93ccf3a9f7924a6b8e33cc5234cebc50",
                         success: function(data) {
-
                             for (var i = 0; i < 6; i++) {
                                 $(".instagram").append("<div class='instagram-placeholder'><a target='_blank' href='" +
                                     data.data[i].link +"'><img class='instagram-image' src='" +
@@ -27,16 +20,13 @@
                             }
                         }
                     });
-                } else {
-                    location.href="https://instagram.com/oauth/authorize/?display=touch&client_id=93ccf3a9f7924a6b8e33cc5234cebc50&redirect_uri=http://testing.pagodabox.com/privacy.php/&response_type=token";
-                }
             });
         </script>
     </head>
 
     <body>
-
-curl https://api.instagram.com/v1/media/popular?client_id=93ccf3a9f7924a6b8e33cc5234cebc50
+        
+        <div class="instagram"></div>
 
     </body>
 </html>
