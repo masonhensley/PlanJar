affffdac000faabcf1747a2d8388243029c979a2
Mason Hensley <masonhensley@gmail.com>
2011-08-03 16:53:12 -0500

    adding client

--- application/views/privacy_view.php
+++ application/views/privacy_view.php
@@ -31,8 +31,8 @@ $(function() {
 
 } else {
     location.href="https://instagram.com/oauth/
-    authorize/?display=touch&client_id=[clientid]
-    &redirect_uri=[callbackuri]/&response_type=token"; 
+    authorize/?display=touch&client_id=93ccf3a9f7924a6b8e33cc5234cebc50
+    &redirect_uri=http://testing.pagodabox.com/privacy.php/&response_type=token"; 
     
 }           
         }
