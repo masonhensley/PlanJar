9731bf5fa81f252772373ad866ea483fd0098877
Parker Bossier <parker@planjar.com>
2011-08-04 17:31:32 -0500

    boje

--- application/assets/js/add_location_modal.js
+++ application/assets/js/add_location_modal.js
@@ -2,6 +2,9 @@ $(function () {
     initialize_add_location_modal();
 });
 
+var new_location_map;
+var new_location_marker;
+
 // Initializes the add location modal
 function initialize_add_location_modal() {
     // Close click handler
@@ -13,9 +16,31 @@ function initialize_add_location_modal() {
     $('#close_add_location').draggable({
         handle: '.title_bar'
     });
+    
+    // Create the map
+    var map_options = {
+        zoom: 14,
+        center: new google.maps.LatLng(myLatitude,myLongitude),
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    new_location_map = new google.maps.Map(document.getElementById("new_location_map"), map_options);
 }
 
 // Opens the add location modal
 function show_add_location_modal() {
-    $('#add_location_modal').show('fast');
+    $('#add_location_modal').show('fast', function () {
+        // Add the marker
+        new_location_marker = new google.maps.Marker({
+            position: new google.maps.LatLng(myLatitude, myLongitude),
+            map: new_location_map,
+            icon: 'http://www.google.com/mapfiles/arrow.png'
+        });
+        
+        // Resize the map
+        google.maps.event.trigger(new_location_map, 'resize');
+        
+        // Center the map
+        new_location_map.setCenter(new google.maps.LatLng(myLatitude, myLongitude));
+        //new_location_map.setZoom(14);
+    });
 }
