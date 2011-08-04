6e582f5cde44016616bb556320a2a1918aeef276
Parker Bossier <parker@planjar.com>
2011-08-04 14:16:23 -0500

    boje

--- application/assets/js/day_tabs.js
+++ application/assets/js/day_tabs.js
@@ -16,7 +16,7 @@ function initialize_day_tabs() {
 //        $('.selected_location_tab').removeClass('selected_location_tab');
         
         // Display the info box
-        display_info();
+        display_info(true);
     });
     
     // Left and right arrow click functions
