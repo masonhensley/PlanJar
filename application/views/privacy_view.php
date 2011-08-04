e5d939c16836d01328b24a9124a012ed0c7057fa
Mason Hensley <masonhensley@gmail.com>
2011-08-03 22:05:59 -0500

    Edited application/views/privacy_view.php via GitHub

--- application/views/privacy_view.php
+++ application/views/privacy_view.php
@@ -28,6 +28,12 @@
 
     <body>
 
+    https://api.instagram.com/v1/media/search?lat=36.14934&lng=-86.80554&distance=2500
+    
+    <br>
+
+    https://api.instagram.com/v1/media/popular?client_id=93ccf3a9f7924a6b8e33cc5234cebc50
+
         <div class="instagram"></div>
 
     </body>
