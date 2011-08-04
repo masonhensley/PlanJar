f50bf344ca6c8a01060ada1cf3c4cc071c2cd6f0
Wells Johnston <wellsjohnston@gmail.com>
2011-08-04 16:04:30 -0500

    5

--- application/assets/php/dashboard_followers.php
+++ application/assets/php/dashboard_followers.php
@@ -1,7 +1,9 @@
 <div class="left">
     <div class="left_header">
-        Your followers<br/>
+        Friends<br/>
         <font style="color:gray;">(select to view profile)</font>
+        <br/>
+        (note: you are not "friends" with someone unless you are following them back.  Until then, the information they can see is limited)
     </div>
     <div id="followers_list"></div>
 </div>
