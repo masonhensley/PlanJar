84089164cde4ebd349f7db91da1b60593b7d3aed
Mason Hensley <masonhensley@gmail.com>
2011-08-03 23:33:18 -0500

    Edited application/views/privacy_view.php via GitHub

--- application/views/privacy_view.php
+++ application/views/privacy_view.php
@@ -24,30 +24,13 @@
                  </div>  
      <div id="container">  
   
-        <h2> Our Team <span class="arrow"></span> </h2> 
+        Lorem ipsizzle dolizzle sit amizzle, ma nizzle adipiscing pizzle. Nullizzle sapien velizzle, aliquet volutpizzle, suscipit dope, gravida vizzle, arcu. Pellentesque dang tortor. Sizzle eros. Cool at brizzle da bomb the bizzle tempizzle uhuh ... yih!. Maurizzle pellentesque dawg et turpizzle. Vestibulum izzle tortor. Pellentesque mah nizzle rhoncizzle shizznit. In hac mofo platea dictumst. Yo dapibus. Curabitur tellus urna, you son of a bizzle eu, mattizzle doggy, eleifend sheezy, nunc. Shit yippiyo. Sizzle dang velit sizzle fizzle.
 
-        <h4> Name <span class="arrow_right"></span> </h4>
-                <div class ="person">
-                      <div class ="info">
-                        </div>
-                      <div class ="pic">
-                        </div>
-        
-        </div>
-        <h4> Name <span class="arrow_right"></span> </h4>
-                <div class ="person">
-                   <div class ="info">
-                     </div>
-                   <div class ="pic">
-                     </div>
-        </div>
+Fo shizzle mah nizzle fo rizzle, mah home g-dizzle laorizzle urna izzle nisl. Fo shizzle quizzle mofo. Maecenas pulvinar, ipsizzle malesuada malesuada rizzle, nulla purus dizzle felis, izzle luctizzle metus nulla et gangster. Vivamizzle ullamcorper, tortizzle sheezy varius for sure, tellivizzle nunc ultricies crazy, izzle check out this leo elit break it down gangsta. Mauris rizzle, shizzlin dizzle doggy volutpizzle consectetizzle, boom shackalack cool ma nizzle nisi, izzle gangsta enim dizzle ut nisl. Nullizzle shizzle my nizzle crocodizzle velizzle sizzle fo shizzle mah nizzle fo rizzle, mah home g-dizzle eleifend hizzle. Phasellus nec fizzle. Curabitizzle nizzle pimpin' for sure pede i saw beyonces tizzles and my pizzle went crizzle facilisizzle. Maecenas boom shackalack owned, iaculizzle fizzle, molestie fo shizzle, egestas a, eros. Pimpin' vitae shiznit we gonna chung nibh bibendum fo. Nizzle pulvinar fo shizzle my nizzle doggy. Aliquizzle crackalackin volutpizzle. Nunc ut mah nizzle at rizzle pretizzle you son of a bizzle. Fo shizzle my nizzle nizzle ma nizzle sed yo mamma condimentum ultricies. Brizzle nisl. Shizznit izzle urna. Uhuh ... yih! laoreet ipsizzle yippiyo mi. Da bomb izzle shiz.
 
-        <h4> Name <span class="arrow_right"></span> </h4>
-                <div class ="person">
-                    <div class ="info">
-                     </div>
-                    <div class ="pic">
-                      </div>
+Curabitizzle bow wow wow dizzle quis nisi bow wow wow mollis. Own yo' potenti. Break yo neck, yall odio. Vivamus neque. Uhuh ... yih! orci. Crizzle maurizzle rizzle, interdizzle shiz, feugizzle shiznit amet, malesuada fo shizzle mah nizzle fo rizzle, mah home g-dizzle, pede. Pellentesque . Vestibulum orci mi, shiznit izzle, sagittizzle break yo neck, yall, adipiscing semper, velit. Crizzle rizzle own yo'. Aliquam volutpat shiznit vel orci. Crizzle quizzle justo sure purizzle sodales ornare. Sed yippiyo gizzle izzle lacus. Nunc shiznit. Suspendisse funky fresh placerizzle lacus. Dawg dang ante. Nunc pharetra, leo eu dapibizzle hendrerizzle, break yo neck, yall felizzle elementizzle sem, in aliquet get down get down felizzle luctizzle pede. Da bomb a boom shackalack. Class aptent taciti shut the shizzle up sheezy torquent pizzle brizzle nostra, per break it down hymenaeos. Own yo' fo shizzle, hizzle nizzle elementum nonummy, nisl yo viverra leo, fizzle sempizzle risus arcu dope sizzle.
+
+My shizz izzle fizzle izzle arcu interdizzle mofo. Dang posuere. For sure non ante. Owned pizzle pot, that's the shizzle ac, condimentizzle for sure, ass quizzle, massa. Vestibulizzle feugizzle. Own yo' vehicula that's the shizzle leo. Quisque bow wow wow, crunk a suscipizzle mah nizzle, dawg shizzle my nizzle crocodizzle commodo tellizzle, mofo eleifend dizzle nulla nec metus. Sizzle feugiat sollicitudin velit. I'm in the shizzle neque yippiyo, nonummy bow wow wow, malesuada fizzle, bibendizzle varius, for sure. In pot, ligula vel pretium hendrerizzle, velit nibh pizzle nisl, a phat the bizzle lorizzle sit amizzle augue. Aliquizzle fo lacizzle pot odio fermentum da bomb. Quisque aliquizzle break it down fo shizzle. Nam vehicula hendrerit mauris. Fo shizzle mah nizzle fo rizzle, mah home g-dizzle vehicula. We gonna chung placerat sheezy a crunk. Etizzle rizzle. In hac rizzle platea dictumst. Cras izzle leo nec diam gizzle ma nizzle. Phasellus erat ante, crackalackin quis, sure a, sure nizzle, funky fresh. Morbi vizzle nibh ut neque cool fringilla.
         
         </div> 
     </div>
