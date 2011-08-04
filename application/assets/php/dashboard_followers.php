1cd98e2aee5353aa7430fb339940a48e085fee48
Wells Johnston <wellsjohnston@gmail.com>
2011-08-04 16:18:21 -0500

    5

--- application/assets/php/dashboard_followers.php
+++ application/assets/php/dashboard_followers.php
@@ -6,8 +6,7 @@
     <div id="followers_list"></div>
 </div>
 <div class="right">
-    Note: you are not "friends" with someone unless you are following them back.  Until then, your followers can only see limited information
+    Note: you are not "friends" with someone unless you are following them back.  Until then, your followers can only see limited information.
     <br/><hr/>
-    <br/>
     <font style="color:gray; font-size:15px;">Select a person on the left to display their profile here</font>
 </div>
