1da08defd52cf5ec9c20deec6b8726db84436cd9
Parker Bossier <parker@planjar.com>
2011-08-04 17:15:16 -0500

    boje

--- application/assets/php/add_location_modal.php
+++ application/assets/php/add_location_modal.php
@@ -3,4 +3,32 @@
         Add a location
         <input type="button" id="close_add_location" value='X' style="float: right;"/>
     </div>
+
+    <div>
+        <div class="left">
+            <div class="in-field_block">
+                <label for="new_location_name">Name</label>
+                <input type="text" id="new_location_name" name="new_location_name"/>
+            </div>
+        </div>
+
+        <div class="right">
+            <div class="in-field_block">
+                <label for="new_location_category">Category</label>
+                <input type="text" id="new_location_category" name="new_location_category"/>
+            </div>
+        </div>
+    </div>
+
+    <div id="new_location_map"></div>
+
+    <div>
+        <div class="left">
+
+        </div>
+
+        <div class="right">
+
+        </div>
+    </div>
 </div>
