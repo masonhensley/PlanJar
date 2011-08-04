cd7c4aa9568708a519804edfa303bb434e506348
Wells Johnston <wellsjohnston@gmail.com>
2011-08-04 17:56:52 -0500

    5

--- application/models/load_group_panel.php
+++ application/models/load_group_panel.php
@@ -23,27 +23,38 @@
         <font style="color:gray;">(select one or multiple)</font>
         <br/><hr/>
         <?php
-        foreach ($joined_groups as $group)
+        if (count($joined_groups) + count($followed_groups) > 1)
         {
-            ?>
-            <div class="selectable_group" group_id="<?php echo($group['id']); ?>">
-                <?php
-                echo($group['name']);
+            foreach ($joined_groups as $group)
+            {
                 ?>
-            </div>
-            <?php
-        }
-        ?>
-        <?php
-        foreach ($followed_groups as $group)
-        {
+                <div class="selectable_group" group_id="<?php echo($group['id']); ?>">
+                    <?php
+                    echo($group['name']);
+                    ?>
+                </div>
+                <?php
+            }
             ?>
-            <div class="selectable_group" group_id="<?php echo($group['id']); ?>">
-                <?php echo($group['name']); ?>
-            </div>
             <?php
+            foreach ($followed_groups as $group)
+            {
+                ?>
+                <div class="selectable_group" group_id="<?php echo($group['id']); ?>">
+                    <?php echo($group['name']); ?>
+                </div>
+                <?php
+            }
+        }else{
+            ?>
+        <br/>
+        <i>Not following any groups yet</i><br/>
+        <a href="/dashboard/groups/suggested" style="color:#black;font-weight:bold;" >Find groups</a><br/> 
+        <?php
         }
-        ?>
+            ?>
+        
+
         <div id="group_select_type">
             <div id="select_one_group" style="width:50px;">one</div>
             <div id="select_mult_groups" style="margin-left:5px;width:50px;">multiple</div>
@@ -54,4 +65,3 @@
     <a href="/dashboard/groups/suggested" style="color:#57A8E2;font-weight:bold;" >Find groups</a><br/> 
     <a href="/dashboard/following/suggested" style="color:#57A8E2;font-weight:bold;">Find people</a>
 </div>
-
