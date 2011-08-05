762f7b7616a59b353b396ebb6dd053a3faa486a8
Wells Johnston <wellsjohnston@gmail.com>
2011-08-05 12:29:50 -0500

    5

--- application/models/load_friend_plans.php
+++ application/models/load_friend_plans.php
@@ -11,7 +11,7 @@ class Load_friend_plans extends CI_Model
 
     function populate_plans($friend_id)
     {
-        $this->_get_friend_plans();
+        $this->_get_friend_plans($friend_id);
     }
 
     function _get_friend_plans($friend_id)
