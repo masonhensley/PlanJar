00ff8b614f58e1c8b364d88b16cfea3026713a85
Wells Johnston <wellsjohnston@gmail.com>
2011-08-05 11:35:17 -0500

    5

--- application/assets/php/friends_plans_modal.php
+++ application/assets/php/friends_plans_modal.php
@@ -3,6 +3,8 @@
         Friend list
         <input  type="button" id="cancel_plan"  style="float:right;" value="X"/>
     </div>
+    <br/>
+    Select a friend to view their upcoming plans
     <hr/>
     <div class="friend_list">
         <?php
