2b9c779e0258d0e1e84481054de3aa0c99138a10
Wells Johnston <wellsjohnston@gmail.com>
2011-08-05 13:24:19 -0500

    5

--- application/models/load_friend_plans.php
+++ application/models/load_friend_plans.php
@@ -35,7 +35,9 @@ class Load_friend_plans extends CI_Model
 
     function _populate_friend_plans($plans_result)
     {
-        
+        return "hey";
+        /*
+        ob_start(); // start the output buffer 
      
         if ($plans_result->num_rows() > 0)
         {
@@ -90,7 +92,7 @@ class Load_friend_plans extends CI_Model
             <font style="font-style:italic;">No plans yet</font><br/><br/>
             <?php
         }
-        
+        */
     }
 
 }
