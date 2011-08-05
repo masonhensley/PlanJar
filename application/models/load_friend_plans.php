24559c81aab51a6d2a8f87dc35769ddc40f33e69
Wells Johnston <wellsjohnston@gmail.com>
2011-08-05 12:37:50 -0500

    5

--- application/models/load_friend_plans.php
+++ application/models/load_friend_plans.php
@@ -22,9 +22,10 @@ class Load_friend_plans extends CI_Model
             SELECT plans.id, events.date, events.time, events.title, plans.event_id, places.name
             FROM plans
             JOIN events ON events.id=plans.event_id AND events.date>=NOW()
-            LEFT JOIN event_invitees ON event_invitees.event_id=events.id
+            LEFT JOIN event_invites ON event_invites.event_id=events.id
             JOIN places ON events.place_id=places.id
-            WHERE plans.user_id=$friend_id AND (events.privacy='open' OR event_invitees.user_id=$user->user_id)
+            WHERE plans.user_id=$friend_id AND (events.privacy='open' OR event_invites.user_id=$user->user_id)
+            ORDER BY date ASC
                 ";
         $this->db->query($query);
     }
