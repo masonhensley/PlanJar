faebc702d4affc88d9c9553f74f7d3f96948b1cd
Wells Johnston <wellsjohnston@gmail.com>
2011-08-05 12:28:31 -0500

    5

--- application/models/load_friend_plans.php
+++ application/models/load_friend_plans.php
@@ -2,16 +2,32 @@
 
 class Load_friend_plans extends CI_Model
 {
+
     // Constructor.
     function __construct()
     {
         parent::__construct();
     }
-    
+
     function populate_plans($friend_id)
     {
-        echo $friend_id;
+        $this->_get_friend_plans();
     }
-    
+
+    function _get_friend_plans($friend_id)
+    {
+        $user = $this->ion_auth->get_user();
+
+        $query = "
+            SELECT plans.id, events.date, events.time, events.title, plans.event_id, places.name
+            FROM plans
+            JOIN events ON events.id=plans.event_id AND events.date>=NOW()
+            LEFT JOIN event_invitees ON event_invitees.event_id=events.id
+            JOIN places ON events.place_id=places.id
+            WHERE plans.user_id=$friend_id AND (events.privacy='open' OR event_invitees.user_id=$user->user_id
+                ";
+    }
+
 }
+
 ?>
