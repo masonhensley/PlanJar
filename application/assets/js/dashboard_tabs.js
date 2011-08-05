ee0b5f8dc531325d71e7dea3b4ad4a32ee716415
Wells Johnston <wellsjohnston@gmail.com>
2011-08-05 14:35:52 -0500

    5

--- application/assets/js/dashboard_tabs.js
+++ application/assets/js/dashboard_tabs.js
@@ -1,6 +1,18 @@
 
 $(function() {
     initialize_dashboard_tabs();
+    
+    // feedback tab
+    var uvOptions = {};
+    (function() {
+        var uv = document.createElement('script');
+        uv.type = 'text/javascript';
+        uv.async = true;
+        uv.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'widget.uservoice.com/sNVz5fZWXQnRMujcc3z4Kg.js';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(uv, s);
+    })();
+    
 });
 
 // Initializes the map/data tabs.
