1cf8eda05d70d464b35a36616eca9adeab0955f6
Wells Johnston <wellsjohnston@gmail.com>
2011-08-05 14:26:14 -0500

    5

--- application/models/load_friend_plans.php
+++ application/models/load_friend_plans.php
@@ -38,8 +38,8 @@ class Load_friend_plans extends CI_Model
         <div class="friend_plan_back_button">
             Back 
         </div>
-        <br/><br/><br/>
-        <font style="font-size:15px; color:navy; position:absolute;top:30px;left:40px;"><?php echo " " . $friend_name . "'s Plans"; ?></font>
+        <br/><br/>
+        <font style="font-size:15px; color:navy;text-align:center;"><?php echo " " . $friend_name . "'s Plans"; ?></font>
 
         <?php
         if ($plans_result->num_rows() > 0)
