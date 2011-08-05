5fc9c8afe8646427073fbacd1e344dcd1c357156
Wells Johnston <wellsjohnston@gmail.com>
2011-08-05 13:32:29 -0500

    5

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -241,7 +241,8 @@ class Home extends CI_Controller
     {
        $friend_id = $this->input->get('friend_id');
        $this->load->model('load_friend_plans');
-       $this->load_friend_plans->populate_plans($friend_id);
+       echo "hey";
+       //$this->load_friend_plans->populate_plans($friend_id);
        //echo $plans;
     }
 
