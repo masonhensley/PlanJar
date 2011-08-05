a4ed51aa54f6a41c7bb5e833b08139284572ae17
Parker Bossier <parker@planjar.com>
2011-08-05 18:33:55 -0500

    boje

--- application/assets/js/day_tabs.js
+++ application/assets/js/day_tabs.js
@@ -9,13 +9,13 @@ function initialize_day_tabs() {
         // Remove any "day_selected" class
         $("div.days_panel .day_selected").removeClass("day_selected");
         
+        // Add "day_selected" class to selected tab
+        $(this).addClass("day_selected");
+        
         // Select the current location if no other controlls are selected
         if (!controlls_are_selected()) {
             $('.network_tab[group_id="current_location"]').addClass('network_active');
         }
-        
-        // Add "day_selected" class to selected tab
-        $(this).addClass("day_selected");
         
         // Display the info box
         display_info(true);
@@ -34,6 +34,7 @@ function initialize_day_tabs() {
         get_new_days(parseInt(current_offset) + 7);
     });
     
+    // Select the first day
     $('.days_panel .day:first').addClass('day_selected');
 }
 
@@ -44,6 +45,6 @@ function get_new_days(offset) {
     }, function (data) {
         $('.seven_days').html(data);
         initialize_day_tabs();
-        $('.days_panel .day:first').click();
+        display_info();
     });
 }
