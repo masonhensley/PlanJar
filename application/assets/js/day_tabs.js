b34bda01e74b5fdabe08b35eedbab85945586ffa
Parker Bossier <parker@planjar.com>
2011-08-05 21:41:06 -0500

    boje

--- application/assets/js/day_tabs.js
+++ application/assets/js/day_tabs.js
@@ -3,7 +3,8 @@ $(function () {
 });
 
 // Set up the day of the week tabs.
-function initialize_day_tabs(day_index) {
+// If day_index is set and in range [0, 6], then the day with that index is selected
+function initialize_day_tabs(day_index, callback) {
     // Click event
     $("div.days_panel .day").click(function() {
         // Remove any "day_selected" class
@@ -36,20 +37,11 @@ function initialize_day_tabs(day_index) {
     if (day_index == undefined) {
         day_index = 0;
     }
-    $('.days_panel .day').eq(day_index).addClass('day_selected');
-}
 
-// Gets and displays the set of days
-// Note that the current day of the week selection is preserved
-function get_new_days(offset, day_index) {
-    if (offset >= 0) {
-        $.get('/home/get_weekday_tab_set', {
-            starting_offset: offset
-        }, function (data) {
-            $('.seven_days').html(data);
-            initialize_day_tabs(day_index);
-            display_info();
-        });
+    $('.days_panel .day').eq(day_index).addClass('day_selected');
+    
+    if (callback != undefined) {
+        callback();
     }
 }
 
@@ -57,7 +49,14 @@ function get_new_days(offset, day_index) {
 function goto_day_offset(offset) {
     if (offset < parseInt($('.day:first').attr('day_offset')) || offset > parseInt($('.day:last').attr('day_offset'))) {
         // Not in current seven days
-        get_new_days(Math.floor(offset/7) * 7, offset % 7);
+        $.get('/home/get_weekday_tab_set', {
+            starting_offset: Math.floor(offset/7) * 7
+        }, function (data) {
+            $('.seven_days').html(data);
+            initialize_day_tabs();
+            $('.day[day_offset="' + offset + '"]').click();
+            display_info();
+        });
     } else  {
         // This week
         $('.day').eq(offset % 7).click();
