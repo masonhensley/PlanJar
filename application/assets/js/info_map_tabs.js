c4f4e4c921e346651b58ec2d548dd4e880923deb
Parker Bossier <parker@planjar.com>
2011-08-06 16:33:01 -0500

    boje

--- application/assets/js/info_map_tabs.js
+++ application/assets/js/info_map_tabs.js
@@ -41,103 +41,101 @@ function display_info(bypass, arg) {
     }
     
     // Show the info tab
-    show_data_container('#info_content', function () {
-        if ($('.selected_location_tab').length > 0) {
-            // Location selected
-            $.get('/home/show_location_data', {
-                'place_id': $('.selected_location_tab').attr('place_id'),
-                'date': get_selected_day(),
-                'selected_groups':get_selected_groups()
-            }, function (data) {
-                initialize_location_info(data);
-            });
-        } else if ($('.network_active, .selected_group').length > 0) {
-            // Network or group selected.
-        
-            // Make 'all' the default filter setting
-            if(arg == undefined)
-            {
-                arg = 'all';
-            }
+    show_data_container('#info_content');
     
-            $.get('/home/load_data_box', {
-                'selected_groups': get_selected_groups(),
-                'selected_day': get_selected_day(),
-                'filter': arg
-            }, function (data) {
-                // Parse the JSON
-                data = $.parseJSON(data);
+    if ($('.selected_location_tab').length > 0) {
+        // Location selected
+        $.get('/home/show_location_data', {
+            'place_id': $('.selected_location_tab').attr('place_id'),
+            'date': get_selected_day(),
+            'selected_groups':get_selected_groups()
+        }, function (data) {
+            initialize_location_info(data);
+        });
+    } else if ($('.network_active, .selected_group').length > 0) {
+        // Network or group selected.
         
-                // Apply the layout HTML
-                $('#info_content').html(data.html);
-
-                // Capture the data
-                data = data.data;
-        
-                // Select the correct value for the select box
-                $('#filter').val(arg);
-        
-                // Populate the graphs
-                populate_percentage_box('.total_percent_container', data.percent_total_going_out, 'percent_bar_total');
-                populate_percentage_box('.male_percent_container', data.percent_males_going_out, 'percent_bar_male');
-                populate_percentage_box('.female_percent_container', data.percent_females_going_out, 'percent_bar_female');
-                populate_day_graph('.group_graph_top_right', data.plan_dates, data.selected_date);
-        
-                // Reload the display info when the filter select is changed
-                $('#filter').change(function(){
-                    display_info(true, $(this).val());
-                });
-            });
-        
-            populate_popular_locations();
-        } else if ($('.selected_plan').length > 0) {
-            // Plan selected (user's plan or friend's plan)
-            $.get('/home/load_selected_plan_data', {
-                'plan_selected': $('.selected_plan').attr('plan_id')
-            }, function (data) {
-                data = $.parseJSON(data);
-        
-                // Replace the data and show the data tab.
-                $('#info_content').html(data.html);
-        
-                // Handles clicking on the delete plan button
-                $('.delete_plan').confirmDiv(function (clicked_elem) {
-                    $.get('/home/delete_plan', {
-                        'plan_selected': $('.selected_plan').attr('plan_id')
-                    }, function (data) {
-                        // Replace the data and show the info tab.
-                        $('#info_content').html(data);
-                    
-                        populate_plan_panel();
-                    });
-                
-                    // Display the info box after the plan tabs HTML has been replaced
-                    populate_plan_panel(function () {
-                        display_info();
-                    });
-                });
-        
-                // Handles clicking on invite people
-                $('.invite_people').click(function () {
-                    open_invite_modal('event', data.event_id, data.privacy, data.originator);
-                });
-            });
-        } else if ($('.selected_friend_plan').length > 0) {
-            // Friend's plan selected
-            $.get('/home/show_location_data', {
-                'place_id': $('.selected_friend_plan').attr('place_id'),
-                'date': get_selected_day(),
-                'selected_groups':get_selected_groups()
-            }, function (data) {
-                initialize_location_info(data);
-            });
-        } else {
-            // No controlls selected
-            $('#info_content').html('<img src="/application/assets/images/center_display.png">');
+        // Make 'all' the default filter setting
+        if(arg == undefined)
+        {
+            arg = 'all';
         }
-    });
     
-    
+        $.get('/home/load_data_box', {
+            'selected_groups': get_selected_groups(),
+            'selected_day': get_selected_day(),
+            'filter': arg
+        }, function (data) {
+            // Parse the JSON
+            data = $.parseJSON(data);
+        
+            // Apply the layout HTML
+            $('#info_content').html(data.html);
+
+            // Capture the data
+            data = data.data;
+        
+            // Select the correct value for the select box
+            $('#filter').val(arg);
+        
+            // Populate the graphs
+            populate_percentage_box('.total_percent_container', data.percent_total_going_out, 'percent_bar_total');
+            populate_percentage_box('.male_percent_container', data.percent_males_going_out, 'percent_bar_male');
+            populate_percentage_box('.female_percent_container', data.percent_females_going_out, 'percent_bar_female');
+            populate_day_graph('.group_graph_top_right', data.plan_dates, data.selected_date);
+        
+            // Reload the display info when the filter select is changed
+            $('#filter').change(function(){
+                display_info(true, $(this).val());
+            });
+        });
+        
+        populate_popular_locations();
+    } else if ($('.selected_plan').length > 0) {
+        // Plan selected (user's plan or friend's plan)
+        $.get('/home/load_selected_plan_data', {
+            'plan_selected': $('.selected_plan').attr('plan_id')
+        }, function (data) {
+            data = $.parseJSON(data);
+        
+            // Replace the data and show the data tab.
+            $('#info_content').html(data.html);
+        
+            // Handles clicking on the delete plan button
+            $('.delete_plan').confirmDiv(function (clicked_elem) {
+                $.get('/home/delete_plan', {
+                    'plan_selected': $('.selected_plan').attr('plan_id')
+                }, function (data) {
+                    // Replace the data and show the info tab.
+                    $('#info_content').html(data);
+                    
+                    populate_plan_panel();
+                });
+                
+                // Display the info box after the plan tabs HTML has been replaced
+                populate_plan_panel(function () {
+                    display_info();
+                });
+            });
+        
+            // Handles clicking on invite people
+            $('.invite_people').click(function () {
+                open_invite_modal('event', data.event_id, data.privacy, data.originator);
+            });
+        });
+    } else if ($('.selected_friend_plan').length > 0) {
+        // Friend's plan selected
+        $.get('/home/show_location_data', {
+            'place_id': $('.selected_friend_plan').attr('place_id'),
+            'date': get_selected_day(),
+            'selected_groups':get_selected_groups()
+        }, function (data) {
+            initialize_location_info(data);
+        });
+    } else {
+        // No controlls selected
+        $('#info_content').html('<img src="/application/assets/images/center_display.png">');
+    }
 }
 
 // Sets up the location view (graphs and whatnot)
