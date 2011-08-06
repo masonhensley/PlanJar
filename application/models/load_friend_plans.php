f53a63746c0da8fb13a57e6d127f591ea93d1f6f
Parker Bossier <parker@planjar.com>
2011-08-05 22:07:16 -0500

    boje

--- application/models/load_friend_plans.php
+++ application/models/load_friend_plans.php
@@ -74,10 +74,9 @@ class Load_friend_plans extends CI_Model
 
                     // Day offset
                     $cur_date = new DateTime();
-                    echo($cur_date->format('r'));
+                    $cur_date->seTime(0, 0, 0);
                     $new_date = new DateTime($plan->date);
-                    echo($new_date->format('r'));
-                    $day_offset  = $cur_date->diff($new_date);
+                    $day_offset = $cur_date->diff($new_date);
                     $day_offset = $day_offset->format('%a');
                     ?>
                     <div class ="friend_plan_content" place_id="<?php echo $id; ?>" day_offset="<?php echo($day_offset); ?>">
