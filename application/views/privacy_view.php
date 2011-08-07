f56b0aca88bf0470fe039b3e1be1e5e3cce1e19e
Mason Hensley <masonhensley@gmail.com>
2011-08-06 20:29:19 -0500

    mixpanel

--- application/views/privacy_view.php
+++ application/views/privacy_view.php
@@ -12,6 +12,8 @@
             .arrow { top: 100%; }  
         </style>  
     <![endif]-->  
+
+<!-- Mixpanel --><script type="text/javascript">var mpq=[];mpq.push(["init","ccd5fd6c9626dca4f5a3b019fc6c7ff4"]);(function(){var a=document.createElement("script");a.type="text/javascript";a.async=true;a.src=(document.location.protocol==="https:"?"https:":"http:")+"//api.mixpanel.com/site_media/js/api/mixpanel.js";var b=document.getElementsByTagName("script")[0];b.parentNode.insertBefore(a,b)})();</script><!-- End Mixpanel -->
   
 </head>  
 <body>
