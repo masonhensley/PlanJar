3f39f79bb509bbe7ac9ec55f8c727307c7fd58a8
Wells Johnston <wellsjohnston@gmail.com>
2011-08-08 12:35:42 -0500

    5

--- application/models/load_plan_data.php
+++ application/models/load_plan_data.php
@@ -75,7 +75,7 @@ class Load_plan_data extends CI_Model
         }
 
         $data_array = array(
-            'location_id' => $row->place_id,
+            'location_id' => $plan_row->place_id,
             'number_attending' => $number_attending,
             'number_invited' => $number_invited,
             'number_males' => $number_males,
