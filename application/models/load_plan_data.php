6e07b4cab8adf88a0d97d4a8dd98b4f989f6d1ac
Parker Bossier <parker@planjar.com>
2011-08-08 17:06:17 -0500

    boje

--- application/models/load_plan_data.php
+++ application/models/load_plan_data.php
@@ -97,7 +97,7 @@ class Load_plan_data extends CI_Model
             'number_invited' => $number_invited,
             'number_attending' => $number_attending,
             'originator_name' => $originator_name,
-            'originator_id' => $plan_row->originator_id,
+            'originator_id' => $plan_row->originator_id == $this->ion_auth->get_user()->id,
             'date' => get_day_offset($plan_row->date),
             'location_id' => $plan_row->place_id,
             'percent_attending' => $percent_attending,
