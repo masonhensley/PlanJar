c878c779d67d972c180b9308c02818eaeb9b1768
Wells Johnston <wellsjohnston@gmail.com>
2011-08-08 12:32:04 -0500

    5

--- application/models/load_plan_data.php
+++ application/models/load_plan_data.php
@@ -12,7 +12,7 @@ class Load_plan_data extends CI_Model
     function display_plan_data($plan_id)
     {
         // pull all user's current events
-        $query = "SELECT events.id, events.date, events.time, events.title, events.privacy, events.originator_id, places.name
+        $query = "SELECT events.id, events.date, events.time, events.title, events.privacy, events.originator_id, places.name, places.id
             FROM plans LEFT JOIN events ON plans.event_id = events.id
             LEFT JOIN places ON events.place_id = places.id
             WHERE plans.id = $plan_id";
@@ -75,6 +75,7 @@ class Load_plan_data extends CI_Model
         }
 
         $data_array = array(
+            'location_id' => $row->places.id,
             'number_attending' => $number_attending,
             'number_invited' => $number_invited,
             'number_males' => $number_males,
