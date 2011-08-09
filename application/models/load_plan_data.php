c86fac8bfdeb8b16aac7d02d54c419e61522ba91
Wells Johnston <wellsjohnston@gmail.com>
2011-08-09 14:25:11 -0500

    pics yo

--- application/models/load_plan_data.php
+++ application/models/load_plan_data.php
@@ -159,13 +159,13 @@ class Load_plan_data extends CI_Model
 
         <div class="plan_graphs">
            
-            <div style="position:absolute; width:12px; height:12px; background-color:lightpink;top:157px; left:126px;"></div>
+            <div style="position:absolute; width:12px; height:12px; background-color:lightpink;top:157px; left:155px;"></div>
             <div style="position:absolute; width:12px; height:12px; background-color:lightblue;top:157px;left:49px;"></div>
             <div style="position:absolute; width:12px; height:12px; background-color:blueviolet;top:47px;left:49px;"></div>
  
             <div style="position:absolute;font-weight:bold;top:45px; left:64px;"><?php echo "%" .$data_array['percent_attending']; ?></div>
             <div style="position:absolute;font-weight:bold;top:155px;left:64px;"><?php echo "%" .$data_array['percent_male']; ?></div>
-            <div style="position:absolute;font-weight:bold;top:155px;left:128px;"><?php echo "%" .$data_array['percent_female']; ?></div>
+            <div style="position:absolute;font-weight:bold;top:155px;left:170px;"><?php echo "%" .$data_array['percent_female']; ?></div>
             
             <div style="position:absolute;top:47px; left:134px;font-weight: bold;">male</div>
             <div style="position:absolute;top:155px;left:130px; font-weight: bold;">female</div>
