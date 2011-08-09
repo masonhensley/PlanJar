05946ec15a9e1dab6a43edb9e664d8e910b01f35
Wells Johnston <wellsjohnston@gmail.com>
2011-08-09 14:21:30 -0500

    pics yo

--- application/models/load_plan_data.php
+++ application/models/load_plan_data.php
@@ -159,12 +159,12 @@ class Load_plan_data extends CI_Model
 
         <div class="plan_graphs">
            
-            <div style="position:absolute; width:12px; height:12px; background-color:lightpink;top:148px; left:117px;"></div>
-            <div style="position:absolute; width:12px; height:12px; background-color:lightblue;top:148px;left:65px;"></div>
+            <div style="position:absolute; width:12px; height:12px; background-color:lightpink;top:157px; left:126px;"></div>
+            <div style="position:absolute; width:12px; height:12px; background-color:lightblue;top:157px;left:49px;"></div>
             <div style="position:absolute; width:12px; height:12px; background-color:blueviolet;top:47px;left:49px;"></div>
  
-            <div style="position:absolute;font-weight:bold;top:184px; left:68px;"><?php echo "%" .$data_array['percent_attending']; ?></div>
-            <div style="position:absolute;font-weight:bold;top:6px;left:120px;"><?php echo "%" .$data_array['percent_male']; ?></div>
+            <div style="position:absolute;font-weight:bold;top:45px; left:64px;"><?php echo "%" .$data_array['percent_attending']; ?></div>
+            <div style="position:absolute;font-weight:bold;top:155px;left:64px;"><?php echo "%" .$data_array['percent_male']; ?></div>
             <div style="position:absolute;font-weight:bold;top:128px;left:128px;"><?php echo "%" .$data_array['percent_female']; ?></div>
             <!--
             <div style="position:absolute;top:20px; left:134px;font-weight: bold;">male</div>
