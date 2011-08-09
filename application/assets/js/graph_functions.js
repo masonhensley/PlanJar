b96504bc735feea6f3128e861c641869ab40aeaa
Parker Bossier <parker@planjar.com>
2011-08-09 12:01:13 -0500

    boje

--- application/assets/js/graph_functions.js
+++ application/assets/js/graph_functions.js
@@ -8,9 +8,9 @@ function populate_percentage_box(container, percentage, active_class, vertical) 
         // Compute the correct styles to add
         var style = "position: absolute; ";
         if (vertical) {
-            style += "bottom: 0px; left: 0px;"
+            style += "bottom: 0px; height: 100%;"
         } else {
-            style += "left: 0px; top: 0px;"
+            style += "left: 0px; width: 100%;"
         }
         $(container).append($('<div class="' + active_class + '" style="' + style + '"></div>'));
     }
