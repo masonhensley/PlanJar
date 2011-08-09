eb7a933bc8b1776c58681e7d98eb2cd4d252c442
Mason Hensley <masonhensley@gmail.com>
2011-08-09 15:26:34 -0500

    adding br's

--- application/views/privacy_view.php
+++ application/views/privacy_view.php
@@ -34,28 +34,28 @@
 
 We’ll also share all of this information with the government as bound by applicable laws or in accordance proper court subpoenas.
 
-Remember, when you visit our Web site, our Web site is also visiting you. And we’ve brought a dozen or more friends with us, depending on how many ad networks and third-party data services we use. We’re not going to tell which ones, though you could probably figure this out by carefully watching the different URLs that flash across the bottom of your browser as each page loads or when you mouse over various bits.  We use some data analytics packages developed by guys who specialize in that kind of stuff so we can interpret information quickly to make our product better for you, the user.  
+Remember, when you visit our Web site, our Web site is also visiting you. And we’ve brought a dozen or more friends with us, depending on how many ad networks and third-party data services we use. We’re not going to tell which ones, though you could probably figure this out by carefully watching the different URLs that flash across the bottom of your browser as each page loads or when you mouse over various bits.  We use some data analytics packages developed by guys who specialize in that kind of stuff so we can interpret information quickly to make our product better for you, the user. <br> 
 
 Furthermore, each of these sites may leave behind a little gift known as a cookie -- a text file filled with inscrutable gibberish that allows various computers around the globe to identify you, including your preferences, browser settings, which parts of the site you visited, which ads you clicked on, and whether you actually use a part of our site or not.
-
+<br>
 Those same cookies may let our advertising and data broker partners track you across other site you visit, then dump all of your information into a huge database attached to a unique ID number, which they may sell without ever notifying you or asking for permission.
-
+<br>
 Also: We collect your IP address, which might change every time you log on but probably doesn’t. At the very least, your IP address tells us the name of your ISP and the city where you live; with a legal court order, it can also give us your name and billing address (see subpoenas, above).
-
+<br>
 Besides your IP, we record some specifics about your operating system and browser. Amazingly, this information (known as your user agent string) can be enough to narrow you down to one of a few hundred people on the internet, all by its lonesome. We didn’t come up with this stuff and don’t really use it, but we may employ this information to make the site perform best for you, the user. Isn’t technology wonderful?
-
+<br>
 The data we collect is strictly anonymous, unless you’ve been kind enough to give us your name, email address, or other identifying information. And even if you have been that kind, we promise we won’t sell your contact information to anyone else because spam sucks. Of course our impossibly obtuse privacy policy can always change our minds tomorrow, but we do unto others as we would have them do to us. 
-
+<br>
 We store this information an indefinite amount of time for reasons even we don’t fully understand. And when we do eventually get around to deleting it, you can bet it’s still kicking around on some network backup drives in a storage closet. So once we have it, there’s really no getting it back. 
-
+<br>
 Not to worry, though, because we use some cutting edge security measures to protect your data against hackers and identity thieves. You’ll pretty much just have to take our word for it.
-
+<br>
 So just to recap: Your information is extremely valuable to us. Our business model would totally collapse without it. No IPO, no stock options; all those 83-hour weeks and nothing to show for it. So we’ll do our very best to use it in as many potentially profitable ways as we can conjure, over and over, while attempting to convince you there’s nothing to worry about. Sorry, we have to afford to eat and pound beer to the face too.
-
-(Did somebody hold a gun to your head and force you to visit this site? No, they did not. Did you run into a pay wall on the home page demanding your Visa number? No, you did not. You think we just give all this stuff away because we’re nice guys?  We are going to have to explore every humanly decent way to keep the lights and servers running here at PlanJar, please be patient with us, the ride should be fun.)
-
+<br>
+Did somebody hold a gun to your head and force you to visit this site? No, they did not. Did you run into a pay wall on the home page demanding your Visa number? No, you did not. You think we just give all this stuff away because we’re nice guys?  We are going to have to explore every humanly decent way to keep the lights and servers running here at PlanJar, please be patient with us, the ride should be fun.)
+<br>
 This privacy policy may change at any time. In fact, it’s changed multiple times since we first started typing. Good luck figuring out how, because we’re sure as hell not going to spam you every time we update it. But then, you probably stopped reading after paragraph three.
-
+<br>
 
 
         </div>
