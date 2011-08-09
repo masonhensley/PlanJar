bc3d36adf9e7c2b1cef43b88808f67d08a14bba1
Parker Bossier <parker@planjar.com>
2011-08-08 20:08:30 -0500

    boje

--- application/views/about_view.php
+++ application/views/about_view.php
@@ -1,72 +1,73 @@
 <html lang="en">  
-<head>  
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <head>  
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
-    <!-- CSS -->
-    <link rel=stylesheet href="/application/assets/css/about.css" type="text/css" />
-    <link href='http://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet' type='text/css'>
-    <title>About us</title>  
-  
-    <!--[if IE]>  
-        <style>  
-            .arrow { top: 100%; }  
-        </style>  
-    <![endif]-->  
-  
+        <!-- CSS -->
+        <link rel=stylesheet href="/application/assets/css/about.css" type="text/css" />
+        <link href='http://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet' type='text/css'>
+        <title>About us</title>  
 
-    <!-- Mixpanel --><script type="text/javascript">var mpq=[];mpq.push(["init","ccd5fd6c9626dca4f5a3b019fc6c7ff4"]);(function(){var a=document.createElement("script");a.type="text/javascript";a.async=true;a.src=(document.location.protocol==="https:"?"https:":"http:")+"//api.mixpanel.com/site_media/js/api/mixpanel.js";var b=document.getElementsByTagName("script")[0];b.parentNode.insertBefore(a,b)})();</script><!-- End Mixpanel -->
+        <!--[if IE]>  
+            <style>  
+                .arrow { top: 100%; }  
+            </style>  
+        <![endif]-->  
 
-</head>  
-<body>
 
- <div class ="top_panel">
+        <!-- Mixpanel --><script type="text/javascript">var mpq=[];mpq.push(["init","ccd5fd6c9626dca4f5a3b019fc6c7ff4"]);(function(){var a=document.createElement("script");a.type="text/javascript";a.async=true;a.src=(document.location.protocol==="https:"?"https:":"http:")+"//api.mixpanel.com/site_media/js/api/mixpanel.js";var b=document.getElementsByTagName("script")[0];b.parentNode.insertBefore(a,b)})();</script><!-- End Mixpanel -->
+
+    </head>  
+    <body>
+
+        <div class ="top_panel">
             <div class = "inside_top_panel">
                 <img src='/application/assets/images/pj_logo_white_text.png' style="float: left; margin-left:30px; height:80%; position:relative; top:5px;"/>
                 <div class="top_links">
                     <a href="/dashboard/profile" id="profile_link"><div class ="top_right_link_outer"><div class="top_right_link_inner">Profile</div></div></a>
-                 </div>  
-     <div id="container">  
-  
-        <h2> Our Team <span class="arrow"></span> </h2> 
+                </div>  
+                <div id="container">  
 
-        <h4>Mason Hensley, CEO & Co-Founder <span class="arrow_right"></span> </h4>
-                <div class ="person">
-                      <div class ="info">
-                           Mason mischievously guides PlanJar’s vision and strategic planning. Before PlanJar, Mason dabbled in energy efficiency analysis at a large power company in Tennessee. Mason holds a Bachelor of Engineering in Biomedical Engineering from Vanderbilt University. 
+                    <h2> Our Team <span class="arrow"></span> </h2> 
+
+                    <h4>Mason Hensley, CEO & Co-Founder <span class="arrow_right"></span> </h4>
+                    <div class ="person">
+                        <div class ="info">
+                            Mason mischievously guides PlanJar’s vision and strategic planning. Before PlanJar, Mason dabbled in energy efficiency analysis at a large power company in Tennessee. Mason holds a Bachelor of Engineering in Biomedical Engineering from Vanderbilt University. 
                         </div>
-                      <div class ="pic">
+                        <div class ="pic">
                         </div>
-        
-        </div>
-        <h4> Name <span class="arrow_right"></span> </h4>
-                <div class ="person">
-                   <div class ="info">
-                     </div>
-                   <div class ="pic">
-                     </div>
-        </div>
 
-        <h4> Name <span class="arrow_right"></span> </h4>
-                <div class ="person">
-                    <div class ="info">
-                     </div>
-                    <div class ="pic">
-                      </div>
-        
-        </div> 
-    </div>
+                    </div>
+                    <h4>Parker Bossier, Resident Bad-ass, Developer & Co-Founder<span class="arrow_right"></span> </h4>
+                    <div class ="person">
+                        <div class ="info">
+                            Coming soon...
+                        </div>
+                        <div class ="pic">
+                        </div>
+                    </div>
 
-<div class="bottom_links">
-            Bottom link content will go in here; links, names, contact info, etc. it will be epic sauce
+                    <h4> Name <span class="arrow_right"></span> </h4>
+                    <div class ="person">
+                        <div class ="info">
+                        </div>
+                        <div class ="pic">
+                        </div>
 
-            <a href="/help" id="profile_link"><div class ="bottom_link_outer"><div class="top_right_link_inner">Help & FAQ</div></div></a>
-            <a href="/about" id="profile_link"><div class ="bottom_link_outer"><div class="top_right_link_inner">About</div></div></a>
-            <a href="/privacy" id="profile_link"><div class="bottom_link_outer"><div class="top_right_link_inner">Privacy</div></div></a>
-            <a href="http://blog.planjar.com/" id="profile_link"><div class="bottom_link_outer"><div class="top_right_link_inner">Blog</div></div></a>
+                    </div> 
+                </div>
+
+                <div class="bottom_links">
+                    Bottom link content will go in here; links, names, contact info, etc. it will be epic sauce
+
+                    <a href="/help" id="profile_link"><div class ="bottom_link_outer"><div class="top_right_link_inner">Help & FAQ</div></div></a>
+                    <a href="/about" id="profile_link"><div class ="bottom_link_outer"><div class="top_right_link_inner">About</div></div></a>
+                    <a href="/privacy" id="profile_link"><div class="bottom_link_outer"><div class="top_right_link_inner">Privacy</div></div></a>
+                    <a href="http://blog.planjar.com/" id="profile_link"><div class="bottom_link_outer"><div class="top_right_link_inner">Blog</div></div></a>
 
 
 
-        </div>
-  
-</body>  
-</html>  
+                </div>
+
+                </body>  
+                </html>  
