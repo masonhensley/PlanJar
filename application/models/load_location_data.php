56b7df9d75783908051ce3964f3b438bce0ed879
Wells Johnston <wellsjohnston@gmail.com>
2011-08-09 15:11:15 -0500

    pics yo

--- application/models/load_location_data.php
+++ application/models/load_location_data.php
@@ -228,7 +228,7 @@ class Load_location_data extends CI_Model
 
         <div class="two_percent_wrapper"></div>
         
-        <div style="position:absolute; color:gray;"># people attending by day</div>
+        <div style="position:absolute; color:gray;top:42px;right:124px;"># people attending by day</div>
         <div class="day_plan_graph"></div>
 
         <div style="position:absolute; width:300px; height:150px; bottom:0px; left:0px;"></div>
