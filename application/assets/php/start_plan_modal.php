0306493a088fde029d54ef35853ad5a85bef5635
Parker Bossier <parker@planjar.com>
2011-08-09 22:20:55 -0500

    boje

--- application/assets/php/start_plan_modal.php
+++ application/assets/php/start_plan_modal.php
@@ -61,7 +61,17 @@
             </div>
         </div>
 
-<!--        <input type="button" id="submit_plan" value="Go" style="position: absolute; right: 0px; bottom: 0px;"/>-->
+        <hr/>
+        <div id="plan_create_event_buttons" style="width: 100%; text-align: right;">
+            <input type="button" id="add_plan_description" value="Add a description"/>
+
+            <div id="plan_description_wrapper">
+                <textarea rows="2" cols="50" id="plan_description" name="plan_description"></textarea>
+                <hr/>
+            </div>
+
+            <input type="button" id="submit_plan" value="Go"/>
+        </div>
 
         <input id="plan_location_id" name="plan_location_id" type="hidden"/>
         <input id="plan_location_name" type="hidden"/>
