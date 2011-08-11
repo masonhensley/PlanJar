655514df80021158961d7760448fe44f730ac767
Parker Bossier <parker@planjar.com>
2011-08-11 18:07:32 -0500

    boje

--- application/assets/js/dashboard_following.js
+++ application/assets/js/dashboard_following.js
@@ -1,11 +1,12 @@
 $(function () {
     initialize_suggested_friends();
+    initialize_follow_search();
 });
 
 // Run when the tab is selected
 function following_setup() {
     populate_following_list();
-    initialize_follow_search();
+    
 }
 
 // This sets up the suggested friends list
@@ -14,24 +15,60 @@ function initialize_suggested_friends()
     $('.suggested_friends').click(function() {
         if($(this).hasClass('suggested_active'))
         {
-            // Already selected. Clear 
+            // Already selected. Deselect and clear
             $(this).removeClass('suggested_active');
-            $('#follow_search').hide('blind', {}, 'fast');
-            $('#friend_search').val('');
-            $('#follow_search').html('');
-            $('#friend_search').blur();
+            $('#follow_search').hide('blind', {}, 'fast', function() {
+                $('#follow_search').html('');
+            });
+            
             $('#friend_search').focus();
-            initialize_follow_search();
-        }else{
-            $(this).addClass('suggested_active');
-            $('.following_profile_body').hide();
+        } else {
             // Clear the search box
             $('#friend_search').val('');
             $('#friend_search').blur();
-            get_suggested_friends();
-            $('#follow_search').show();
+            
+            // Select this.
+            $(this).addClass('suggested_active');
+            
+            // Get the suggested friends
+            $.get('/dashboard/get_suggested_friends', function (data) {
+                $('#follow_search').html(data);
+                    
+                // Hide the profile body if necessary and show the result list
+                if ($('.following_profile_body:visible').length > 0) {
+                    $('.following_profile_body').hide('fast', function() {
+                        $('#follow_search').show('fast');
+                    });
+                } else {
+                    $('#follow_search').show('fast');
+                }
+                    
+                // Add following click handler
+                $('.add_following').confirmDiv(function (clicked_elem) {
+                    $.get('/dashboard/add_user_following', {
+                        following_id: clicked_elem.parent().attr('user_id')
+                    }, function (data) {
+                        populate_following_list();
+                        get_suggested_friends();
+                    });
+                });
+            
+                // click handler for getting the profile
+                $('.user_entry').click(function(){
+                    $.get('/dashboard/get_profile', {
+                        user_id: $(this).attr('user_id')
+                    }, function (data) {
+                        $('#follow_search').hide();
+                        $('.following_profile_body').html(data);
+                        $('.following_profile_body').show("slow");
+                    });
+                });
+            });
         } 
     });
+    
+    // Refer to the definition in dashboard_view.
+    // Essentially selects the suggested button if necessary at load
     show_suggested_init('#following_content', '.suggested_friends');    
 }
 
@@ -109,30 +146,4 @@ function initialize_follow_search() {
             });
         });
     });
-}
-
-function get_suggested_friends()
-{
-    $.get('/dashboard/get_suggested_friends',
-        function (data) {
-            $('#follow_search').html(data);
-            $('.add_following').confirmDiv(function (clicked_elem) {
-                $.get('/dashboard/add_user_following', {
-                    following_id: clicked_elem.parent().attr('user_id')
-                }, function (data) {
-                    populate_following_list();
-                    get_suggested_friends();
-                });
-            });
-            
-            $('.user_entry').click(function(){ // click handler for getting the profile
-                $.get('/dashboard/get_profile', {
-                    user_id: $(this).attr('user_id')
-                }, function (data) {
-                    $('#follow_search').hide();
-                    $('.following_profile_body').html(data);
-                    $('.following_profile_body').show("slow");
-                });
-            });
-        });
 }
--- application/assets/php/dashboard_following.php
+++ application/assets/php/dashboard_following.php
@@ -2,20 +2,23 @@
     <div class="right_header">
         People you're following...
     </div>
+
     <div id="following_list"></div>
 </div>
+
 <div class="right">
     <div class ="left_header" >
         <div class="in-field_block in-field_block_group">
             <label for="friend_search" style=" color:gray;">Search for people to follow</label>
             <input type="text" class="textbox" id="friend_search" />
         </div>
+
         <div class="suggested_friends">
             <div style="position:relative; top:7px;">Suggest people</div>
         </div>
     </div>
-    <div id="follow_search">
-    </div>
-</div>
-<div class="following_profile_body">
+
+    <div id="follow_search"></div>
+
+    <div class="following_profile_body"></div>
 </div>
