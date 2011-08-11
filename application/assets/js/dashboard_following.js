fb65b537443da059f10930cc45cdce0991e806aa
Parker Bossier <parker@planjar.com>
2011-08-11 18:41:38 -0500

    boje

--- application/assets/js/dashboard_following.js
+++ application/assets/js/dashboard_following.js
@@ -6,12 +6,63 @@ $(function () {
 // Run when the tab is selected
 function following_setup() {
     populate_following_list();
-    
 }
 
 // This sets up the suggested friends list
 function initialize_suggested_friends()
 {
+    // In-field label
+    $('#following_content .in-field_block label').inFieldLabels();
+    
+    // Search for friends
+    $('#friend_search').keyup(function () {
+        $('.following_profile_body').hide('fast');
+        $('.suggested_active').removeClass('suggested_active'); // this unselects the "suggested friends" tab
+        
+        $.get('/dashboard/follow_search', {
+            needle: $(this).val()
+        }, function (data) {
+            $('#follow_search').html(data);
+            $('#follow_search').show('blind', {}, 'fast');
+            
+            // Friend search user click handler
+            $('#follow_search .user_entry').click(function(){
+                // Deselect any of the selected user's followers
+                $('#following_list .selected_follower').removeClass('selected_follower');
+                
+                $.get('/dashboard/get_profile', {
+                    user_id: $(this).attr('user_id')
+                }, function (data) {
+                    $('.following_profile_body').html(data);
+                    
+                    // Hide the search body if necessary and show the profile
+                    if ($('#follow_search:visible').length > 0) {
+                        $('#follow_search').hide('fast', function() {
+                            $('.following_profile_body').show('fast');
+                        });
+                    } else {
+                        $('.following_profile_body').show('fast');
+                    }
+                });
+            });
+            
+            // Follow click handler
+            $('#follow_search .add_following').confirmDiv(function (clicked_elem) {
+                var following_id = clicked_elem.parent().attr('user_id');
+                
+                $.get('/dashboard/add_user_following', {
+                    'following_id': following_id
+                }, function () {
+                    populate_following_list(function() {
+                        // Click the resulting entry on the left
+                        $('#following_list .user_entry[user_id="' + following_id + '"]').click();
+                    });
+                });
+            });
+        });
+    });
+    
+    // Toggle suggested friends
     $('.suggested_friends').click(function() {
         if($(this).hasClass('suggested_active'))
         {
@@ -54,13 +105,13 @@ function initialize_suggested_friends()
                 });
             
                 // click handler for getting the profile
-                $('.user_entry').click(function(){
+                $('#follow_search .user_entry').click(function(){
                     $.get('/dashboard/get_profile', {
                         user_id: $(this).attr('user_id')
                     }, function (data) {
-                        $('#follow_search').hide();
+                        $('#follow_search').hide('blind', {}, 'fast');
                         $('.following_profile_body').html(data);
-                        $('.following_profile_body').show("slow");
+                        $('.following_profile_body').show("fast");
                     });
                 });
             });
@@ -73,22 +124,21 @@ function initialize_suggested_friends()
 }
 
 // Populates the following list and assigns the click events.
-function populate_following_list() {
-    
+function populate_following_list(callback) {
     $.get('/dashboard/get_following', function (data) {
         $('#following_list').html(data);
        
-        // Click handler.
+        // Unfollow click handler
         $('#following_list .remove_following').confirmDiv(function (clicked_elem) {
             $.get('/dashboard/remove_following', {
                 following_id: clicked_elem.parent().attr('user_id')
             }, function (data) {
-                populate_following_list();
-                populate_followers_list();           
+                populate_following_list();         
             });
         });
         
-        $('.user_entry').click(function(){
+        // User entry click handler
+        $('#following_list .user_entry').click(function(){
             $('.suggested_active').removeClass('suggested_active');
             $('#follow_search').hide();
             if(!$(this).hasClass('selected_follower'))
@@ -100,50 +150,19 @@ function populate_following_list() {
                     user_id: $(this).attr('user_id')
                 }, function (data) {
                     $('.following_profile_body').html(data);
-                    $('.following_profile_body').show("slow");
+                    if ($('#follow_search').is(':visible')) {
+                        $('#follow_search').hide('blind', {}, 'fast', function() {
+                            $('.following_profile_body').show("fast");
+                        });
+                    } else {
+                        $('.following_profile_body').show("fast");
+                    }
                 });
             }
         });
-
     });
-}
-
-function initialize_follow_search() {
-    // In-field labels;
-    $('.in-field_block label').inFieldLabels();
     
-    $('#friend_search').keyup(function () {
-        $('.following_profile_body').hide('fast');
-        $('.suggested_active').removeClass('suggested_active'); // this unselects the "suggested friends" tab first
-        $.get('/dashboard/follow_search', {
-            needle: $(this).val()
-        }, function (data) {
-            $('#follow_search').html(data);
-            $('#follow_search').show("slow");
-            
-            
-            $('.user_entry').click(function(){ // click handler for getting the profile
-                $.get('/dashboard/get_profile', {
-                    user_id: $(this).attr('user_id')
-                }, function (data) {
-                    $('#follow_search').hide();
-                    $('.following_profile_body').html(data);
-                    $('.following_profile_body').show("slow");
-                });
-            });
-            
-            // Click handler.
-            $('#follow_search .add_following').confirmDiv(function (clicked_elem) {
-                $.get('/dashboard/add_user_following', {
-                    following_id: clicked_elem.parent().attr('user_id')
-                }, function () {
-                    $('#follow_search').hide();
-                    $('#follow_search').html('');
-                    $('#friend_search').val('');
-                    populate_following_list();
-                    $('#friend_search').blur();
-                });
-            });
-        });
-    });
+    if (callback != undefined) {
+        callback();
+    }
 }
