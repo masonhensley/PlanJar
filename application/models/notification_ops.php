569556ad0de2e2bd246546196630b9c2b0c7c64a
Parker Bossier <parker@planjar.com>
2011-08-10 23:04:32 -0500

    boje

--- application/models/notification_ops.php
+++ application/models/notification_ops.php
@@ -40,7 +40,8 @@ class Notification_ops extends CI_Model
             {
                 if ($joined_user != $this->ion_auth->get_user()->id)
                 {
-                    $values_string .= "(DEFAULT, $joined_user, $group_id, " . $this->ion_auth->get_user()->id . ", $date, $type, $subject_id, DEFAULT, DEFAULT), ";
+                    $accepted = $this->deduce_accepted($type, $subject_id);
+                    $values_string .= "(DEFAULT, $joined_user, $group_id, " . $this->ion_auth->get_user()->id . ", $date, $type, $subject_id, $accepted, $accepted), ";
                 }
             }
         }
