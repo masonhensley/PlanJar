2f8609e668bfe7fdb1643af74082af1f6a246b98
Parker Bossier <parker@planjar.com>
2011-08-10 23:16:05 -0500

    boje

--- application/models/notification_ops.php
+++ application/models/notification_ops.php
@@ -25,6 +25,7 @@ class Notification_ops extends CI_Model
             if ($user_id != $this->ion_auth->get_user()->id)
             {
                 $accepted = $this->deduce_accepted($type, $subject_id);
+                var_dump($accepted);
                 $values_string .= "(DEFAULT, $user_id, DEFAULT, " . $this->ion_auth->get_user()->id . ", $date, $type, $subject_id, $accepted, $accepted), ";
             }
         }
@@ -41,6 +42,7 @@ class Notification_ops extends CI_Model
                 if ($joined_user != $this->ion_auth->get_user()->id)
                 {
                     $accepted = $this->deduce_accepted($type, $subject_id);
+                    var_dump($accepted);
                     $values_string .= "(DEFAULT, $joined_user, $group_id, " . $this->ion_auth->get_user()->id . ", $date, $type, $subject_id, $accepted, $accepted), ";
                 }
             }
@@ -348,9 +350,6 @@ class Notification_ops extends CI_Model
             // Follow notification
             case 'follow_notif':
                 $this->load->model('follow_ops');
-                var_dump($this->follow_ops->is_following($user_id, $subject_id));
-                var_dump($user_id);
-                var_dump($subject_id);
                 return $this->follow_ops->is_following($user_id, $subject_id);
         }
     }
