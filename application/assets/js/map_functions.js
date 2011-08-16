6aaba64646303556feb8a0f25c05eaf7e2b2e137
Parker Bossier <parker@planjar.com>
2011-08-16 12:51:19 -0500

    boje

--- application/assets/js/map_functions.js
+++ application/assets/js/map_functions.js
@@ -21,6 +21,7 @@ function populate_map(data) {
             title: data[i][0],
             icon: '/application/assets/images/map_markers/marker' + (i + 1) + '.png'
         });
+        console.log('/application/assets/images/map_markers/marker' + (i + 1) + '.png');
         
         // Assign the click event
         google.maps.event.addListener(temp_marker, 'click', function() {
