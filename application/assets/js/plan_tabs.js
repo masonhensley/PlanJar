f8d123756650e4b9ad21f825c29d2f864679ab64
Wells Johnston <wellsjohnston@gmail.com>
2011-08-17 12:10:07 -0400

    view attending list

--- application/assets/js/plan_tabs.js
+++ application/assets/js/plan_tabs.js
@@ -30,15 +30,15 @@ function initialize_plan_panel(){
         // Display the info box
         display_info();
         
-    // not sure how to make this be called after display_info() is done
-    // setup the view attendees button
-    }, function(){
+        // not sure how to make this be called after display_info() is done
+        // setup the view attendees button
         $('#view_attendees').click(function(){
-            alert('hey');
             $.get('/home/attending_list', {
                 plan_id : $('.selected_plan').attr('plan_id')
             });    
         });
+    
+        
     });
 }
 
