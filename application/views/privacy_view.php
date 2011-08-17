9c77e9cdcd17ef00721cd6ae950e49d82334338c
Mason Hensley <masonhensley@gmail.com>
2011-08-17 12:28:36 -0500

    Edited application/views/privacy_view.php via GitHub

--- application/views/privacy_view.php
+++ application/views/privacy_view.php
@@ -1,6 +1,8 @@
 <html lang="en">  
 <head>  
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    
+    <script type="text/javascript">var _sf_startpt=(new Date()).getTime()</script>
 
     <!-- CSS -->
     <link rel=stylesheet href="/application/assets/css/privacy.css" type="text/css" />
@@ -62,7 +64,26 @@ This privacy policy may change at any time. In fact, it’s changed multiple tim
         
         </div> 
     </div>
+        
+        <script type="text/javascript">
+            var _sf_async_config={uid:27655,domain:"testing.pagodabox.com"};
+            (function(){
+              function loadChartbeat() {
+                window._sf_endpt=(new Date()).getTime();
+                var e = document.createElement('script');
+                e.setAttribute('language', 'javascript');
+                e.setAttribute('type', 'text/javascript');
+                e.setAttribute('src',
+                   (("https:" == document.location.protocol) ? "https://a248.e.akamai.net/chartbeat.download.akamai.com/102508/" : "http://static.chartbeat.com/") +
+                   "js/chartbeat.js");
+                document.body.appendChild(e);
+              }
+              var oldonload = window.onload;
+              window.onload = (typeof window.onload != 'function') ?
+                 loadChartbeat : function() { oldonload(); loadChartbeat(); };
+            })();
 
+        </script>
   
 </body>  
 </html>  
