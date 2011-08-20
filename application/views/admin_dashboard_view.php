0ff817a76de7761496c420846dfa7f44247f003a
Mason Hensley <masonhensley@gmail.com>
2011-08-20 03:16:26 -0500

    Edited application/views/admin_dashboard_view.php via GitHub

--- application/views/admin_dashboard_view.php
+++ application/views/admin_dashboard_view.php
@@ -12,4 +12,86 @@
         <script type="text/javascript" src="/application/assets/js/admin_dashboard.js"></script>
 
     </head>
+    
+    
+        <body>
+
+        <div class ="top_panel">
+            <div class = "inside_top_panel">
+                <img src='/application/assets/images/pj_logo_white_text.png' style="float: left; margin-left:30px; height:80%; position:relative; top:5px;"/>
+                <div class="top_links">
+                    <a href="/dashboard/profile" id="profile_link"><div class ="top_right_link_outer"><div class="top_right_link_inner">Profile</div></div></a>
+                </div>  
+                <div id="container">  
+
+                    <h2> Our Team <span class="arrow"></span> </h2> 
+
+                    <h4>Mason Hensley, Director of Black Operations, CEO & Co-Founder <span class="arrow_right"></span> </h4>
+                    <div class ="person">
+                        <div class ="info">
+                            Mason Hensley is Co-Founder and CEO of PlanJar.  Prior to founding PlanJar, he briefly worked at the energy efficiency team at the Tennessee Valley Authority. <br> <br>
+                            Mason holds a bachelor’s degree in Biomedical Engineering from Vanderbilt University; while there, he dabbled a little in Financial Economics. He’s also an Eagle Scout.
+
+                        </div>
+                        <div class ="pic">
+                        </div>
+
+                    </div>
+                    <h4>Parker Bossier, Director of Tactical Operations, Software Architect & Co-Founder &#3232;_&#3232;<span class="arrow_right"></span> </h4>
+                    <div class ="person">
+                        <div class ="info">
+                            Parker leads the architecture and development teams with Wells (currently the only other member of said teams).
+                            Prior to this venture, Parker worked in IT for the legal sector and designed the VSVS online application as VSVS IT Chair.
+                            <br/><br/>
+                            Academically, Parker expects to graduate from Vanderbilt University in 2012 with a bachelor's degree in Computer Science and Math.
+                        </div>
+                        <div class ="pic">
+                        </div>
+                    </div>
+
+                    <h4> Wells Johnston, Director of Field Operations, Software Architect & Co-Founder ¯\_(ツ)_/¯<span class="arrow_right"></span> </h4>
+                    <div class ="person">
+                        <div class ="info">
+                            Wells tactfully runs PlanJar’s engineering team with Parker, and directs the site design and back-end architecture.  Before PlanJar, Wells worked as a programmer at Moontoast.com, as well as a freelance web developer. <br><br> Outside the PlanJar, Wells is currently pursuing a Bachelor of Science in both Computer Science and Mathematics from Vanderbilt University.
+                        </div>
+                        <div class ="pic">
+                        </div>
+
+                    </div> 
+                </div>
+
+                <div class="bottom_links">
+                    Bottom link content will go in here; links, names, contact info, etc. it will be epic sauce
+
+                    <a href="/help" id="profile_link"><div class ="bottom_link_outer"><div class="top_right_link_inner">Help & FAQ</div></div></a>
+                    <a href="/about" id="profile_link"><div class ="bottom_link_outer"><div class="top_right_link_inner">About</div></div></a>
+                    <a href="/privacy" id="profile_link"><div class="bottom_link_outer"><div class="top_right_link_inner">Privacy</div></div></a>
+                    <a href="http://blog.planjar.com/" id="profile_link"><div class="bottom_link_outer"><div class="top_right_link_inner">Blog</div></div></a>
+
+
+
+                </div>
+                    
+                    <script type="text/javascript">
+                        var _sf_async_config={uid:27655,domain:"testing.pagodabox.com"};
+                        (function(){
+                          function loadChartbeat() {
+                            window._sf_endpt=(new Date()).getTime();
+                            var e = document.createElement('script');
+                            e.setAttribute('language', 'javascript');
+                            e.setAttribute('type', 'text/javascript');
+                            e.setAttribute('src',
+                               (("https:" == document.location.protocol) ? "https://a248.e.akamai.net/chartbeat.download.akamai.com/102508/" : "http://static.chartbeat.com/") +
+                               "js/chartbeat.js");
+                            document.body.appendChild(e);
+                          }
+                          var oldonload = window.onload;
+                          window.onload = (typeof window.onload != 'function') ?
+                             loadChartbeat : function() { oldonload(); loadChartbeat(); };
+                        })();
+                        
+                    </script>
+                    
+                </body> 
+    
 </html>
