011a86fb5e71718895cef8a65d946d759e3d8a8d
Mason Hensley <masonhensley@gmail.com>
2011-08-20 05:57:01 -0500

    Edited application/errors/error_404.php via GitHub

--- application/errors/error_404.php
+++ application/errors/error_404.php
@@ -12,26 +12,12 @@
         <br>
         <br>
         
-        <a href="/home" data-role="button" data-inline="true">Click Here for stuff that works</a>
-    
-    
-        <a href="/help" data-role="button" data-inline="true">Help</a>
-        
-        <div id="button_div">
-                <div style="margin-top:10px; margin-bottom:10px;">
-                    <input class="buttons" type="submit" value="Sign up">
-                </div>
-            </div>
 
         <div class="tab_container">
-                <div class="tab" assoc_div="#profile_content">Profile</div>
-                <div class="tab" assoc_div="#groups_content">Groups</div>
-                <div class="tab" assoc_div="#friends_content">Friends</div>
-                <div class="tab" assoc_div="#following_content">Following</div>
-                <div class="tab" assoc_div="#notifications_content">Notifications</div>
-                <div class="tab" assoc_div="#settings_content">Settings</div>
+                <div class="tab" assoc_div="#profile_content">Return Home</div>
+                <div class="tab" assoc_div="#groups_content">About</div>
 
-                <div id="create_group">+ Create Group</div>
+
             </div>
 
 
