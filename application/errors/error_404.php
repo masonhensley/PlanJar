c7f4d57bc736aad05caa1734417523d1e5b09ce0
Parker Bossier <parker@planjar.com>
2011-08-19 23:03:11 -0500

    boje

--- application/errors/error_404.php
+++ application/errors/error_404.php
@@ -1,6 +1,6 @@
 <html>
     <head>
-        <title>404 Page Not Found</title>
+        <title>404 - Jar Not Found</title>
     </head>
     <body style="text-align: center;">
         <img src="/application/assets/images/404.png" style="width: 500px; height: 449px"/>
