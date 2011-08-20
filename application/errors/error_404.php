6e4a7af235fa2d9ab8a19e0cb637565f9b1ba938
Mason Hensley <masonhensley@gmail.com>
2011-08-20 05:36:52 -0500

    Edited application/errors/error_404.php via GitHub

--- application/errors/error_404.php
+++ application/errors/error_404.php
@@ -5,8 +5,11 @@
     <body style="text-align: center;">
         <img src="/application/assets/images/404.png" style="width: 500px; height: 449px"/>
         
-    <a href="/home" data-role="button" data-inline="true">Click Here for stuff that works</a>
-    <a href="/help" data-role="button" data-inline="true">Help</a>
+        <br>
+        <br>
+        
+        <a href="/home" data-role="button" data-inline="true">Click Here for stuff that works</a>
+        <a href="/help" data-role="button" data-inline="true">Help</a>
         
     </body>
 </html>
