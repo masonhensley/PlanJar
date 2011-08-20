b0c8e8c0669ebae693a8d0efddaddbd1191b6968
Mason Hensley <masonhensley@gmail.com>
2011-08-20 05:42:43 -0500

    Edited application/errors/error_404.php via GitHub

--- application/errors/error_404.php
+++ application/errors/error_404.php
@@ -8,8 +8,44 @@
         <br>
         <br>
         
-        <a href="/home" data-role="button" data-inline="true">Click Here for stuff that works</a>
-        <a href="/help" data-role="button" data-inline="true">Help</a>
+        <a href="/home" data-role="button" data-inline="true" style: "-webkit-box-shadow: 0 1px 3px rgba(0,0,0,0.5);
+    -moz-box-shadow: 0 1px 3px rgba(0,0,0,0.5);
+    box-shadow: 0 1px 3px rgba(0,0,0,0.5);
+    background: -webkit-gradient(linear,left top,left bottom,from(#F3F6FF),to(#EEE));
+    background: -webkit-linear-gradient(top, #F3F6FF, #EEE);
+    background: -moz-linear-gradient(top, #F3F6FF, #EEE);
+    background: -o-linear-gradient(top, #F3F6FF, #EEE);
+    background: -ms-linear-gradient(top, #F3F6FF, #EEE);
+    background: linear-gradient(top, #F3F6FF, #EEE);
+    position: relative;
+    float: left;
+    margin-right: 10px;
+    width: 100px;
+    height: auto;
+    color: black;
+    text-align: center;
+    padding: 10px;
+    border-radius: 5px;">Click Here for stuff that works</a>
+    
+    
+        <a href="/help" data-role="button" data-inline="true" style: "-webkit-box-shadow: 0 1px 3px rgba(0,0,0,0.5);
+    -moz-box-shadow: 0 1px 3px rgba(0,0,0,0.5);
+    box-shadow: 0 1px 3px rgba(0,0,0,0.5);
+    background: -webkit-gradient(linear,left top,left bottom,from(#F3F6FF),to(#EEE));
+    background: -webkit-linear-gradient(top, #F3F6FF, #EEE);
+    background: -moz-linear-gradient(top, #F3F6FF, #EEE);
+    background: -o-linear-gradient(top, #F3F6FF, #EEE);
+    background: -ms-linear-gradient(top, #F3F6FF, #EEE);
+    background: linear-gradient(top, #F3F6FF, #EEE);
+    position: relative;
+    float: left;
+    margin-right: 10px;
+    width: 100px;
+    height: auto;
+    color: black;
+    text-align: center;
+    padding: 10px;
+    border-radius: 5px;">Help</a>
         
     </body>
 </html>
