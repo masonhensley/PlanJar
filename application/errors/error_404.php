f17c53f91dab2c544cc216d895cbed577c6df815
Parker Bossier <parker@planjar.com>
2011-08-19 23:02:12 -0500

    boje

--- application/errors/error_404.php
+++ application/errors/error_404.php
@@ -2,7 +2,7 @@
     <head>
         <title>404 Page Not Found</title>
     </head>
-    <body>
-        <img src="/application/assets/images/404.png" style="width: 500px; height: 449px; margin: 0px auto;"/>
+    <body style="text-align: center;">
+        <img src="/application/assets/images/404.png" style="width: 500px; height: 449px"/>
     </body>
 </html>
