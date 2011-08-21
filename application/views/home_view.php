93d57f54d18443a877c7a52a8332aabdea40a5a5
Wells Johnston <wellsjohnston@gmail.com>
2011-08-20 20:39:07 -0400

    spinner

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -133,6 +133,8 @@
 
                 <?php include(APPPATH . 'assets/php/load_group_panel.php'); ?>
             </div>
+            
+            <div id="home_data_spinner" style="position:absolute; left:50%; right:50%; top:200px;"></div>
             <div class ="center">
                 <div class="data_container_wrapper" style="display: block;">
                     <div id="info_content" class ="data_container" style="display: block;"></div>
