e12181c7c24cbcf85ce0a1dbf1fc9507ec936fe7
Wells Johnston <wellsjohnston@gmail.com>
2011-08-20 20:10:52 -0400

    spinner

--- application/assets/php/dashboard_groups.php
+++ application/assets/php/dashboard_groups.php
@@ -14,7 +14,7 @@
     Select a group on the left or right to see its profile
     </div>
 </div>
-<div id="group_middle_spinner" style="position:absolute; left: 314px; top:22px;">
+<div id="group_middle_spinner" style="position:absolute; left: 470px; top:127px;">
 </div>
 
 <div class="right">
