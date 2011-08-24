8054e24d274f1aa39e989b913f0276315406ab82
Wells Johnston <wellsjohnston@gmail.com>
2011-08-24 18:21:25 -0500

    spinner

--- application/models/load_plan_comments.php
+++ application/models/load_plan_comments.php
@@ -13,7 +13,7 @@ Class Load_plan_comments extends CI_Model
         ob_start();
         ?>
         <font style="color:darkgray;">
-        <textarea id="box_text_area" name="comments" cols="30" rows="3" maxlength="139" style="height:45px;margin-top:2px;" >Comment me bro</textarea>
+        <textarea id="box_text_area" name="comments" cols="30" rows="3" maxlength="139" >Comment me bro</textarea>
         </font>
         <?php
         return ob_get_clean();
