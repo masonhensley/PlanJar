7e3b0111fd50e1ed9b48d2d1906597881fe32182
Wells Johnston <wellsjohnston@gmail.com>
2011-08-23 23:15:43 -0500

    spinner

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -217,7 +217,7 @@
                 <input type="button" id="cancel_group_member_panel" style="float:right;" value="X" />
             </div>
             <div id="group_member_content">
-                <div class="member_list" style="max-height: 344px;">
+                <div class="member_list" style="max-height: 344px; overflow: auto;">
                 </div>    
             </div>
         </div>
