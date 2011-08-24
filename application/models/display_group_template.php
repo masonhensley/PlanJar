7d52448ff46bc26f010a8a0b907779835c8aece0
Wells Johnston <wellsjohnston@gmail.com>
2011-08-23 23:07:56 -0500

    spinner

--- application/models/display_group_template.php
+++ application/models/display_group_template.php
@@ -519,17 +519,17 @@ class Display_group_template extends CI_Model
             if ($format_type == 'groups' && count($selected_groups) == 1)
             {
                 ?>
-                <div id="view_group_list" style="color:navy; position:absolute; top:23px; left:0px">See Members</div>
+                <div id="view_group_list" style="color:navy; position:absolute; top:46px; left:0px">See Members</div>
                 <?php
             } else if ($format_type == 'friends')
             {
                 ?>
-                <a href="/dashboard/friends/" id="friends_link"><div style="color:navy; position:absolute; top:23px; left:0px">See Friends</div></a>
+                <a href="/dashboard/friends/" id="friends_link"><div style="color:navy; position:absolute; top:46px; left:0px">See Friends</div></a>
                 <?php
             } else if ($format_type == 'school')
             {
                 ?> 
-                <a href="/dashboard/groups/" id="groups_link"><div style="color:navy; position:absolute; top:23px; left:0px">See School</div></a>
+                <a href="/dashboard/groups/" id="groups_link"><div style="color:navy; position:absolute; top:46px; left:0px">See School</div></a>
                 <?php
             }
             ?>
