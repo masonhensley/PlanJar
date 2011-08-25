cda4097a75bd905161376628f1781c0ed40f6626
Parker Bossier <parker@planjar.com>
2011-08-24 23:47:58 -0500

    boje

--- application/assets/js/find_places.js
+++ application/assets/js/find_places.js
@@ -5,7 +5,7 @@ $(function() {
 function initialize_find_places() {
     $('#find_places').click(function() {
         $(this).addClass('selected');
-        show_data_container('#info_content')
+        show_data_container('#info_content');
         display_info();
     });
 }
