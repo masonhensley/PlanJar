1da5f12f32eac1581dd4762b1e35e9c32fcbc132
Wells Johnston <wellsjohnston@gmail.com>
2011-08-24 21:42:55 -0500

    spinner

--- application/models/load_plan_comments.php
+++ application/models/load_plan_comments.php
@@ -13,9 +13,11 @@ Class Load_plan_comments extends CI_Model
         ob_start();
         ?>
         <font style="color:darkgray;">
+        <font style="color:gray">
         <textarea id="comment_area" name="comments" cols="30" rows="3" maxlength="139"  style="float:left;height:45px;margin-top:2px;" >
-                <font style="color:gray">Leave a comment for this event...</font>
+                        Leave a comment for this event...
         </textarea>
+        </font>
         </font>
         <div class="submit_comment">Submit</div>
         <?php
