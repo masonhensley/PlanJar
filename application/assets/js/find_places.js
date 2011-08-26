763974bc98a136140a456f182c3fa62c558d5687
Parker Bossier <parker@planjar.com>
2011-08-26 09:52:48 -0500

    boje

--- application/assets/js/find_places.js
+++ application/assets/js/find_places.js
@@ -4,6 +4,8 @@ $(function() {
 
 function initialize_find_places() {
     $('#find_places').click(function() {
+        deselect_all_controlls();
+        
         $(this).addClass('selected');
         show_data_container('#info_content');
         display_info();
--- application/assets/php/dashboard_settings.php
+++ application/assets/php/dashboard_settings.php
@@ -9,34 +9,6 @@
             </td>
         </tr>
 
-        <form id="=change_password">
-            <tr>
-                <td rowspan="3">Change your password</td>
-                <td>
-                    <div class="in-field_block">
-                        <label for="old_password">Old password</label>
-                        <input type="text" id="old_password" name="old_password"/>
-                    </div>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <div class="in-field_block">
-                        <label for="new_password">New password</label>
-                        <input type="text" id="new_password" name="new_password"/>
-                    </div>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <div class="in-field_block">
-                        <label for="new_password_1">Re-enter new password</label>
-                        <input type="text" id="new_password_1"/>
-                    </div>
-                </td>
-            </tr>
-        </form>
-
         <tr>
             <td>Change your email</td>
             <td>
