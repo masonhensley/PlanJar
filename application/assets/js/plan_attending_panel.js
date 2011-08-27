a28aaac05f1adb292e99ef43f870a77fd4f192a4
Wells Johnston <wellsjohnston@gmail.com>
2011-08-27 10:00:00 -0500

    awaiting reply

--- application/assets/js/plan_attending_panel.js
+++ application/assets/js/plan_attending_panel.js
@@ -61,7 +61,7 @@ function following_click_handler(){
     // Add following click handler
     $('.attending_list .add_following').confirmDiv(function(clicked_elem) {
         $.get('/dashboard/add_user_following', {
-            following_id: clicked_elem.parent().attr('user_id')
+            following_id: $('.user_entry').attr('user_id')
         }, function () {
             populate_plan_attending_panel();
         });
