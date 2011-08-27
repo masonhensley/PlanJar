9823116f3d5f93372f14d26b1638bab1d0a80f53
Parker Bossier <parker@planjar.com>
2011-08-26 21:30:47 -0500

    boje

--- application/assets/php/dashboard_settings.php
+++ application/assets/php/dashboard_settings.php
@@ -9,16 +9,35 @@
             </td>
         </tr>
 
-        <tr>
-            <td>Change your email</td>
-            <td>
-                <form id="=change_email">
+        <form id="change_password">
+            <tr>
+                <td rowspan="3">Change your password</td>
+
+                <td>
                     <div class="in-field_block">
-                        <label for="old_password">New email</label>
-                        <input type="text" id="new_email" name="new_email"/>
+                        <label for="old_password">Old password</label>
+                        <input type="text" id="old_password" name="old_passwor"/>
                     </div>
-                </form>
-            </td>
-        </tr>
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <div class="in-field_block">
+                        <label for="new_password">New password</label>
+                        <input type="text" id="new_password" name="new_password"/>
+                    </div>
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <div class="in-field_block">
+                        <label for="new_password_1">Re-enter new password</label>
+                        <input type="text" id="new_password_1" name="new_password_1"/>
+                    </div>
+                </td>
+            </tr>
+        </form>
     </table>
 </div>
