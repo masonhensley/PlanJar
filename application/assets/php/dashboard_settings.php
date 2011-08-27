f9a7f6eaaf147390ee66a3e6d56e321b1dca7962
Parker Bossier <parker@planjar.com>
2011-08-27 17:06:13 -0500

    boje

--- application/assets/php/dashboard_settings.php
+++ application/assets/php/dashboard_settings.php
@@ -62,7 +62,7 @@
 
         <tr>
             <td></td>
-            <td style="color: navy;"><?php echo(anchor('auth/forgot_password', 'Forgot password?')); ?></td>
+            <td><?php echo(anchor('auth/forgot_password', 'Forgot password?')); ?></td>
         </tr>
 
         <tr>
