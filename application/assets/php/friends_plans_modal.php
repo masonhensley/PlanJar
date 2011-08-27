1a96b9d371f5cea2d35b39acf57973699f64d7f9
Wells Johnston <wellsjohnston@gmail.com>
2011-08-27 14:57:59 -0500

    awaiting reply

--- application/assets/php/friends_plans_modal.php
+++ application/assets/php/friends_plans_modal.php
@@ -8,8 +8,7 @@
         Select a friend to view their upcoming plans
         <br/><hr/>
         <div class="friend_list">
-            <div class="friend_tab" user_id="all">All Upcoming Friends' Plans</div>
-            <br/><hr/>
+            <div class="friend_tab" user_id="all">All Upcoming Friends' Plans</div><hr/>
             <?php
             foreach ($friend_names as $id => $name)
             {
