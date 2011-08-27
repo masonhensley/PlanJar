4463bc388701ff0f563449f790055cec2c9bd347
Wells Johnston <wellsjohnston@gmail.com>
2011-08-27 12:21:33 -0500

    awaiting reply

--- application/models/load_profile.php
+++ application/models/load_profile.php
@@ -239,7 +239,7 @@ class Load_profile extends CI_Model
                 if ($index < 10)// limit display to ten
                 {
                     ?><font style="color:navy;"><?php echo $location . " ($count)"; ?></font><?php
-                    if ($index + 1 != $most_visited_count)
+                    if ($index + 1 != 10)
                     {
                         ?><font style="color:black;"><?php echo ", "; ?></font><?php
                     }
