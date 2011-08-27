dceb37df3e6a774129d3306754ed8b919adc1ad3
Wells Johnston <wellsjohnston@gmail.com>
2011-08-27 09:52:37 -0500

    awaiting reply

--- application/assets/js/plan_attending_panel.js
+++ application/assets/js/plan_attending_panel.js
@@ -19,38 +19,40 @@ function populate_plan_attending_panel() {
         plan_id : $('#view_attendees').attr('plan_id')
     }, function(data){
         $('#awaiting_list').html(data);
+    }).complete(function(){ // when the awaiting list is fetched, get the attendees and assign follow click handler
+        $.get('/home/attending_list', {
+            plan_id : $('#view_attendees').attr('plan_id')
+        }, function(data){
+            $('.attending_list').html(data);
+            $('#plan_attending_panel').show('fast');
+            
+            following_click_handler();
+        
+            $('.attending_button').click(function(){
+                if(!$(this).hasClass('guest_list_button_selected'))
+                {
+                    $('.guest_list_button_selected').removeClass('guest_list_button_selected');
+                    $(this).addClass('guest_list_button_selected');
+                    $('#awaiting_reply').hide();
+                    $('#attending_modal_content').show();
+                }
+            });
+        
+            $('.awaiting_button').click(function(){
+                if(!$(this).hasClass('guest_list_button_selected'))
+                {
+                    $('.guest_list_button_selected').removeClass('guest_list_button_selected');
+                    $(this).addClass('guest_list_button_selected');
+                    $('#attending_modal_content').hide();
+                    $('#awaiting_reply').show();
+                }
+            
+            });
+        
+        });
     });
     
-    $.get('/home/attending_list', {
-        plan_id : $('#view_attendees').attr('plan_id')
-    }, function(data){
-        $('.attending_list').html(data);
-        $('#plan_attending_panel').show('fast');
-            
-        following_click_handler();
-        
-        $('.attending_button').click(function(){
-            if(!$(this).hasClass('guest_list_button_selected'))
-            {
-                $('.guest_list_button_selected').removeClass('guest_list_button_selected');
-                $(this).addClass('guest_list_button_selected');
-                $('#awaiting_reply').hide();
-                $('#attending_modal_content').show();
-            }
-        });
-        
-        $('.awaiting_button').click(function(){
-            if(!$(this).hasClass('guest_list_button_selected'))
-            {
-                $('.guest_list_button_selected').removeClass('guest_list_button_selected');
-                $(this).addClass('guest_list_button_selected');
-                $('#attending_modal_content').hide();
-                $('#awaiting_reply').show();
-            }
-            
-        });
-        
-    });
+    
 }
 
 function following_click_handler(){
