f7e83017dfffc03572f92bd2edaea06aa837a337
Wells Johnston <wellsjohnston@gmail.com>
2011-08-28 08:40:42 -0500

    stuff

--- application/models/load_friend_plans.php
+++ application/models/load_friend_plans.php
@@ -141,7 +141,7 @@ class Load_friend_plans extends CI_Model
         } else
         {
             ?>
-            <br/><hr/><font style="font-style:italic;">No plans yet</font><br/><br/>
+            <br/><hr/><font style="font-style:italic;color:gray; position: relative;top:3px;">No plans yet</font><br/><br/>
             <?php
         }
         return ob_get_clean();
