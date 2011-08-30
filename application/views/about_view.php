dca77836ea1dcef6b5e00b7fa01e3e5a46327557
Wells Johnston <wellsjohnston@gmail.com>
2011-08-30 00:02:56 -0500

    stuff

--- application/views/about_view.php
+++ application/views/about_view.php
@@ -44,7 +44,7 @@
                 </div>  
                 <div id="container">  
 
-                    <h2> Our Team <span class="arrow"></span> </h2> 
+                    <h2> About Our Team <span class="arrow"></span> </h2> 
 
                     <h4>Mason Hensley, Director of Black Ops, CEO & Co-Founder <span class="arrow_right"></span> </h4>
                     <div class ="person">
