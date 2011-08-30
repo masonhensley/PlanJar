cc10b932a11cb72e795cc1ffa484ebfa3a398a64
Parker Bossier <parker@planjar.com>
2011-08-30 02:03:05 -0500

    boje

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -442,7 +442,7 @@ class Dashboard extends CI_Controller
             // Success
             echo(json_encode(array(
                 'status' => 'success',
-                'img' => urlencode(base_url() . "dashboard/display_temp_image/$user_id"),
+                'img' => urlencode(base_url() . "dashboard/display_temp_image/$user_id/" . rand(1000, 99999999)),
                 'user_id' => $user_id,
                 'width' => $width,
                 'height' => $height
