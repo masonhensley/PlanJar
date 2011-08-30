a36e92dc83a4a9e7ee7c0b088729967843fcd705
Wells Johnston <wellsjohnston@gmail.com>
2011-08-30 17:53:53 -0500

    stuff

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -175,8 +175,8 @@
         </div>
 
         <div class="bottom_links">
-            <a href="/help" id="bottom_link">Help & FAQ</a>
-            <a href="/about" id="bottom_link">About</a>
+            <a href="/help" id="bottom_link">FAQ</a>
+            <a href="/about" id="bottom_link">About Us</a>
             <a href="/privacy" id="bottom_link">Privacy</a>
             <!--<a href="http://blog.planjar.com/" id="profile_link">Blog</a>-->
         </div>
