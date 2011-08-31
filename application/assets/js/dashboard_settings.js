c749eaeb5e6b8d799b99f6e8d176d43af2590ba9
Parker Bossier <parker@planjar.com>
2011-08-30 20:54:00 -0500

    boje

--- application/assets/js/dashboard_settings.js
+++ application/assets/js/dashboard_settings.js
@@ -73,14 +73,21 @@ function initialize_settings() {
             if (data.status == 'error') {
                 alert(data.message);
             } else {
+                // Disable the imgAreaSelect
+                $('#preview_image').imgAreaSelect({
+                    disabled: true,
+                    hide: true
+                });
+                
                 // Success. Reset everything
                 $('#settings_content .right').hide('fast', function() {
                     $('#preview_image').attr('src', '');
-                    $('#crop_image input, #image_upload input').val('');
+                    $('#crop_image input, #image_upload input').not('[type="submit"]').val('');
                     $('#upload_crop').css('display', 'none');
                 });
                 
                 // Hide/show the upload form/alt text
+                $('#upload_submit').hide('fast');
                 $('#image_upload_alt').hide('fast', function() {
                     $('#image_upload').show('fast');
                 });
