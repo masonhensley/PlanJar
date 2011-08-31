deddbfda7ce6438a54068d9aa0a765a902a3e282
Wells Johnston <wellsjohnston@gmail.com>
2011-08-31 10:50:43 -0500

    stuff

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -92,7 +92,7 @@ class Dashboard extends CI_Controller
         {
             foreach ($query->result() as $row)
             {
-                $this->follow_ops->echo_user_entry($row, 'remove following');
+                $this->follow_ops->echo_user_entry($row, 'remove following', $profile_links_enabled=false);
             }
         } else
         {
