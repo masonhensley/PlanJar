cb6680e321d24de63b2d38c32d5b1fde35065e5e
Wells Johnston <wellsjohnston@gmail.com>
2011-08-30 19:05:19 -0500

    stuff

--- application/assets/js/home_functions.js
+++ application/assets/js/home_functions.js
@@ -20,7 +20,7 @@ function fetch_notifications() {
     $.get('/home/get_notification_popup', function(data){
         if(data > 0)
         {
-            $('#notifications .top_right_link_inner').badger(data);
+            $('#notifications .top_right_link_outer').badger(data);
         }
             
         notification_timer = setTimeout(fetch_notifications, 45000);
