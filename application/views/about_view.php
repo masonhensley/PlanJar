339a939611ceeb5409385750033d9f4459cd8b01
Parker Bossier <parker@planjar.com>
2011-08-31 16:38:09 -0500

    boje

--- application/views/about_view.php
+++ application/views/about_view.php
@@ -54,6 +54,9 @@
                             Mason holds a bachelor’s degree in Biomedical Engineering from Vanderbilt University; while there, he dabbled a little in Financial Economics. He’s also an Eagle Scout.<br/>
                             <a href="http://twitter.com/masonhensley" class="twitter-follow-button" data-show-count="false">Follow @bossier330</a>
                             <script src="http://platform.twitter.com/widgets.js" type="text/javascript"></script>
+                            <a rel="author" href="https://plus.google.com/u/1/106727975968254892658/about">
+                                <img src="http://www.google.com/images/icons/ui/gprofile_button-32.png" width="32" height="32">
+                            </a>
                         </div>
                         <div class ="pic">
                             <img src="/application/assets/images/mason_bio.jpg" style="width:100%; height:100%;"/>
@@ -63,7 +66,7 @@
                     <h4>Parker Bossier, Director of Tactical Ops, Software Architect & Co-Founder &#3232;_&#3232;<span class="arrow_right"></span> </h4>
                     <div class ="person">
                         <div class ="info">
-                            Parker runs the engineering team with Wells (currently the only other member of said team) and directs the site architecture and development.
+                            Parker runs the engineering team with Wells (currently the only other member of said team) and directs the site's architecture and development.
                             Prior to this venture, Parker worked in IT for the legal sector and designed the VSVS online application as VSVS IT Chair.
                             <br/><br/>
                             Academically, Parker expects to graduate from Vanderbilt University in 2012 with a bachelor's degree in Computer Science and Math.<br/>
@@ -86,6 +89,9 @@
                             Outside of PlanJar, Wells is currently pursuing a Bachelor of Science in both Computer Science and Mathematics from Vanderbilt University.<br/>
                             <a href="http://twitter.com/wellsjohnston" class="twitter-follow-button" data-show-count="false">Follow @bossier330</a>
                             <script src="http://platform.twitter.com/widgets.js" type="text/javascript"></script>
+                            <a rel="author" href="https://plus.google.com/u/1/107114366163444332724/posts">
+                                <img src="http://www.google.com/images/icons/ui/gprofile_button-32.png" width="32" height="32">
+                            </a>
                         </div>
                         <div class ="pic">
                             <img src="/application/assets/images/wells_bio.jpg"/>
