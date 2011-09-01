2c2563ccd130ab3701cc526628c5846c3e13a444
Wells Johnston <wellsjohnston@gmail.com>
2011-09-01 16:08:37 -0500

    stuff

--- application/views/about_view.php
+++ application/views/about_view.php
@@ -6,36 +6,9 @@
         <!-- CSS -->
         <link rel=stylesheet href="/application/assets/css/about.css" type="text/css" />
         <link href='http://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet' type='text/css'>
-        <title>About us</title>  
-
-        <!--[if IE]>  
-            <style>  
-                .arrow { top: 100%; }  
-            </style>  
-        <![endif]-->  
-
-
-        <!-- Mixpanel --><script type="text/javascript">var mpq=[];mpq.push(["init","ccd5fd6c9626dca4f5a3b019fc6c7ff4"]);(function(){var a=document.createElement("script");a.type="text/javascript";a.async=true;a.src=(document.location.protocol==="https:"?"https:":"http:")+"//api.mixpanel.com/site_media/js/api/mixpanel.js";var b=document.getElementsByTagName("script")[0];b.parentNode.insertBefore(a,b)})();</script><!-- End Mixpanel -->
-
-        <!-- Google Analytics -->
-        <script type="text/javascript">
-
-            var _gaq = _gaq || [];
-            _gaq.push(['_setAccount', 'UA-23115103-4']);
-            _gaq.push(['_setDomainName', '.planjar.com']);
-            _gaq.push(['_trackPageview']);
-            
-            (function() {
-                var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-                ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-            })();
-            
-        </script>
-
+        <title>About us</title> 
     </head>  
     <body>
-
         <div class ="top_panel">
             <div class = "inside_top_panel">
                 <img src='/application/assets/images/pj_logo_white_text.png' style="float: left; margin-left:30px; height:80%; position:relative; top:5px;"/>
@@ -44,9 +17,7 @@
                     <a href='/auth/logout' id="profile_link" style="position:absolute; top: 11px; left: 293px;">Log Out</a>
                 </div>  
                 <div id="container">  
-
                     <h2> About Our Team <span class="arrow"></span> </h2> 
-
                     <h4>Mason Hensley, Director of Black Ops, CEO & Co-Founder <span class="arrow_right"></span> </h4>
                     <div class ="person">
                         <div class ="info">
@@ -61,7 +32,6 @@
                         <div class ="pic">
                             <img src="/application/assets/images/mason_bio.jpg" style="width:100%; height:100%;"/>
                         </div>
-
                     </div>
                     <h4>Parker Bossier, Director of Tactical Ops, Software Architect & Co-Founder &#3232;_&#3232;<span class="arrow_right"></span> </h4>
                     <div class ="person">
@@ -80,7 +50,6 @@
                             <img src="/application/assets/images/parker_bio.jpg" style="width:100%; height:100%;" />
                         </div>
                     </div>
-
                     <h4> Wells Johnston, Director of Field Operations, Software Architect & Co-Founder ¯\_(ツ)_/¯<span class="arrow_right"></span> </h4>
                     <div class ="person">
                         <div class ="info">
@@ -99,33 +68,11 @@
 
                     </div> 
                 </div>
-
                 <div class="bottom_links">
                     <a href="/help" id="bottom_link">FAQ</a>
                     <a href="/about" id="bottom_link">About Us</a>
                     <a href="/privacy" id="bottom_link">Privacy</a>
                     <!--<a href="http://blog.planjar.com/" id="profile_link">Blog</a>-->
                 </div>
-
-                <script type="text/javascript">
-                    var _sf_async_config={uid:27655,domain:"testing.pagodabox.com"};
-                    (function(){
-                        function loadChartbeat() {
-                            window._sf_endpt=(new Date()).getTime();
-                            var e = document.createElement('script');
-                            e.setAttribute('language', 'javascript');
-                            e.setAttribute('type', 'text/javascript');
-                            e.setAttribute('src',
-                            (("https:" == document.location.protocol) ? "https://a248.e.akamai.net/chartbeat.download.akamai.com/102508/" : "http://static.chartbeat.com/") +
-                                "js/chartbeat.js");
-                            document.body.appendChild(e);
-                        }
-                        var oldonload = window.onload;
-                        window.onload = (typeof window.onload != 'function') ?
-                            loadChartbeat : function() { oldonload(); loadChartbeat(); };
-                    })();
-                        
-                </script>
-
                 </body>  
                 </html>  
