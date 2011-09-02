22365920138ee2792a73334d6e3aa92c8546f482
Wells Johnston <wellsjohnston@gmail.com>
2011-09-02 18:26:18 -0500

    stuff

--- application/views/privacy_view.php
+++ application/views/privacy_view.php
@@ -6,36 +6,13 @@
 
         <!-- CSS -->
         <link rel=stylesheet href="/application/assets/css/privacy.css" type="text/css" />
+        <link rel=stylesheet href="/application/assets/css/home.css" type="text/css" />
+        
         <link href='http://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet' type='text/css'>
+        
         <title>Privacy</title>  
-
-        <!--[if IE]>  
-            <style>  
-                .arrow { top: 100%; }  
-            </style>  
-        <![endif]-->  
-
-        <!-- Mixpanel --><script type="text/javascript">var mpq=[];mpq.push(["init","ccd5fd6c9626dca4f5a3b019fc6c7ff4"]);(function(){var a=document.createElement("script");a.type="text/javascript";a.async=true;a.src=(document.location.protocol==="https:"?"https:":"http:")+"//api.mixpanel.com/site_media/js/api/mixpanel.js";var b=document.getElementsByTagName("script")[0];b.parentNode.insertBefore(a,b)})();</script><!-- End Mixpanel -->
-
-        <!-- Google Analytics -->
-        <script type="text/javascript">
-
-            var _gaq = _gaq || [];
-            _gaq.push(['_setAccount', 'UA-23115103-4']);
-            _gaq.push(['_setDomainName', '.planjar.com']);
-            _gaq.push(['_trackPageview']);
-            
-            (function() {
-                var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-                ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-            })();
-            
-        </script>
-
     </head>  
     <body>
-
         <div class ="top_panel">
             <div class = "inside_top_panel">
                 <img src='/application/assets/images/pj_logo_white_text.png' style="float: left; margin-left:30px; height:80%; position:relative; top:5px;"/>
@@ -82,6 +59,7 @@
                 </div> 
                 <div class="bottom_links">
                     <a href="/help" id="bottom_link">FAQ</a>
+                    <a href="/tutorial" id="bottom_link">Tour</a>
                     <a href="/about" id="bottom_link">About Us</a>
                     <a href="/privacy" id="bottom_link">Privacy</a>
                     <!--<a href="http://blog.planjar.com/" id="profile_link">Blog</a>-->
