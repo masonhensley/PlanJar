5527d87e1c1a7f1c1fec9039b09180e24c4560a0
Wells Johnston <wellsjohnston@gmail.com>
2011-09-03 11:56:38 -0500

    stuff

--- application/assets/php/dashboard_friends.php
+++ application/assets/php/dashboard_friends.php
@@ -8,7 +8,7 @@
 </div>
 <div class="right">
     <font style="color:darkgray;">Note: you are not "friends" with someone unless you are both following each other.  
-    If you are not following someone back, they can only see limited information on your profile.</font>
+    If you are not following someone back, they can only see your profile (not your plans or anything else).</font>
     <br/><hr/>
     <font style="color:gray; font-size:15px;">Select a person on the left to display their profile here</font>
 </div>
