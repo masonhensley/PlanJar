c113e2c11b743b1167930ec9af9cf8c3cb7806f4
Parker Bossier <parker@planjar.com>
2011-09-03 10:39:04 -0500

    boje

--- application/assets/php/dashboard_settings.php
+++ application/assets/php/dashboard_settings.php
@@ -6,7 +6,7 @@
                 <form id="image_upload" enctype="multipart/form-data">
                     <input name="image" id="image" type="file"/>
                     <br/>
-                    <input id="upload_submit" type="submit" value="Upload" style="display: none;"/>
+                    <inputtype="submit" value="Upload"/>
                 </form>
                 <div id="image_upload_alt" style="display: none;">Crop your image to the right...</div>
             </td>
