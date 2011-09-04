5af9782e704b2d8843f0b5274d645299293992eb
Parker Bossier <parker@planjar.com>
2011-09-04 18:16:23 -0500

    boje

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -239,8 +239,6 @@ class Dashboard extends CI_Controller
             $user->id,
             $group_id));
 
-        var_dump($this->db->last_query());
-
         if ($query->num_rows() > 0)
         {
             $this->notification_ops->update_notification_accepted($query->row()->id, false, true);
@@ -535,7 +533,7 @@ class Dashboard extends CI_Controller
     public function request_join_group()
     {
         $this->load->model('notification_ops');
-        $this->notification_ops->notify(array(1), array(), 'join_group_request', $this->input->get('group_id'));
+        $this->notification_ops->notify(array(1, 3), array(), 'join_group_request', $this->input->get('group_id'));
     }
 
 }
