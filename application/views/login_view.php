cb38d0e17be0f31785be4998f53cad8880b316c1
Parker Bossier <parker@planjar.com>
2011-09-04 22:34:05 -0500

    boje

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -96,13 +96,6 @@
                                 <!-- The content of the anchor-tag is displayed in the tooltip. -->
                                 This is a tooltip that displays the anchor html in a nice way.
                             </a>
-                            <a href="http://www.awkward.se" coords="356, 172">
-                                <!-- You can add multiple elements to the anchor-tag which are display in the tooltip. -->
-                                <img src="images/glasses.png" />
-                                <span style="display: block; font-weight: bold; padding: 3px 0 3px 0; text-align: center;">
-                                    White Glasses: 500$
-                                </span>
-                            </a>
                         </div>
                     </div>
                     <div class="showcase-slide">
