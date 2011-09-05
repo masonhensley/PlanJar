9f5f0a241e0116f18a3202659f99f3bf75f998e8
Parker Bossier <parker@planjar.com>
2011-09-05 12:23:08 -0500

    boje

---
+++ application/views/auth/reset_password_view.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html>
+    <head>
+        <title>PlanJar | Reset Password</title>
+
+        <link rel=stylesheet href="/application/assets/css/login.css" type="text/css" />
+        <style type="text/css">
+            .almost_there {
+                width: 400px;
+                padding-top:15%;
+                font-family:Arial, Helvetica, sans-serif;
+            }
+        </style>
+
+        <script type="text/javascript" src="/application/assets/js/chartbeat_head.js"></script>
+    </head>
+
+    <body>
+    <center>
+        <div class="almost_there">
+            <fieldset>
+                <legend>Reset Password</legend>
+                <p>Please check your email for your new password. You can then login and change your password.</p>
+            </fieldset>
+        </div>
+    </center>
+
+    <!-- Chartbeat -->
+    <script type="text/javascript" src="/application/assets/js/chartbeat_body.js"></script>
+</body>
+</html>
