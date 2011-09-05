697b83277473bf9a76db11e26e68b261b6e19fc1
Parker Bossier <parker@planjar.com>
2011-09-04 19:57:36 -0500

    boje

--- application/views/email_notification_view.php
+++ application/views/email_notification_view.php
@@ -33,7 +33,10 @@
         <div class="wrapper">
             <a href="<?php echo(base_url()); ?>">
                 <img src="<?php echo(base_url() . APPPATH . 'assets/images/logo_email_beta.png'); ?>"/>
-                <div style="float: right;"><?php echo($image); ?></div>
+                <?php if (isset($image))
+                { ?>
+                    <div style="float: right;"><?php echo($image); ?></div>
+                <?php } ?>
 
             </a>
             <hr/>
