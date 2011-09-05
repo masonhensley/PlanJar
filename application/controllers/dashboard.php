2cdb1ce77a16354b68ee3235ac8de609aa340b8e
Parker Bossier <parker@planjar.com>
2011-09-05 01:18:59 -0500

    boje

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -174,14 +174,6 @@ class Dashboard extends CI_Controller
                     $this->group_ops->echo_group_entry($row, 'joined');
                 }
             }
-        } else
-        {
-            ?> 
-            <div style="width:100%; height:40px; background-color:white;text-align: center;border-bottom: 1px solid black; border-top:1px solid black;padding-top:15px; padding-bottom: 15px">
-                You are not following any groups<br/>
-                <a href="/dashboard/groups/suggested" style="color:#110055; font-weight:bold;">Find groups</a>
-            </div>
-            <?php
         }
     }
 
