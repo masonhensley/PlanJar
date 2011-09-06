a9f6de27f2b1bd90bba2559b130dedc869747d76
Parker Bossier <parker@planjar.com>
2011-09-06 17:47:26 -0500

    boje

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -148,8 +148,6 @@ class Login extends CI_Controller
 
         $email_exists = $this->ion_auth->email_check($email);
 
-        var_dump($this->db->last_query());
-
         if ($query->num_rows() == 0)
         {
             echo ("If you entered you university address, PlanJar isn't at your school yet, but we're coming!");
