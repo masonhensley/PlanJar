3c20bcbae23678560f54e7a133c63716073b40b3
Wells Johnston <wellsjohnston@gmail.com>
2011-11-09 17:20:19 -0600

    find plans

--- application/config/config.php
+++ application/config/config.php
@@ -23,10 +23,10 @@ if ($_SERVER['environment'] == 'live')
     $config['base_url'] = 'http://planjar.com/';
 } else if ($_SERVER['environment'] == 'production')
 {
-    $config['base_url'] = 'http://pj-production.newpagodabox.com/';
+    $config['base_url'] = 'http://pj-production.pagodabox.com/';
 } else if ($_SERVER['environment'] == 'demo')
 {
-    $config['base_url'] = 'http://pj-demo.newpagodabox.com/';
+    $config['base_url'] = 'http://pj-demo.pagodabox.com/';
 }
 
 /*
