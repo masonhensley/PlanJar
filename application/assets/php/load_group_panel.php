0582f405ddcea96706eb4c49d659c72a9e56091a
Parker Bossier <parker@planjar.com>
2011-11-09 21:18:35 -0600

    boje

--- application/assets/php/load_group_panel.php
+++ application/assets/php/load_group_panel.php
@@ -56,7 +56,7 @@
         <div id="select_one_group" style="width:50px;">one</div>
         <div id="select_mult_groups" style="margin-left:5px;width:50px;">multiple</div>
     </div>
-    <br/>
+
     <div class="find_link"><a href="/dashboard/groups/suggested" style="display:inline-block;">Find groups</a></div><br/>
     <div class="find_link"><a href="/dashboard/following/suggested">Find people</a></div>
 </div>
